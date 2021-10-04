ac5830199f9830618247c81cf6d4242fd8831281
SilentTush <tkushwaha70@gmail.com>
2021-10-04 19:12:43 +0530

    changed subscribe images
    Former-commit-id: 9de86a4d6712fac2faa2c594b1e6cadc57e3a31f

--- src/components/Home/JoinUs.js
+++ src/components/Home/JoinUs.js
@@ -6,26 +6,17 @@ function JoinUs() {
   return (
     <section className={styles.joinSection}>
       <div className={`${styles.doodle} ${styles.dl1}`}>
-        <img
-          src="https://images.unsplash.com/photo-1595366266798-9752bd56c375?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
-          alt=""
-        />
+        <img src="https://i.ibb.co/yRsxh0y/Untitled-design-4.png" alt="" />
       </div>
       <div className={`${styles.doodle} ${styles.dl2}`} />
       <div className={`${styles.doodle} ${styles.dl3}`} />
       <div className={`${styles.doodle} ${styles.dl4}`}>
-        <img
-          src="https://images.unsplash.com/photo-1507036066871-b7e8032b3dea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
-          alt=""
-        />
+        <img src="https://i.ibb.co/GtLBxMY/Untitled-design-7.png" alt="" />
       </div>
       <div className={`${styles.doodle} ${styles.dl5}`} />
 
       <div className={`${styles.doodle} ${styles.dr1}`}>
-        <img
-          src="https://images.unsplash.com/photo-1476703993599-0035a21b17a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80"
-          alt=""
-        />
+        <img src="https://i.ibb.co/92nwnTL/Untitled-design-6.png" alt="" />
       </div>
       <div className={`${styles.doodle} ${styles.dr2}`} />
       <div className={`${styles.doodle} ${styles.dr3}`} />
