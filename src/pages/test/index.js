d0576024c1701895e3e883beff3017b3aa1d9f8b
SilentTush <tkushwaha70@gmail.com>
2021-10-21 20:44:45 +0530

    Added leftpanel
    Former-commit-id: 5235c47edb6f8285963da9f5cbd0966fd3ec879a [formerly 28eb8f26ea147e62ceff118d0d33df26cf11f776]
    Former-commit-id: 9c2526b90d3d9d4a98240cb972c2d488935fc791

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Home/Footer";
+import LeftPanel from "../../components/LeftPanel";
 import styles from "../../styles/test/test.module.scss";
 export default function Test() {
+  const [openLeftPanel, setOpenLeftPanel] = useState(false);
+
   return (
     <div className={styles.test}>
-      <Header />
+      <Header
+        openLeftPanel={openLeftPanel}
+        setOpenLeftPanel={setOpenLeftPanel}
+      />
+      <LeftPanel
+        openLeftPanel={openLeftPanel}
+        setOpenLeftPanel={setOpenLeftPanel}
+      />
       <iframe src="https://scriptjs.in/client-showcase/Module1/story.html" />
       <Footer />
     </div>
