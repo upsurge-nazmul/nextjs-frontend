635d34cd0ede46af7c9cde677230105a34eea947
SilentTush <tkushwaha70@gmail.com>
2021-11-03 18:53:29 +0530

    added modal for waitiling list cta in calcs and blogs
    Former-commit-id: fcab6a533992771ff5d90e3d37fd05b1d01dbc50 [formerly e30ca3e1bc64f8f12e97feb34388733f195803e8]
    Former-commit-id: 96dc302fd86342a830238fd05707272f2b94ec63

--- src/components/WaitingListCta.js
+++ src/components/WaitingListCta.js
@@ -1,11 +1,60 @@
 import { useRouter } from "next/dist/client/router";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/GeneralComponents/waitinglistcta.module.scss";
-
+import validator from "validator";
+import LoginApis from "../actions/apis/LoginApis";
 export default function WaitingListCta() {
   const router = useRouter();
+  const [show, setshow] = useState(false);
+  const [error, seterror] = useState("");
+  const [email, setemail] = useState("");
+  async function handleSignup() {
+    if (!validator.isEmail(email)) {
+      seterror("Enter valid email address");
+    } else {
+      let response = await LoginApis.saveemail({ email: email });
+      if (response) {
+        if (response.data.success) {
+          router.push("/waitlist/" + email);
+        } else {
+          seterror(response.data.message);
+        }
+      } else {
+        seterror("Error connecting to server");
+      }
+      // setshowauth(true);
+      // setauthmode("parent");
+      // setmailfromhome(email);
+    }
+  }
   return (
     <div className={styles.waitinglist}>
+      {show && (
+        <div className={styles.popup}>
+          <div
+            className={styles.background}
+            onClick={() => setshow(false)}
+          ></div>
+          <div className={styles.main}>
+            {error && <p className={styles.error}>{error}</p>}
+            <p className={styles.heading}>Join the Waiting List</p>
+            <input
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+              type="text"
+              placeholder="Enter your email"
+            />
+            <div className={styles.buttons}>
+              <div className={styles.skip} onClick={() => setshow(false)}>
+                Skip
+              </div>
+              <div className={styles.join} onClick={handleSignup}>
+                Join
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <div className={styles.left}>
         <img
           src="https://i.ibb.co/rstCXKx/rupixen-com-5lw6-CLBZl-Cg-unsplash.png"
@@ -25,10 +74,7 @@ export default function WaitingListCta() {
         ></img>
         <div className={styles.text}>
           <p>
-            <span
-              style={{ cursor: "pointer" }}
-              onClick={() => router.push("/")}
-            >
+            <span style={{ cursor: "pointer" }} onClick={() => setshow(true)}>
               Click here
             </span>{" "}
             to join the
