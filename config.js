7f2c3cd4d8632aa6627cc8eb74f63b44bf3aee69
SilentTush <tkushwaha70@gmail.com>
2021-11-19 18:02:03 +0530

    made demo
    Former-commit-id: 8f92893bbaba5e6fa9f80339c5c55ae9b1c3c05f [formerly dfae3875bdb5e25680967b91ca1faecb94da18e0]
    Former-commit-id: f3fc5698880e7398c8612a400bda3647eeaa9933

---
+++ config.js
@@ -0,0 +1,8 @@
+export const Servers = {
+  TestServer: "http://192.168.29.204:4000/",
+  LiveServer: "https://api.upsurgefi.com/",
+};
+export const GClientId =
+  "375248822516-a08u6u16jk762tjdjcc1nodb13dor3qj.apps.googleusercontent.com";
+
+export const apple_client_id = "";
