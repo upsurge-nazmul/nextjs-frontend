cb64223c8582a5345c1a9e8c39a27c16542333a4
SilentTush <tkushwaha70@gmail.com>
2021-11-20 18:44:51 +0530

    fixed quiz
    Former-commit-id: ab20804deb512153148f64505016cb313413bf18 [formerly 295e45b0d17696c6f5c065c7090ebebafe9ea591]
    Former-commit-id: 9d9fdab4ac0475c2d5b928df955cd7f46f648261

--- src/pages/quiz/index.js
+++ src/pages/quiz/index.js
@@ -284,64 +284,70 @@ function Quiz({ leaderboard }) {
           <div className={styles.left}>
             <p className={styles.headingmain}>We need a few more details</p>
             <p className={styles.error}>{error}</p>
-            <input
-              type="text"
-              className={styles.input}
-              value={name}
-              onChange={(e) => {
-                if (
-                  e.target.value.length > 1 &&
-                  e.target.value[e.target.value.length - 1] === " "
-                ) {
-                  setname(e.target.value);
-                }
-                if (!e.target.value[e.target.value.length - 1]) {
-                  setname("");
-                  return;
-                }
-                if (
-                  specialchars.includes(
-                    e.target.value[e.target.value.length - 1].toString()
-                  )
-                ) {
-                  return;
-                }
-                if (isNaN(e.target.value[e.target.value.length - 1]))
-                  setname(e.target.value);
+            <form
+              onKeyPress={(e) => {
+                if (e.key === "Enter") startgame();
               }}
-              placeholder="Name"
-            />
-            <input
-              type="text"
-              className={styles.input}
-              value={nickname}
-              onChange={(e) => setnickname(e.target.value)}
-              placeholder="Nickname (optional)"
-            />
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={styles.input}
-              placeholder="Email"
-            />
-            <input
-              value={phone}
-              type="text"
-              maxLength={10}
-              onChange={(e) => {
-                if (!e.target.value[e.target.value.length - 1]) {
-                  setphone("");
-                  return;
-                }
-                if (isNaN(e.target.value[e.target.value.length - 1])) {
-                  return;
-                }
-                setphone(e.target.value);
-              }}
-              className={styles.input}
-              placeholder="Phone (optional)"
-            />
+            >
+              <input
+                type="text"
+                className={styles.input}
+                value={name}
+                onChange={(e) => {
+                  if (
+                    e.target.value.length > 1 &&
+                    e.target.value[e.target.value.length - 1] === " "
+                  ) {
+                    setname(e.target.value);
+                  }
+                  if (!e.target.value[e.target.value.length - 1]) {
+                    setname("");
+                    return;
+                  }
+                  if (
+                    specialchars.includes(
+                      e.target.value[e.target.value.length - 1].toString()
+                    )
+                  ) {
+                    return;
+                  }
+                  if (isNaN(e.target.value[e.target.value.length - 1]))
+                    setname(e.target.value);
+                }}
+                placeholder="Name"
+              />
+              <input
+                type="text"
+                className={styles.input}
+                value={nickname}
+                onChange={(e) => setnickname(e.target.value)}
+                placeholder="Nickname (optional)"
+              />
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={styles.input}
+                placeholder="Email"
+              />
+              <input
+                value={phone}
+                type="text"
+                maxLength={10}
+                onChange={(e) => {
+                  if (!e.target.value[e.target.value.length - 1]) {
+                    setphone("");
+                    return;
+                  }
+                  if (isNaN(e.target.value[e.target.value.length - 1])) {
+                    return;
+                  }
+                  setphone(e.target.value);
+                }}
+                className={styles.input}
+                placeholder="Phone (optional)"
+              />
+            </form>
             <div className={styles.buttons}>
               <div className={styles.startbutton} onClick={startgame}>
                 Start Playing
