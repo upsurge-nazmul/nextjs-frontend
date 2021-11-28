bb68414639503a3c46c54f926ff9f086d7448c19
akshay <akshay>
2021-11-28 17:10:34 +0530

    waitlist cahnges
    Signed-off-by: akshay <akshay>
    
    Former-commit-id: 70c8d891ae6d2cc62006efd2a64c12766a45d0db [formerly 88cc0fc453a2d0764daa05ddd8932a7e070f55d0]
    Former-commit-id: 0a6a56bd5dd06505fe0a885c4bdbf577213b7e8a

---
+++ src/components/Dashboard/WaitlistComponent.js
@@ -0,0 +1,43 @@
+import { Router, useRouter } from "next/dist/client/router";
+import React from "react";
+import styles from "../../styles/Dashboard/waitlistDashboard.module.css";
+import Fb from "..//SVGcomponents/Fb";
+import Insta from "../SVGcomponents/Insta";
+import LinkedIN from "../SVGcomponents/LinkedInSvg";
+
+const  WaitlistComponent =({email, waitNum}) => {
+
+    return <div className={styles.cover}>
+        <p className={styles.heading}>{`#${waitNum}`}</p>
+        <div className={styles.restBlock}>
+            <p className={styles.restName}>Your current waiting list number</p>
+            <p className={styles.restNameSub}>To stay up to date at all times, follow us on.</p>
+            <div className={styles.socials}>
+            <a
+                href="https://www.facebook.com/upsurgeindia/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <Fb className={styles.social} />
+            </a>
+            <a
+                href="https://www.instagram.com/upsurge.india/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <Insta className={styles.social} />
+            </a>
+            <a
+                href="https://www.linkedin.com/company/upsurgeindia/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <LinkedIN className={styles.socialyt} />
+            </a>
+            </div>
+        </div>
+        
+    </div>;
+}
+
+export default WaitlistComponent;
