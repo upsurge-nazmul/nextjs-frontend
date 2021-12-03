305d3d71995326712dcf9c4705753392d099c531
SilentTush <tkushwaha70@gmail.com>
2021-12-03 13:42:28 +0530

    added mobile design for waitlist dashbaord
    Former-commit-id: 7ac32750cb2fdf55073a8f895620130b7f39bd3d [formerly 937b487ff5d5fad7a8fd490c32a8934fac3886ec]
    Former-commit-id: 6892c64fc5f16d119d4c45020f984e8acbb3eebb

--- src/components/Dashboard/UnicoinsSummary.js
+++ src/components/Dashboard/UnicoinsSummary.js
@@ -1,25 +1,15 @@
-import { Router, useRouter } from "next/dist/client/router";
 import React from "react";
-import styles from "../../styles/Dashboard/unicoinSummary.module.css";
-import Fb from "..//SVGcomponents/Fb";
-import Insta from "../SVGcomponents/Insta";
-import LinkedIN from "../SVGcomponents/LinkedInSvg";
+import styles from "../../styles/Dashboard/unicoinSummary.module.scss";
 
-const UnicoinsSummary =({unicoins}) => {
-    return (
+const UnicoinsSummary = ({ unicoins }) => {
+  return (
     <div className={styles.cover}>
-        <p className={styles.heading}>{unicoins}</p>
-            <div className={styles.restBlock}>
-            <p className={styles.restName}>Earned Unicoins</p>
-            
-        </div>
-        
-        {/* <div className={styles.green}></div>
-        <div className={styles.white}></div>
-        <div className={styles.ball4}></div>
-        <div className={styles.yellow}></div> */}
+      <p className={styles.heading}>{unicoins}</p>
+      <div className={styles.restBlock}>
+        <p className={styles.restName}>Earned Unicoins</p>
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default UnicoinsSummary;
