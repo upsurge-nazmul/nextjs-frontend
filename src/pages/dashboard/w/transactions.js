47527531a6932f4c49e9ff3e16d9ee82e538a540
SilentTush <tkushwaha70@gmail.com>
2021-12-24 20:16:37 +0530

    fixed prod issues
    Former-commit-id: 6270b5a099da013b1e2cc932349c37ce1f4a6511 [formerly 8aeea86e946e7163dde3a3e00d57076ba77414dc]
    Former-commit-id: 02c70f9edd4703567850eb311ba6628e5b4b38e3

--- src/pages/dashboard/w/transactions.js
+++ src/pages/dashboard/w/transactions.js
@@ -7,7 +7,8 @@ import styles from "../../../styles/Dashboard/transactions.module.scss";
 import DashboardLeftPanel from "../../../components/Dashboard/DashboardLeftPanel";
 import Toast from "../../../components/Toast";
 import { MainContext } from "../../../context/Main";
-export default function transactions({ userdatafromserver, vouchers }) {
+import TransactionVoucher from "../../../components/Dashboard/TransactionVoucher";
+export default function Transactions({ userdatafromserver, vouchers }) {
   const [toastdata, settoastdata] = useState({
     show: false,
     type: "success",
@@ -32,32 +33,11 @@ export default function transactions({ userdatafromserver, vouchers }) {
           <div className={styles.voucherWrapper}>
             {vouchers.map((item) => {
               return (
-                <div key={item.id} className={styles.voucher}>
-                  <div className={styles.imgwrapper}>
-                    <Image
-                      src={item?.img_url}
-                      layout="fill"
-                      objectFit="contain"
-                    />
-                  </div>
-                  <div className={styles.data}>
-                    <p className={styles.name}>{item.name}</p>
-                    <p className={styles.vendor}>Vendor : {item.vendor}</p>
-                    <p className={styles.time}>
-                      Date :{" "}
-                      {new Date(Number(item.timestamp)).getDate() +
-                        "-" +
-                        new Date(Number(item.timestamp)).getMonth() +
-                        "-" +
-                        new Date(Number(item.timestamp)).getFullYear() +
-                        " " +
-                        new Date(Number(item.timestamp)).getHours() +
-                        ":" +
-                        new Date(Number(item.timestamp)).getMinutes()}
-                    </p>
-                  </div>
-                  <div className={styles.details}>View Code</div>
-                </div>
+                <TransactionVoucher
+                  data={item}
+                  key={item.id}
+                  settoastdata={settoastdata}
+                />
               );
             })}
           </div>
