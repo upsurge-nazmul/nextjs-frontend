b6e9e7aeddf28e2c7ce93319528a08449ee1e1ea
SilentTush <tkushwaha70@gmail.com>
2021-12-31 18:03:24 +0530

    added avatars in edit screen
    Former-commit-id: 65d8464530c72f399d5fb637b9c9fda39e22f4bd [formerly fdc536df44ec6f06cfa9612e8f899a7eacf83aea]
    Former-commit-id: aef468226b2a1bd670431f4b84fdefb271c0388b

---
+++ src/components/ChangePassPopUp.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import styles from "../styles/GeneralComponents/changepass.module.scss";
+import validator from "validator";
+import LoginApis from "../actions/apis/LoginApis";
+import { useRouter } from "next/dist/client/router";
+import OTPCustomComponent from "./OTPCustomComponent";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import ModernInputBox from "./ModernInputBox";
+
+export default function ChangePassPopUp({
+  password,
+  setpassword,
+  confirmpassword,
+  setconfirmpassword,
+  setshowpopup,
+  subscribe,
+  settoastdata,
+  handleSave,
+  phone,
+  error,
+  seterror,
+  saveprofile,
+}) {
+  const [OTP, setOTP] = useState("");
+  const [mode, setmode] = useState("data"); // data and otp
+  useEffect(() => {
+    seterror("");
+  }, [password, mode, confirmpassword]);
+
+  async function genotp() {
+    LoginApis.genotp({ phone }).then((response) => {
+      if (response && response.data && response.data.success) {
+        settoastdata({
+          msg: "Otp sent",
+          show: true,
+          type: "success",
+        });
+        setmode("otp");
+      } else {
+        settoastdata({
+          msg: response?.data.message || "Error",
+          show: true,
+          type: "error",
+        });
+      }
+    });
+  }
+  return (
+    <div className={styles.changepass}>
+      <div
+        className={styles.background}
+        onClick={() => setshowpopup(false)}
+      ></div>
+
+      {mode !== "otp" ? (
+        <div className={styles.block}>
+          <div className={styles.cross} onClick={() => setshowpopup(false)}>
+            <CancelOutlinedIcon className={styles.icon} />
+          </div>
+          <p className={styles.heading}>Change Password</p>
+          <ModernInputBox
+            value={password}
+            setvalue={setpassword}
+            placeholder="Password"
+            secure={true}
+          />
+          <ModernInputBox
+            value={confirmpassword}
+            setvalue={setconfirmpassword}
+            placeholder="Confirm Password"
+            secure={true}
+          />
+          {error && <p className={styles.error}>{error}</p>}
+          <div className={styles.button} onClick={() => saveprofile()}>
+            Change
+          </div>
+        </div>
+      ) : (
+        <div className={styles.otpblock}>
+          <div className={styles.cross} onClick={() => setshowpopup(false)}>
+            <CancelOutlinedIcon className={styles.icon} />
+          </div>
+          <div className={styles.otpHeadWrapper}>
+            <p className={styles.text}>Enter the 6-digit code sent to you at</p>
+            <p className={styles.phone}>{"+91 " + phone}</p>
+          </div>
+          <div className={styles.otpWrapper} id="otpWrapper">
+            <OTPCustomComponent setotp={setOTP} size={6} />
+          </div>
+          {error && <p className={styles.error}>{error}</p>}
+
+          <div className={styles.resendButton} onClick={genotp}>
+            Resend OTP
+          </div>
+          <div className={styles.button} onClick={() => handleSave()}>
+            Continue
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/dashboard/w/editprofile.js
+++ src/pages/dashboard/w/editprofile.js
@@ -11,6 +11,7 @@ import LoginApis from "../../../actions/apis/LoginApis";
 import ModernInputBox from "../../../components/ModernInputBox";
 import CustomDatePicker from "../../../components/CustomDatePicker";
 import { MainContext } from "../../../context/Main";
+import ChangePassPopUp from "../../../components/ChangePassPopUp";
 
 export default function EditProfile({ data }) {
   const router = useRouter();
@@ -19,32 +20,44 @@ export default function EditProfile({ data }) {
     type: "success",
     msg: "",
   });
+  const [error, seterror] = useState("");
   const [type, settype] = useState(
     data.is_waiting_active ? "waitlist" : router.query.type || "parent"
   );
   const [mode, setmode] = useState("Edit Profile");
+  const [img, setimg] = useState(
+    data?.img_url || "https://i.ibb.co/v3vVV8r/default-avatar.png"
+  );
   const [firstname, setfirstname] = useState(data?.first_name || "");
   const [lastname, setlastname] = useState(data?.last_name || "");
   const [dob, setdob] = useState(data?.dob ? new Date(Number(data.dob)) : "");
-  console.log("dob", data);
   const [gender, setgender] = useState(data?.gender || "");
   const [phone, setphone] = useState(data?.phone || "");
   const [password, setpassword] = useState("");
   const [confirmpassword, setconfirmpassword] = useState("");
   const [showotp, setshowotp] = useState(false);
-  const [otpverified, setotpverified] = useState(false);
+  const [showpopup, setshowpopup] = useState(false);
   const { userdata, setuserdata } = useContext(MainContext);
 
   useEffect(() => {
     setuserdata(data);
   }, []);
   async function saveprofile() {
-    if (
-      (data && data.phone && phone === data.phone) ||
-      (type === "waitlist" && !phone)
-    ) {
+    if (data && data.phone && phone === data.phone && !password) {
       handleSave();
     } else {
+      if (password) {
+        if (password !== confirmpassword) {
+          seterror("Passwords do not match");
+          return;
+        }
+        if (!validatePassword(password)) {
+          seterror(
+            "Password must be of length 8 and also must contain minimum 1 number,1 symbol,1 uppercase,1 lowercase"
+          );
+          return;
+        }
+      }
       LoginApis.genotp({ phone }).then((response) => {
         if (response && response.data && response.data.success) {
           settoastdata({
@@ -63,7 +76,40 @@ export default function EditProfile({ data }) {
       });
     }
   }
-
+  function validatePassword(pass) {
+    if (!checkLength(pass)) {
+      return false;
+    }
+    if (!checkLower(pass)) {
+      return false;
+    }
+    if (!checkUpper(pass)) {
+      return false;
+    }
+    if (!checkSpecial(pass)) {
+      return false;
+    }
+    if (!checkNumber(pass)) {
+      return false;
+    }
+    return true;
+  }
+  function checkLength(pass) {
+    return pass.length >= 8;
+  }
+  function checkLower(pass) {
+    return !(pass.search(/[a-z]/) < 0);
+  }
+  function checkUpper(pass) {
+    // password.search(/.*[A-Z].*/) > 0)
+    return !(pass.search(/[A-Z]/) < 0);
+  }
+  function checkNumber(pass) {
+    return !(pass.search(/[0-9]/) < 0);
+  }
+  function checkSpecial(pass) {
+    return !(pass.search(/[!@#$%^&*]/) < 0);
+  }
   async function handleSave() {
     let updated_data = {
       email: data.email,
@@ -94,11 +140,38 @@ export default function EditProfile({ data }) {
     }
     let response = await DashboardApis.updateprofile(updated_data);
     if (response && response.data && response.data.success) {
+      setshowpopup(false);
       settoastdata({ msg: "Saved Successfully", show: true, type: "success" });
     } else {
       settoastdata({ msg: "Error", show: true, type: "error" });
     }
   }
+  const avatars = [
+    "girl1",
+    "girl2",
+    "girl3",
+    "girl4",
+    "girl5",
+    "girl6",
+    "girl7",
+    "girl8",
+    "girl9",
+    "girl10",
+    "girl11",
+    "girl12",
+    "girl13",
+    "boy1",
+    "boy2",
+    "boy3",
+    "boy4",
+    "boy5",
+    "boy6",
+    "boy7",
+    "boy8",
+    "boy10",
+    "boy11",
+    "boy12",
+  ];
   return (
     <div className={styles.manageChore}>
       <DashboardLeftPanel type="waitlist" />
@@ -120,96 +193,96 @@ export default function EditProfile({ data }) {
         />
         <div className={styles.mainContent}>
           <div className={styles.imagesection}>
-            <img
-              src={
-                data?.img_url || "https://i.ibb.co/v3vVV8r/default-avatar.png"
-              }
-              alt=""
-            />
+            <img src={img} alt="" />
           </div>
           <div className={styles.details}>
-            <ModernInputBox
-              value={firstname}
-              maxLength={10}
-              setvalue={setfirstname}
-              placeholder="First name"
-              disabled
-            />
-            <ModernInputBox
-              maxLength={10}
-              value={lastname}
-              setvalue={setlastname}
-              placeholder="Last Name"
-            />
-
-            {/* <input
-              type="date"
-              value={
-                dob
-                  ? new Date(parseInt(dob))?.toISOString().substr(0, 10) || ""
-                  : ""
-              }
-              onChange={(e) => {
-                if (
-                  new Date(e.target.value).getDate() >= new Date().getDate()
-                ) {
-                  settoastdata({
-                    msg: "Invaild date of birth",
-                    show: true,
-                    type: "error",
-                  });
-                } else {
-                  setdob(new Date(e.target.value).getTime());
-                }
-              }}
-              min="1997-01-01"
-              max="2030-12-31"
-              placeholder="dd-mm-yyyy"
-            /> */}
-            <DropDown
-              value={gender}
-              options={["male", "female", "other"]}
-              setvalue={setgender}
-              placeholder="Gender"
-            />
-            <ModernInputBox
-              maxLength={10}
-              value={phone}
-              extrastyle={{ marginTop: "20px" }}
-              setvalue={setphone}
-              placeholder="Phone"
-            />
-            <ModernInputBox
-              type="date"
-              placeholder="Date of birth"
-              value={dob}
-              onChange={(e) => {
-                if (e.getTime() >= new Date().getTime()) {
-                  settoastdata({
-                    msg: "Invaild date of birth",
-                    show: true,
-                    type: "error",
-                  });
-                } else {
-                  setdob(e);
-                }
-              }}
-            />
-            <ModernInputBox
-              value={password}
-              setvalue={setpassword}
-              placeholder="Password"
-              secure={true}
-            />
-
-            <ModernInputBox
-              value={confirmpassword}
-              setvalue={setconfirmpassword}
-              placeholder="Confirm Password"
-            />
-            <div className={styles.button} onClick={saveprofile}>
-              Save Changes
+            <div className={styles.row}>
+              <ModernInputBox
+                value={firstname}
+                maxLength={10}
+                setvalue={setfirstname}
+                placeholder="First name"
+                disabled
+              />
+              <ModernInputBox
+                maxLength={10}
+                value={lastname}
+                setvalue={setlastname}
+                placeholder="Last Name"
+              />
             </div>
+            <div className={styles.row}>
+              <DropDown
+                value={gender}
+                options={["male", "female", "other"]}
+                setvalue={setgender}
+                placeholder="Gender"
+              />
+              <ModernInputBox
+                maxLength={10}
+                value={phone}
+                setvalue={setphone}
+                placeholder="Phone"
+              />
+              <ModernInputBox
+                type="date"
+                placeholder="Date of birth"
+                value={dob}
+                onChange={(e) => {
+                  if (e.getTime() >= new Date().getTime()) {
+                    settoastdata({
+                      msg: "Invaild date of birth",
+                      show: true,
+                      type: "error",
+                    });
+                  } else {
+                    setdob(e);
+                  }
+                }}
+              />
+            </div>
+            <div className={styles.row}>
+              <p
+                className={styles.changepass}
+                onClick={() => setshowpopup(true)}
+              >
+                Change password
+              </p>
+              {showpopup && (
+                <ChangePassPopUp
+                  setpassword={setpassword}
+                  password={password}
+                  confirmpassword={confirmpassword}
+                  setconfirmpassword={setconfirmpassword}
+                  setshowpopup={setshowpopup}
+                  handleSave={handleSave}
+                  saveprofile={saveprofile}
+                  settoastdata={settoastdata}
+                  phone={phone}
+                  error={error}
+                  seterror={seterror}
+                />
+              )}
+
+              <div className={styles.button} onClick={saveprofile}>
+                Save Changes
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.avatars}>
+          <p className={styles.heading}>Select your avatar</p>
+          <div className={styles.wrapper}>
+            {avatars.map((item) => {
+              return (
+                <img
+                  onClick={() => setimg("/images/avatars/" + item + ".png")}
+                  key={"avatar" + item}
+                  src={"/images/avatars/" + item + ".png"}
+                  alt=""
+                />
+              );
+            })}
           </div>
         </div>
       </div>
