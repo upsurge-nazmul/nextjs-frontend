fd400e4f688a639e8df6524c613f1fe554fbe503
SilentTush <tkushwaha70@gmail.com>
2022-01-13 14:27:43 +0530

    fixed spinner
    Former-commit-id: 705e8faf7dbd91ed3492f9ba76c3ed92d7dba22b [formerly 815e555d23ed06b657f4345187db5be852df10c9]
    Former-commit-id: 33c541ef24410eaf6def050aa6daa4f810b80df9

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "../styles/GeneralComponents/spinner.module.scss";
 export default function Spinner() {
   return (
-    <div class={styles.spinner}>
-      <div class={styles.spinnerInner}>
+    <div className={styles.spinner}>
+      <div className={styles.spinnerInner}>
         <div></div>
       </div>
     </div>
