ed8a66dcb76dcb2298525e3fceca15fa71f4e7c4
SilentTush <tkushwaha70@gmail.com>
2022-03-01 16:35:36 +0530

    fixed issues
    Former-commit-id: a5c321f40f1c5bbe8060a1f83847607139156069 [formerly da7292ad1dfa57e623378026f415ddf267ee63f4]
    Former-commit-id: 6dbd565e3a87a367f6f346bc47e75ce734ef09d8

--- src/pages/products/[type].js
+++ src/pages/products/[type].js
@@ -21,6 +21,7 @@ export default function Products() {
   const [authmode, setauthmode] = useState("");
   const [showauth, setshowauth] = useState(false);
   const [error, setError] = useState("");
+  const [showpopup, setshowpopup] = useState(false);
   const [email, setEmail] = useState("");
   const [toastdata, settoastdata] = useState({
     show: false,
@@ -109,7 +110,10 @@ export default function Products() {
         authmode={authmode}
         setauthmode={setauthmode}
         setshowauth={setshowauth}
+        showpopup={showpopup}
+        settoastdata={settoastdata}
         showauth={showauth}
+        setshowpopup={setshowpopup}
       />
       <Toast data={toastdata} />
 
@@ -128,6 +132,7 @@ export default function Products() {
         setauthmode={setauthmode}
         setshowauth={setshowauth}
         showauth={showauth}
+        setshowpopup={setshowpopup}
         error={error}
         id="knowledge-quest"
       />
@@ -140,6 +145,7 @@ export default function Products() {
         authmode={authmode}
         setauthmode={setauthmode}
         setshowauth={setshowauth}
+        setshowpopup={setshowpopup}
         showauth={showauth}
         settoastdata={settoastdata}
         setshowwaitlistblock={setshowwaitlistblock}
