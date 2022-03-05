7b4f4b0073b868ee74281e37bb0e138d3b019d47
SilentTush <tkushwaha70@gmail.com>
2022-03-05 15:23:18 +0530

    added dexie open
    Former-commit-id: 70cf154a49c741d93c995d18c714757a0aba292e [formerly 6e7ba0b87f0dc9f166380b4d12397046247a1afb]
    Former-commit-id: c328bdfa0aadb8c94b6e2d271f04b0a0ba63397b

--- src/db.js
+++ src/db.js
@@ -4,3 +4,6 @@ export const db = new Dexie("Upsurge");
 db.version(1).stores({
   games: "++id, data ,wasm,framework,loader, version", // Primary key and indexed props
 });
+db.open().catch(function (e) {
+  console.error("Open failed: " + e);
+});
