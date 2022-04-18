39295bcc74a7ca6c6518118ab7ff0e3cac10babe
Nazmul Alom <nazmul@upsurge.in>
2022-04-19 00:18:24 +0600

    stock simulator basic chart added
    Former-commit-id: d22a0ee2d9e2e3dfb275e9077868ef4a7267b19e [formerly 762aa46eb9e176b483491d14c13e121d57cca6f9]
    Former-commit-id: 1d76cb7c3408e8d30f9d5e30627add1cfd25e1a9

--- src/components/StockSimulator/Dash/SimulatorChart.js
+++ src/components/StockSimulator/Dash/SimulatorChart.js
@@ -1,7 +1,46 @@
+import { Line } from "react-chartjs-2";
+import simulatorData from "./data.json";
+
 export default function SimulatorChart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Chart.js Line Chart",
+      },
+    },
+    scales: {
+      A: {
+        type: "linear",
+        position: "right",
+      },
+    },
+  };
+
+  const labels = simulatorData.map((item, i) => `${i + 1}/04`);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: simulatorData[0].Symbol,
+        data: simulatorData.map((item) => item.Last),
+        fill: true,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        tension: 0.4, // this smoothen the curve
+      },
+    ],
+  };
   return (
-    <div>
-      <h1>Stock simulator Chart </h1>
-    </div>
+    <Line
+      options={options}
+      data={data}
+      fallbackContent={"Something went wrong"}
+    />
   );
 }
