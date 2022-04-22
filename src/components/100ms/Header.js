df2858ec9ee389684b73f432abade7db2833f565
SilentTush <tkushwaha70@gmail.com>
2022-04-22 19:50:01 +0530

    fixed build issue
    Former-commit-id: f2807d17527ccff667bb4f13ba6c36a276860200 [formerly d44d44f571ab012f3ccaf77d2cbe42fe6bd38997]
    Former-commit-id: 49cfbcd24a4ddd8a00aebb05c0989695b84d3747

--- src/components/100ms/Header.js
+++ src/components/100ms/Header.js
@@ -1,33 +1,32 @@
 import {
-    selectIsConnectedToRoom,
-    useHMSActions,
-    useHMSStore
-  } from "@100mslive/react-sdk";
-  import React from "react";
-  
-  function Header() {
-    const isConnected = useHMSStore(selectIsConnectedToRoom);
-    const hmsActions = useHMSActions();
-  
-    return (
-      <header>
-        <img
-          className="logo"
-          src="https://ashwins93.app.100ms.live/static/media/100ms_logo.3cfd8818.svg"
-          alt="logo"
-        />
-        {isConnected && (
-          <button
-            id="leave-btn"
-            class="btn-danger"
-            onClick={() => hmsActions.leave()}
-          >
-            Leave Room
-          </button>
-        )}
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from "@100mslive/react-sdk";
+import React from "react";
+
+function Header() {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
+
+  return (
+    <header>
+      <img
+        className="logo"
+        src="https://ashwins93.app.100ms.live/static/media/100ms_logo.3cfd8818.svg"
+        alt="logo"
+      />
+      {isConnected && (
+        <button
+          id="leave-btn"
+          className="btn-danger"
+          onClick={() => hmsActions.leave()}
+        >
+          Leave Room
+        </button>
+      )}
+    </header>
+  );
+}
+
+export default Header;
