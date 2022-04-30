45eb0b358fa5a2b4cd88e51197cfd34241be379d
Nazmul Alom <nazmul@upsurge.in>
2022-04-30 15:43:16 +0600

    performance tables flex fix
    Former-commit-id: 7fbebee1695c68fdff110bdb7f56c4a6506c7452 [formerly c492957daf8a92ecd2b18bfec57caf91e5d0cfb9]
    Former-commit-id: 036d46400f5b9831784b13776fa30da4258518ce

--- src/components/StockSimulator/Portfolio/Performance.js
+++ src/components/StockSimulator/Portfolio/Performance.js
@@ -8,8 +8,8 @@ export default function Performance({ performanceData = [] }) {
       </div>
       <div className={styles.container}>
         <div className={styles.headRow}>
-          <div className={styles.rowitem}>Symbol</div>
           <div className={styles.rowitem}>Name</div>
+          <div className={styles.rowitem}>Symbol</div>
           <div className={styles.rowitem}>Current Price</div>
           <div className={styles.rowitem}>Purchase Price</div>
           <div className={styles.rowitem}>QTY</div>
@@ -20,8 +20,8 @@ export default function Performance({ performanceData = [] }) {
           {performanceData.map((row, index) => {
             return (
               <div className={styles.row} key={index}>
-                <div className={styles.rowitem}>{row.symbol}</div>
                 <div className={styles.rowitem}>{row.name}</div>
+                <div className={styles.rowitem}>{row.symbol}</div>
                 <div className={styles.rowitem}>{row.current_price}</div>
                 <div className={styles.rowitem}>{row.buying_price}</div>
                 <div className={styles.rowitem}>{row.quantity}</div>
