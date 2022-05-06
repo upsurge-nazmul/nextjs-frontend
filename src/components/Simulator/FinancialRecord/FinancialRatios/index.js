70a14476bbdc03f78acd6debb41c8f76961ae78f
Nazmul Alom <nazmul@upsurge.in>
2022-05-06 16:30:00 +0600

    financial ratios charts created
    Former-commit-id: 1666fb9f905c2589d3510c0e8670bec9c50dbb2e [formerly a3259a476d219b9a1e6c8b7851bca91278bd9e36]
    Former-commit-id: ae271bd9c5ef3d395d03e42a4ccab3521360bb1c

---
+++ src/components/Simulator/FinancialRecord/FinancialRatios/index.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react";
+import styles from "../../../../styles/StockSimulator/financialRatios.module.scss";
+import SimulatorApis from "../../../../actions/apis/SimulatorApis";
+import Chart from "./Chart";
+
+const Options = [
+  { name: "FWD PE NTM", value: "fwdPPerENtm" },
+  { name: "Fwd PEG Ratio NTM", value: "fwdPegRatioNtm" },
+  { name: "P/E", value: "pPerE" },
+  { name: "TEV/Fwd EBITDA NTM", value: "tevPerFwdEbitdaNtm" },
+  { name: "TEV/LTM EBITDA", value: "tevPerLtmEbitda" },
+];
+
+export default function FinancialRatios({ token, company, backAction }) {
+  const [finRatios, setFinRatios] = useState([]);
+  const [fwdPPerENtm, setFwdPPerENtm] = useState();
+  const [fwdPegRatioNtm, setFwdPegRatioNtm] = useState();
+  const [pPerE, setPPerE] = useState();
+  const [tevPerFwdEbitdaNtm, setTevPerFwdEbitdaNtm] = useState();
+  const [tevPerLtmEbitda, setTevPerLtmEbitda] = useState();
+  const [chartView, setChartView] = useState(Options[0].value);
+
+  useEffect(() => {
+    async function fetchFinRatios() {
+      let finRatios = await SimulatorApis.getFinancialRatios({
+        payload: {
+          symbol: company.symbol,
+        },
+        token,
+      });
+      if (finRatios.data.success) {
+        setFinRatios(finRatios.data.data.rows);
+      }
+    }
+    if (company) {
+      fetchFinRatios();
+    }
+  }, [company]);
+
+  useEffect(() => {
+    if (finRatios && finRatios.length) {
+      let fwdp = [];
+      let fwdPeg = [];
+      let ppe = [];
+      let tevNtm = [];
+      let tevLtm = [];
+
+      for (let item of finRatios) {
+        fwdp.push({
+          x: item.date,
+          y: parseFloat(item.fwd_p_per_e_ntm),
+        });
+        fwdPeg.push({
+          x: item.date,
+          y: parseFloat(item.fwd_peg_ratio_ntm),
+        });
+        ppe.push({
+          x: item.date,
+          y: parseFloat(item.p_per_e),
+        });
+        tevNtm.push({
+          x: item.date,
+          y: parseFloat(item.tev_per_fwd_ebitda_ntm),
+        });
+        tevLtm.push({
+          x: item.date,
+          y: parseFloat(item.tev_per_ltm_ebitda),
+        });
+      }
+
+      setFwdPPerENtm(fwdp);
+      setFwdPegRatioNtm(fwdPeg);
+      setPPerE(ppe);
+      setTevPerFwdEbitdaNtm(tevNtm);
+      setTevPerLtmEbitda(tevLtm);
+    }
+  }, [finRatios]);
+
+  console.log("!!!!!!!!!!", finRatios);
+
+  return (
+    <div className={styles.financialRatios}>
+      <div className={styles.headingArea}>
+        <button className={styles.backButton} onClick={backAction}>
+          back
+        </button>
+        <p className={styles.heading}>Financial Ratios</p>
+      </div>
+      <div className={styles.bodyArea}>
+        {chartView === Options[0].value && (
+          <div clssName={styles.chartArea}>
+            <p>{Options[0].name}</p>
+            <Chart chartData={fwdPPerENtm} className={styles.chart} />
+          </div>
+        )}
+        {chartView === Options[1].value && (
+          <div clssName={styles.chartArea}>
+            <p>{Options[1].name}</p>
+            <Chart chartData={fwdPegRatioNtm} className={styles.chart} />
+          </div>
+        )}
+        {chartView === Options[2].value && (
+          <div clssName={styles.chartArea}>
+            <p>{Options[2].name}</p>
+            <Chart chartData={pPerE} className={styles.chart} />
+          </div>
+        )}
+        {chartView === Options[3].value && (
+          <div clssName={styles.chartArea}>
+            <p>{Options[3].name}</p>
+            <Chart chartData={tevPerFwdEbitdaNtm} className={styles.chart} />
+          </div>
+        )}
+        {chartView === Options[4].value && (
+          <div clssName={styles.chartArea}>
+            <p>{Options[4].name}</p>
+            <Chart chartData={tevPerLtmEbitda} className={styles.chart} />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.footerArea}>
+        {Options.map((option, i) => {
+          return (
+            <button key={i} onClick={() => setChartView(option.value)}>
+              {option.name}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
