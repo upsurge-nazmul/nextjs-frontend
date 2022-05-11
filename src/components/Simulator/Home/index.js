9d0dbe08b8d28ab37d550137165c52f782734f18
Nazmul Alom <nazmul@upsurge.in>
2022-05-11 14:18:14 +0600

    home structure created
    Former-commit-id: 1add7e680713a4bbdc2acf0afa29a94179c32a48 [formerly 112355d83753543f3179be59a07dd27fca75a972]
    Former-commit-id: daf4c4b8a69bae594e563cb3c13dfcf4d99c7040

--- src/components/Simulator/Home/index.js
+++ src/components/Simulator/Home/index.js
@@ -1,7 +1,16 @@
+import styles from "../../../styles/StockSimulator/home.module.scss";
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is homepage</h1>
+    <div className={styles.home}>
+      <div className={styles.left}>
+        <div className={styles.topLeft}>This is top left</div>
+        <div className={styles.bottomLeft}>This is bottom left</div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.topRight}>This is top right</div>
+        <div className={styles.bottomRight}>This is bottom right</div>
+      </div>
     </div>
   );
 }
