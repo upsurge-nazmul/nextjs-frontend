4da9b2b2fec14672b303a76c4cd58b80595f201b
Nazmul Alom <nazmul@upsurge.in>
2022-05-11 15:00:53 +0600

    home structure updated
    Former-commit-id: ef9fda58ebbb1703365c911ff32e324c6e2f9fe0 [formerly 6eefb95614db95705b624106402a62907e8f056b]
    Former-commit-id: 83c7ff3628e60e986c53932e41196be9d7f13c8d

--- src/components/Simulator/Home/index.js
+++ src/components/Simulator/Home/index.js
@@ -4,12 +4,40 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.left}>
-        <div className={styles.topLeft}>This is top left</div>
-        <div className={styles.bottomLeft}>This is bottom left</div>
+        <div className={styles.topLeft}>
+          <div className={styles.portfolioHoldings}>Portfolio holdings</div>
+          <div className={styles.portfolioInfo}>
+            <div className={styles.portfolioInfo1}>Info 1</div>
+            <div className={styles.portfolioInfo2}>Info 2</div>
+            <div className={styles.portfolioInfo3}>Info 3</div>
+            <div className={styles.portfolioInfo4}>Info 4</div>
+          </div>
+        </div>
+        <div className={styles.bottomLeft}>
+          <div className={styles.firstChart}>Chart 1</div>
+          <div className={styles.secondChart}>Chart 2</div>
+        </div>
       </div>
       <div className={styles.right}>
-        <div className={styles.topRight}>This is top right</div>
-        <div className={styles.bottomRight}>This is bottom right</div>
+        <div className={styles.topRight}>
+          <div className={styles.portfolioHeadingSection}>
+            <div className={styles.portfolioHeading}>
+              <div className={styles.heading}>You Portfolio</div>
+              <div className={styles.subHeading}>Last Updated...</div>
+            </div>
+            <div className={styles.portfolioOptions}>Options</div>
+          </div>
+          <div className={styles.myStocks}>List of my stocks</div>
+          <div className={styles.topReturns}>Top Return Stocks</div>
+        </div>
+        <div className={styles.bottomRight}>
+          <div className={styles.follwoingTitle}>People You're following</div>
+          <div className={styles.follwingCards}>
+            <div>Card 1</div>
+            <div>Card 2</div>
+            <div>Card 3</div>
+          </div>
+        </div>
       </div>
     </div>
   );
