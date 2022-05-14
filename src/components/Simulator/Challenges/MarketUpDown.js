5e353531645b3bc326b4c6cced3d40cb47d44b74
Nazmul Alom <nazmul@upsurge.in>
2022-05-14 17:52:58 +0600

    market up or down created
    Former-commit-id: 40694d3b921f10bf5505b9ae95961f319f5dc2ff [formerly d8a5a80d3f60dab754385586a8961282a726cb76]
    Former-commit-id: 91b70fca70d4ea120d525f68c38abb57a0e6f93c

--- src/components/Simulator/Challenges/MarketUpDown.js
+++ src/components/Simulator/Challenges/MarketUpDown.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import styles from "../../../styles/StockSimulator/marketUpDown.module.scss";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 export default function MarketUpDown() {
+  const [selected, setSelected] = useState();
+
+  const handleSelect = (value) => {
+    setSelected(value);
+    console.log("selected", value);
+  };
+
   return (
     <div className={styles.marketUpDown}>
       <div className={styles.topSection}>
@@ -12,6 +23,31 @@ export default function MarketUpDown() {
           Sed morbi pulvinar ornare gravida. Pulvinar turpis pellentesque
           porttitor nec phasellus justo, viverra. Duis varius risus, in tellus.
           In enim tincidunt nulla.
+        </div>
+      </div>
+      <div className={styles.bottomSection}>
+        <div className={styles.optionsArea}>
+          <div
+            className={selected === "up" ? styles.activeUp : styles.upOption}
+          >
+            <button onClick={() => handleSelect("up")}>
+              <CheckCircleIcon />
+              <div className={styles.name}>Market will go up</div>
+              <ArrowDropUpIcon />
+            </button>
+          </div>
+          <div className={styles.or}>OR</div>
+          <div
+            className={
+              selected === "down" ? styles.activeDown : styles.downOption
+            }
+          >
+            <button onClick={() => handleSelect("down")}>
+              <CheckCircleIcon />
+              <div className={styles.name}>Market will go down</div>
+              <ArrowDropDownIcon />
+            </button>
+          </div>
         </div>
       </div>
     </div>
