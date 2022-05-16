5532d9ff3082b96cf1da44624a1d435ca920fbe5
Nazmul Alom <nazmul@upsurge.in>
2022-05-16 19:05:11 +0600

    get stock x and get stock y api connected
    Former-commit-id: 6103a55205e38c3c782f28a0876a88ea6d6a063d [formerly 13f3873cdff2f896aed35e32d5426be635381ce1]
    Former-commit-id: 1820f880593d2a0c8076606b94bac2de16df16c1

--- src/components/Simulator/Challenges/StockXStockY.js
+++ src/components/Simulator/Challenges/StockXStockY.js
@@ -1,9 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../../styles/StockSimulator/stockXStockY.module.scss";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import SimulatorApis from "../../../actions/apis/SimulatorApis";
 
-export default function StockXStockY() {
+export default function StockXStockY({ token, simulatorType }) {
   const [selected, setSelected] = useState();
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+  useEffect(() => {
+    async function fetchStockXY() {
+      let stocks = await SimulatorApis.getStockXY({
+        payload: {},
+        token,
+        type: simulatorType,
+      });
+      if (stocks.data && stocks.data.success) {
+        const { x, y } = stocks.data.data;
+        setX(x);
+        setY(y);
+      }
+    }
+    fetchStockXY();
+  }, []);
+
+  useEffect(() => {
+    async function fetchStockXYAnswer() {
+      let stocks = await SimulatorApis.getStockXYAnswer({
+        payload: {},
+        token,
+        type: simulatorType,
+      });
+      if (stocks.data && stocks.data.success) {
+        const { answer } = stocks.data.data;
+        if (answer) setSelected(answer);
+      }
+    }
+    fetchStockXYAnswer();
+  }, []);
 
   const handleSelect = (value) => {
     setSelected(value);
@@ -24,33 +58,35 @@ export default function StockXStockY() {
         </div>
       </div>
       <div className={styles.bottomSection}>
-        <div className={styles.optionsArea}>
-          <div
-            className={selected === "up" ? styles.activeOption : styles.option}
-          >
-            <button onClick={() => handleSelect("up")}>
-              <div className={styles.checkIcon}>
-                {selected === "up" ? <CheckCircleIcon /> : ""}
-              </div>
-              <div className={styles.name}>First Company Name</div>
-              <div>₹567.23</div>
-            </button>
+        {x && y ? (
+          <div className={styles.optionsArea}>
+            <div
+              className={selected === "x" ? styles.activeOption : styles.option}
+            >
+              <button onClick={() => handleSelect("x")}>
+                <div className={styles.checkIcon}>
+                  {selected === "x" ? <CheckCircleIcon /> : ""}
+                </div>
+                <div className={styles.name}>{x.name}</div>
+                <div>{`₹${parseFloat(x.close).toFixed(2)}`}</div>
+              </button>
+            </div>
+            <div className={styles.or}>OR</div>
+            <div
+              className={selected === "y" ? styles.activeOption : styles.option}
+            >
+              <button onClick={() => handleSelect("y")}>
+                <div className={styles.checkIcon}>
+                  {selected === "y" ? <CheckCircleIcon /> : ""}
+                </div>
+                <div className={styles.name}>{y.name}</div>
+                <div>{`₹${parseFloat(y.close).toFixed(2)}`}</div>
+              </button>
+            </div>
           </div>
-          <div className={styles.or}>OR</div>
-          <div
-            className={
-              selected === "down" ? styles.activeOption : styles.option
-            }
-          >
-            <button onClick={() => handleSelect("down")}>
-              <div className={styles.checkIcon}>
-                {selected === "down" ? <CheckCircleIcon /> : ""}
-              </div>
-              <div className={styles.name}>Second Company Name</div>
-              <div>₹274.56</div>
-            </button>
-          </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
