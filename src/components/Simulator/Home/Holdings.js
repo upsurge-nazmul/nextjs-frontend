2c46d94ea3aa69c75dd0716aaa62246c7798bf07
Nazmul Alom <nazmul@upsurge.in>
2022-05-23 17:41:03 +0600

    dashboard portfolio holdings chart updated
    Former-commit-id: 98c620964953ff840febca992f2ca25c3cf1fbd6 [formerly a039db98a8ba2cc2b776e7b607197f2d9a21f330]
    Former-commit-id: 1a069b22ec7a8f041447885b63413a0037867d49

--- src/components/Simulator/Home/Holdings.js
+++ src/components/Simulator/Home/Holdings.js
@@ -1,62 +1,118 @@
-import { ResponsivePieCanvas } from "@nivo/pie";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
-export default function Holdings({ chartData, height = "200" }) {
+export default function Holdings({
+  chartData,
+  className,
+  legendPosition = "bottom",
+}) {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    if (chartData && chartData.length) {
+      let data = [];
+      let labels = [];
+
+      for (let item of chartData) {
+        data.push(item.amount);
+        labels.push(item.name);
+      }
+      setState({
+        series: data,
+        options: {
+          chart: {
+            type: "donut",
+          },
+          labels: labels,
+          dataLabels: {
+            enabled: true,
+            formatter: function (val, opts) {
+              return String(parseFloat(val).toFixed(2)) + "%";
+            },
+            textAnchor: "start",
+            offsetX: 20,
+            offsetY: -10,
+            style: {
+              fontSize: "14px",
+              fontWeight: 600,
+              fontFamily: "Helvetica, Arial, sans-serif",
+              fontWeight: "bold",
+              // colors: ["#fff"],
+            },
+            // dropShadow: {
+            //   enabled: false,
+            //   top: 1,
+            //   left: 1,
+            //   blur: 1,
+            //   color: "#000",
+            //   opacity: 0.45,
+            // },
+          },
+          legend: {
+            show: true,
+            position: legendPosition,
+            showForSingleSeries: true,
+            horizontalAlign: "center",
+            labels: {
+              colors: undefined,
+              useSeriesColors: true,
+            },
+            // width: 100,
+            formatter: function (val, opts) {
+              return opts.w.config.labels[opts.seriesIndex];
+              // + "₹" +
+              // String(
+              //   parseFloat(opts.w.config.series[opts.seriesIndex]).toFixed(2)
+              // )
+            },
+          },
+          responsive: [
+            {
+              breakpoint: 480,
+              options: {
+                legend: {
+                  position: "bottom",
+                },
+              },
+            },
+          ],
+          stroke: {
+            show: false,
+          },
+          plotOptions: {
+            pie: {
+              offsetX: 0,
+              offsetY: 0,
+              dataLabels: {
+                offset: 0,
+                minAngleToShowLabel: 10,
+              },
+            },
+          },
+          tooltip: {
+            y: {
+              formatter: (v) => parseFloat(v).toFixed(2),
+            },
+          },
+        },
+      });
+    }
+  }, [chartData]);
+
   return (
-    <ResponsivePieCanvas
-      data={chartData}
-      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-      valueFormat=" >-0.2f"
-      innerRadius={0.55}
-      activeOuterRadiusOffset={8}
-      colors={{ scheme: "nivo" }}
-      borderColor={{
-        from: "color",
-        modifiers: [["darker", 0.2]],
-      }}
-      arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextOffset={3}
-      arcLinkLabelsTextColor={{ from: "color", modifiers: [] }}
-      arcLinkLabelsDiagonalLength={10}
-      arcLinkLabelsStraightLength={12}
-      arcLinkLabelsThickness={3}
-      arcLinkLabelsColor={{ from: "color", modifiers: [] }}
-      enableArcLabels={false}
-      arcLabelsSkipAngle={10}
-      arcLabelsTextColor={{
-        from: "color",
-        modifiers: [["darker", 2]],
-      }}
-      defs={[
-        {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "rgba(255, 255, 255, 0.3)",
-          size: 4,
-          padding: 1,
-          stagger: true,
-        },
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "rgba(255, 255, 255, 0.3)",
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10,
-        },
-      ]}
-      fill={[]}
-      legends={[]}
-      motionConfig={{
-        mass: 1,
-        tension: 170,
-        friction: 26,
-        clamp: true,
-        precision: 0.01,
-        velocity: 0,
-      }}
-      height={height}
-    />
+    <div id="chart" className={className}>
+      {state && (
+        <ReactApexChart
+          options={state.options}
+          series={state.series}
+          type="donut"
+          width="100%"
+          height="100%"
+        />
+      )}
+    </div>
   );
 }
