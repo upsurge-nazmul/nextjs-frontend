398193a4fd2ae6213c37be2bec90ec67691da4d3
Nazmul Alom <nazmul@upsurge.in>
2022-05-27 16:51:01 +0600

    challenges updated with time
    Former-commit-id: dbc911fc024831a8b15989e334b068d8509d2976 [formerly fc3af88bffc881f4a1378c302aeb3344205030db]
    Former-commit-id: d5b8df35ccefdd5d26f2df9d1e21648c05124604

--- src/components/Simulator/Challenges/index.js
+++ src/components/Simulator/Challenges/index.js
@@ -6,10 +6,12 @@ import MarketUpDown from "./MarketUpDown";
 import StockXStockY from "./StockXStockY";
 import Topgainer from "./Topgainer";
 import BitcoinPriceEst from "./BitcoinPriceEst";
+import NoData from "../NoData";
 
 export default function Challenges({ userData, token, simulatorType }) {
   const [stockList, setStockList] = useState();
   const [topComps, setTopComps] = useState();
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
     async function fetchStocks() {
@@ -41,12 +43,35 @@ export default function Challenges({ userData, token, simulatorType }) {
     fetchTopCompanies();
   }, []);
 
+  useEffect(() => {
+    /**
+     * Monday to Friday
+     * Everyday, 3.30 pm to next day 9 am
+     * 24 hrs format, 15.30 to next day 09.00
+     * UTC 10.00 to next day 3.30
+     */
+    let today = new Date();
+    let day = today.getDay();
+    let hour = today.getUTCHours();
+    let minutes = today.getUTCMinutes();
+    let time = parseInt(hour) + parseFloat(((minutes / 60) * 100) / 100); // time in minute percentage; like half past ten, or quarter to 10
+
+    if (day >= 1 && day <= 5) {
+      // Monday to Friday
+      if (time >= 10 || time <= 3.5) {
+        // after 10 and before 3.30
+        setActive(true);
+      }
+    }
+    console.log(active, hour, minutes, time);
+  }, []);
+
   return (
     <div className={styles.stockChallenges}>
       <div className={styles.banerArea}>
         <div className={styles.banerLeft}>
           <div className={styles.banerText}>
-          {`Participate in daily challenges to win unicoins & other exciting rewards!`}
+            {`Participate in daily challenges to win unicoins & other exciting rewards!`}
           </div>
         </div>
         <div className={styles.banerRight}>
@@ -60,29 +85,39 @@ export default function Challenges({ userData, token, simulatorType }) {
           </div>
         </div>
       </div>
-      <div className={styles.topSection}>
-        <Topgainer
-          list={stockList}
-          currenTops={topComps}
-          {...{ token, simulatorType, userData }}
+      {active ? (
+        <>
+          <div className={styles.topSection}>
+            <Topgainer
+              list={stockList}
+              currenTops={topComps}
+              {...{ token, simulatorType, userData, active }}
+            />
+          </div>
+          {simulatorType === "cryptosimulator" ? (
+            <div className={styles.bottomSection}>
+              <BitcoinPriceEst
+                {...{ token, simulatorType, userData, active }}
+              />
+            </div>
+          ) : (
+            <div className={styles.bottomSection}>
+              <div className={styles.bottomLeft}>
+                <MarketUpDown {...{ token, simulatorType, userData, active }} />
+              </div>
+              <div className={styles.bottomRight}>
+                <StockXStockY {...{ token, simulatorType, userData, active }} />
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <NoData
+          message={
+            "You can participate in challenges before 9am and after 3.30pm every Monday to Friday"
+          }
         />
-      </div>
-      {
-        simulatorType === 'cryptosimulator'? (
-          <div className={styles.bottomSection}>
-          <BitcoinPriceEst {...{ token, simulatorType, userData }} />
-        </div>
-        ): (
-          <div className={styles.bottomSection}>
-        <div className={styles.bottomLeft}>
-          <MarketUpDown {...{ token, simulatorType, userData }} />
-        </div>
-        <div className={styles.bottomRight}>
-          <StockXStockY {...{ token, simulatorType, userData }} />
-        </div>
-      </div>
-        )
-      }
+      )}
     </div>
   );
 }
