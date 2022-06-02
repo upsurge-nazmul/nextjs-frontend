8d401c7fd358279ffa3c6adfb237358cc206c00a
Nazmul Alom <nazmul@upsurge.in>
2022-06-02 14:11:23 +0600

    child module dashboard user data fix
    Former-commit-id: 5cb55578747ffb53a16291c8a8b7f963c259815d [formerly fb97c5fcf5bf939e019566fdcd2416e6f0f04490]
    Former-commit-id: a63a54e954fe62f30517cf0a64dd259ba447589e

--- src/pages/dashboard/p/store.js
+++ src/pages/dashboard/p/store.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import DashboardApis from "../../../actions/apis/DashboardApis";
 import LoginApis from "../../../actions/apis/LoginApis";
 import Toast from "../../../components/Toast";
@@ -13,6 +13,7 @@ import DashboardHeader from "../../../components/Dashboard/DashboardHeader";
 import ApproveModal from "../../../components/ParentStore/ApproveModal";
 import ChoreApis from "../../../actions/apis/ChoreApis";
 import VoucherApis from "../../../actions/apis/VoucherApis";
+import { MainContext } from "../../../context/Main";
 
 export default function ParentStore({
   isLogged,
@@ -26,6 +27,7 @@ export default function ParentStore({
   requests,
 }) {
   // modes are different pages like home,kids,store,payments,notifications
+  const { setuserdata } = useContext(MainContext);
   const [mode, setmode] = useState("Store");
   const router = useRouter();
   const [searchresult, setsearchresult] = useState([]);
@@ -43,6 +45,10 @@ export default function ParentStore({
     name: "",
     description: "",
   });
+
+  useEffect(() => {
+    setuserdata(userdatafromserver);
+  }, [userdatafromserver]);
 
   useEffect(() => {
     if (isLogged === false) {
