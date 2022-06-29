588e496f3fe4cfe6ff00ec3361dec41b25d55db1
Nazmul Alom <nazmul@upsurge.in>
2022-06-29 18:44:05 +0600

    new lesson data added
    Former-commit-id: 0da6d33a5d525aa74c658d8abc4b77bce52e9e17 [formerly 88a4d09b47a2b1ab705414d6de88727197f8ffe3]
    Former-commit-id: 0c3a151ef979e243b98d120a4afc8b866f72883f

--- src/components/ChildQuest/positions.js
+++ src/components/ChildQuest/positions.js
@@ -1,130 +1,130 @@
 export const positions = {
   quest1: [
     {
-      bottom: "10%",
-      right: "38%",
+      bottom: "23%",
+      right: "45%",
     },
     {
-      bottom: "50%",
-      right: "65%",
+      bottom: "47%",
+      right: "70%",
     },
     {
-      bottom: "65%",
-      right: "42%",
+      bottom: "70%",
+      right: "40%",
     },
     {
-      bottom: "32%",
-      right: "15%",
+      bottom: "23%",
+      right: "5%",
     },
   ],
   quest2: [
     {
-      bottom: "10%",
+      bottom: "3%",
+      right: "47%",
+    },
+    {
+      bottom: "26%",
+      right: "50%",
+    },
+    {
+      bottom: "43%",
+      right: "74%",
+    },
+    {
+      bottom: "64%",
+      right: "68%",
+    },
+    {
+      bottom: "69%",
       right: "38%",
     },
     {
-      bottom: "15%",
-      right: "38%",
+      bottom: "52%",
+      right: "35%",
     },
     {
-      bottom: "50%",
-      right: "65%",
-    },
-    {
-      bottom: "55%",
-      right: "65%",
-    },
-    {
-      bottom: "65%",
-      right: "42%",
-    },
-    {
-      bottom: "60%",
-      right: "42%",
-    },
-    {
-      bottom: "32%",
-      right: "15%",
+      bottom: "29%",
+      right: "6%",
     },
   ],
   quest3: [
     {
-      bottom: "10%",
-      right: "38%",
-    },
-    {
-      bottom: "15%",
-      right: "38%",
-    },
-    {
-      bottom: "50%",
-      right: "80%",
-    },
-    {
-      bottom: "55%",
-      right: "80%",
-    },
-    {
-      bottom: "50%",
-      right: "70%",
-    },
-    {
-      bottom: "55%",
-      right: "70%",
-    },
-    {
-      bottom: "65%",
+      bottom: "4%",
       right: "42%",
     },
     {
-      bottom: "60%",
-      right: "42%",
-    },
-    {
-      bottom: "75%",
-      right: "32%",
-    },
-    {
-      bottom: "70%",
-      right: "32%",
+      bottom: "24%",
+      right: "49%",
     },
     {
       bottom: "32%",
-      right: "15%",
+      right: "77%",
+    },
+    {
+      bottom: "45%",
+      right: "80%",
+    },
+    {
+      bottom: "72%",
+      right: "83%",
+    },
+    {
+      bottom: "66%",
+      right: "68%",
+    },
+    {
+      bottom: "70%",
+      right: "38%",
+    },
+    {
+      bottom: "53%",
+      right: "36%",
+    },
+    {
+      bottom: "33%",
+      right: "1%",
+    },
+    {
+      bottom: "65%",
+      right: "1%",
+    },
+    {
+      bottom: "83%",
+      right: "13%",
     },
   ],
   quest4: [
     {
-      bottom: "10%",
-      right: "38%",
+      bottom: "3%",
+      right: "49%",
     },
     {
-      bottom: "15%",
-      right: "38%",
+      bottom: "25%",
+      right: "50%",
     },
     {
-      bottom: "50%",
-      right: "65%",
+      bottom: "45%",
+      right: "76%",
     },
     {
-      bottom: "60%",
-      right: "65%",
+      bottom: "64%",
+      right: "67%",
     },
     {
-      bottom: "65%",
-      right: "42%",
+      bottom: "70%",
+      right: "36%",
     },
     {
-      bottom: "55%",
-      right: "42%",
+      bottom: "52%",
+      right: "35%",
     },
     {
-      bottom: "32%",
-      right: "15%",
+      bottom: "33%",
+      right: "6%",
     },
     {
-      bottom: "22%",
-      right: "15%",
+      bottom: "17%",
+      right: "2%",
     },
   ],
 };
