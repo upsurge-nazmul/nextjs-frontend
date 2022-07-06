2b579d7f17c8a4f1a6953e43a685612005d9361e
Nazmul Alom <nazmul@upsurge.in>
2022-07-06 11:51:23 +0600

    enamuration updated
    Former-commit-id: 382320d438008a153453f69638619c040fce4175 [formerly fa3fe1cb96d338b3fccfd76e0d4606601a06c52c]
    Former-commit-id: fa0b2db2ee8ffa9a95d954107105546eed8cf29b

--- src/components/ChildQuest/Quiz/Enamuration.js
+++ src/components/ChildQuest/Quiz/Enamuration.js
@@ -1,28 +1,45 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "../../../styles/knowledgeQuest/Quiz.module.scss";
 
 export default function Enamuration({ data, value, setValue }) {
-  useEffect(() => {
-    setValue(["", ""]);
-  }, []);
+  const [answers, setAnswers] = useState();
 
-  const handleChange = (e, val) => {
-    // setValue(e.target.value);
-    val[e.target.id] += e.target.value;
-    setValue(val);
+  useEffect(() => {
+    if (data && data.correct_answer) {
+      let ansArr = {};
+      for (let i in data.correct_answer) {
+        ansArr[i] = "";
+      }
+      setAnswers(ansArr);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (answers) {
+      setValue(answers);
+    }
+  }, [answers]);
+
+  const handleChange = (e, i) => {
+    setAnswers((prev) => {
+      return {
+        ...prev,
+        [i]: (prev[i] = e.target.value),
+      };
+    });
   };
 
   return (
     <div className={styles.enamuration}>
       <div className={styles.question}>{data.question}</div>
-      {value && (
+      {answers && (
         <div className={styles.answerArea}>
           {data.correct_answer.map((ans, i) => {
             return (
               <input
                 id={i}
-                value={value[i]}
-                onChange={(e) => handleChange(e, value)}
+                value={answers[i]}
+                onChange={(e) => handleChange(e, i)}
                 key={i}
               />
             );
