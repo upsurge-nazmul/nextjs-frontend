1d109fe61ff67e639c425a7553d4c86a7c063d2c
Nazmul Alom <nazmul@upsurge.in>
2022-08-08 20:13:47 +0600

    head area data updated
    Former-commit-id: 3051c6f3770a258a9aafce40e781b6fc84a231ba [formerly 3858d9fab410031f6accb5c2db625604e829d627]
    Former-commit-id: 763727fd3f6cf13c9020594ea11b4893adce835c

--- src/components/ChildQuest/HeadArea.js
+++ src/components/ChildQuest/HeadArea.js
@@ -4,59 +4,70 @@ import Image from "next/image";
 import HeaderCard from "./HeaderCard";
 
 export default function HeadArea({ data, handleCardClick, tab }) {
+  const [currentData, setCurrentData] = useState();
   const [highlight, setHighlight] = useState(data[0]);
 
   useEffect(() => {
-    if (data && tab) {
-      let ht = data.find((item) => item.quest_type === tab.title);
-      if (ht) setHighlight(ht);
+    if (data) {
+      let filt = data.filter((item) => item.quest_type === tab.title);
+      if (filt.length) {
+        setCurrentData(filt);
+        setHighlight(filt[0]);
+      } else {
+        setCurrentData(data);
+        setHighlight(data[0]);
+      }
     }
   }, [data, tab]);
 
   return (
-    <div className={styles.headSection}>
-      <div
-        className={styles.left}
-        onClick={() => handleCardClick(highlight.questId)}
-      >
-        <div className={styles.banner}>
-          <Image
-            src={require(`../../assets/kqTiles/${highlight.questId}Tile.svg`)}
-            alt={highlight.questId}
-            className={styles.img}
-            height={400}
-            width={300}
-          />
+    <>
+      {highlight && currentData && (
+        <div className={styles.headSection}>
+          <div
+            className={styles.left}
+            onClick={() => handleCardClick(highlight.questId)}
+          >
+            <div className={styles.banner}>
+              <Image
+                src={require(`../../assets/kqTiles/${highlight.questId}Tile.svg`)}
+                alt={highlight.questId}
+                className={styles.img}
+                height={400}
+                width={300}
+              />
+            </div>
+            <div
+              className={styles.typeChip}
+              style={{ backgroundColor: tab.background, color: tab.font }}
+            >
+              <p>{highlight.quest_type}</p>
+            </div>
+            <div>
+              <p className={styles.title}>{highlight.title}</p>
+              <p className={styles.detail}>
+                {highlight?.questDescription.length > 100
+                  ? highlight?.questDescription.substring(0, 100) + "..."
+                  : highlight?.questDescription || ""}
+              </p>
+              <p
+                className={styles.info}
+              >{`${highlight.chapters.length} chapters`}</p>
+            </div>
+          </div>
+          <div className={styles.right}>
+            {currentData.slice(1, 4).map((item) => {
+              return (
+                <HeaderCard
+                  data={item}
+                  key={item.questNo}
+                  handleCardClick={handleCardClick}
+                />
+              );
+            })}
+          </div>
         </div>
-        <div
-          className={styles.typeChip}
-          style={{ backgroundColor: tab.background, color: tab.font }}
-        >
-          <p>{highlight.quest_type}</p>
-        </div>
-        <div>
-          <p className={styles.title}>{highlight.title}</p>
-          <p className={styles.detail}>
-            {highlight?.questDescription.length > 100
-              ? highlight?.questDescription.substring(0, 100) + "..."
-              : highlight?.questDescription || ""}
-          </p>
-          <p
-            className={styles.info}
-          >{`${highlight.chapters.length} chapters`}</p>
-        </div>
-      </div>
-      <div className={styles.right}>
-        {data.slice(1, 4).map((item) => {
-          return (
-            <HeaderCard
-              data={item}
-              key={item.questNo}
-              handleCardClick={handleCardClick}
-            />
-          );
-        })}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
