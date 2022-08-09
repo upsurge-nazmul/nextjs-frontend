5e84e4c4e1c57cea34dd6f4f001bcbf53347f07f
Nazmul Alom <nazmul@upsurge.in>
2022-08-09 17:08:24 +0600

    money activity added
    Former-commit-id: ae5ab8678ab7d12f218e9bb42ec5c82e4ac80414 [formerly d8e3e65f1b0e9d1f4a22e328e7efe387779c045d]
    Former-commit-id: 01ce99ed145033112d2d6ac6efbfa407e4c5355d

--- src/components/ChildQuest/ActivityView.js
+++ src/components/ChildQuest/ActivityView.js
@@ -1,10 +1,17 @@
 import React from "react";
-import styles from "../../styles/Quest/questActivity.module.scss";
+import styles from "../../styles/knowledgeQuest/Views.module.scss";
 
-export default function ActivityView({}) {
+export default function ActivityView({ chapterId }) {
   return (
-    <div className={styles.activity}>
-      <div className={styles.commingSoon}>Coming Soon</div>
+    <div className={styles.view}>
+      <div className={styles.activityView}>
+        {/* <div className={styles.commingSoon}>Coming Soon</div> */}
+        <iframe
+          id="iframe"
+          className={styles.iframe}
+          src={`/KnowledgeQuest/${chapterId}/story.html`}
+        ></iframe>
+      </div>
     </div>
   );
 }
