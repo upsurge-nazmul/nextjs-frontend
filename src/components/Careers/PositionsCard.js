bc7f116d764ee49bee68e03e7be0bc802527e67b
Nazmul Alom <nazmul@upsurge.in>
2022-08-22 18:32:31 +0600

    positions card added in career page
    Former-commit-id: 591591ebe616fef644c112b98042d90600bd7845 [formerly 3747240a8a3bb8181ddfa8ca0b20d178e0d7b2f5]
    Former-commit-id: 38aaecc9fa7d28c862d8e8cff7336da9b1a1bea8

---
+++ src/components/Careers/PositionsCard.js
@@ -0,0 +1,18 @@
+import styles from "../../styles/Careers/positionsCard.module.scss";
+import ArrowRight from "../SVGcomponents/ArrowRight";
+
+export default function PositionsCard({ data }) {
+  return (
+    <div className={styles.positionsCard}>
+      <div className={styles.left}>
+        <div className={styles.position}>{data.position}</div>
+        <div className={styles.location}>{data.location}</div>
+      </div>
+      <div className={styles.right}>
+        <button className={styles.applyButton}>
+          Apply <ArrowRight clr={"#fff"} />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -1,44 +1,38 @@
 import React from "react";
-import JobComponent from "../../components/Careers/JobComponent";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Home/Footer";
 import styles from "../../styles/Careers/careers.module.scss";
 import Curve1 from "../../components/SVGcomponents/Curve1";
 import Curve2 from "../../components/SVGcomponents/Curve2";
+import Banner from "../../assets/careers/banner.png";
+import PositionsCard from "../../components/Careers/PositionsCard";
 
 function Careers() {
-  const data = [
+  const positionData = [
     {
-      section: "Brand & Marketing",
-      jobs: [
-        { title: "Content Specialist", location: "Delhi" },
-        { title: "Creative Social Media Designer", location: "Delhi" },
-        { title: "Growth Manager", location: "Delhi" },
-      ],
+      id: "gameProgrammer",
+      position: "Game Programmer",
+      location: "Delhi, India",
     },
     {
-      section: "Customer Success",
-      jobs: [
-        { title: "Customer Service", location: "Delhi" },
-        { title: "Senior Manager - Customer Experience", location: "Delhi" },
-        { title: "Team lead - Customer Service", location: "Delhi" },
-      ],
+      id: "gameArtist2D",
+      position: "Game Artist - 2D",
+      location: "Delhi, India",
     },
     {
-      section: "Brand & Marketing",
-      jobs: [
-        { title: "Content Specialist", location: "Delhi" },
-        { title: "Creative Social Media Designer", location: "Delhi" },
-        { title: "Growth Manager", location: "Delhi" },
-      ],
+      id: "reactJsIntern",
+      position: "Reactjs Intern",
+      location: "Delhi, India",
     },
     {
-      section: "Customer Success",
-      jobs: [
-        { title: "Customer Service", location: "Delhi" },
-        { title: "Senior Manager - Customer Experience", location: "Delhi" },
-        { title: "Team lead - Customer Service", location: "Delhi" },
-      ],
+      id: "reactNativeIntern",
+      position: "React Native Intern",
+      location: "Delhi, India",
+    },
+    {
+      id: "uiUxDesigner",
+      position: "UI/UX designer",
+      location: "Delhi, India",
     },
   ];
 
@@ -48,7 +42,30 @@ function Careers() {
       <div className={styles.mainContent}>
         <Curve1 className={styles.curve1} />
         <Curve2 className={styles.curve2} />
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <div className={styles.title}>Work with us</div>
+            <div className={styles.description}>
+              upsurge is building one of the first educational gaming platform
+              for Indian students to learn about entrepreneurship and financial
+              literacy, and develop critical modern life-skills through
+              educational games, gamified learning & real rewards!. <br />
+              We are an experiential education company working on novel products
+              to promote development among India's youth.
+            </div>
+          </div>
+          <div className={styles.banner}>
+            <img src={Banner.src} className={styles.bannerImg} />
+          </div>
+          <div className={styles.positionArea}>
+            <div className={styles.positionTitle}>Open Positions</div>
+            <div className={styles.positions}>
+              {positionData.map((position, i) => {
+                return <PositionsCard data={position} key={i} />;
+              })}
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
