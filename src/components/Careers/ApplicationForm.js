3928742ebf42da7183458a01179dda55daceb4bf
Nazmul Alom <nazmul@upsurge.in>
2022-08-23 17:14:55 +0600

    career input fields created
    Former-commit-id: a72415e952927d20c05d0db3f39420e2cee81665 [formerly bc039e95497f469913214aaf80980aadce5cedcb]
    Former-commit-id: 9a190785922693782aec4d6fab521da5b61cc0ff

---
+++ src/components/Careers/ApplicationForm.js
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import styles from "../../styles/Careers/application.module.scss";
+import Selection from "../Selection";
+
+export default function ApplicationForm({ positionData, selectedPosition }) {
+  const [name, setName] = useState("");
+  const [position, setPosition] = useState(selectedPosition);
+  const [resume, setResume] = useState();
+  const [portfolioLink, setPortfolioLink] = useState();
+  const [age, setAge] = useState();
+  const [lastCompany, setLastCompany] = useState();
+  const [lastDsn, setLastDsn] = useState();
+  const [lastCTC, setLastCTC] = useState();
+  const [whyUpsurge, setWhyUpsurge] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("*********** submit ", resume, whyUpsurge);
+  };
+
+  return (
+    <div className={styles.application}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.inputArea}>
+          <input
+            className={styles.input}
+            type={"text"}
+            value={name}
+            maxLength={1000}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+          <input
+            className={styles.input}
+            type={"number"}
+            value={age}
+            maxLength={1000}
+            onChange={(e) => setAge(e.target.value)}
+            placeholder="Age"
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <Selection
+            value={position}
+            setvalue={setPosition}
+            options={positionData}
+            placeholder="Select Child"
+            keyAccessor={"id"}
+            valueAccessor={"position"}
+          />
+          <input
+            className={styles.input}
+            type={"text"}
+            value={portfolioLink}
+            maxLength={1000}
+            onChange={(e) => setPortfolioLink(e.target.value)}
+            placeholder="Portfolio or GitHub Link"
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <label className={styles.fileInput}>
+            <input
+              type={"file"}
+              onChange={(e) => setResume(e.target.files[0])}
+              placeholder="Resume"
+            />
+            {resume ? resume.name : "Click here to select your resume"}
+          </label>
+        </div>
+        <div className={styles.inputArea}>
+          <input
+            className={styles.input}
+            type={"text"}
+            value={lastCompany}
+            maxLength={1000}
+            onChange={(e) => setLastCompany(e.target.value)}
+            placeholder="Last Company"
+          />
+          <input
+            className={styles.input}
+            type={"text"}
+            value={lastDsn}
+            maxLength={1000}
+            onChange={(e) => setLastDsn(e.target.value)}
+            placeholder="Last Designation"
+          />
+          <input
+            className={styles.input}
+            type={"text"}
+            value={lastCTC}
+            maxLength={1000}
+            onChange={(e) => setLastCTC(e.target.value)}
+            placeholder="Last CTC"
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <textarea
+            rows={"10"}
+            name={"Why upsurge"}
+            placeholder={"Why upsurge"}
+            value={whyUpsurge}
+            onChange={(e) => setWhyUpsurge(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <button onClick={handleSubmit} className={styles.submitButton}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/pages/careers/[position].js
+++ src/pages/careers/[position].js
@@ -5,6 +5,35 @@ import Curve1 from "../../components/SVGcomponents/Curve1";
 import Curve2 from "../../components/SVGcomponents/Curve2";
 import { useRouter } from "next/router";
 import styles from "../../styles/Careers/position.module.scss";
+import ApplicationForm from "../../components/Careers/ApplicationForm";
+
+const positionData = [
+  {
+    id: "gameProgrammer",
+    position: "Game Programmer",
+    location: "Delhi, India",
+  },
+  {
+    id: "gameArtist2D",
+    position: "Game Artist - 2D",
+    location: "Delhi, India",
+  },
+  {
+    id: "reactJsIntern",
+    position: "Reactjs Intern",
+    location: "Delhi, India",
+  },
+  {
+    id: "reactNativeIntern",
+    position: "React Native Intern",
+    location: "Delhi, India",
+  },
+  {
+    id: "uiUxDesigner",
+    position: "UI/UX designer",
+    location: "Delhi, India",
+  },
+];
 
 export default function Position() {
   const router = useRouter();
@@ -16,7 +45,14 @@ export default function Position() {
       <div className={styles.mainContent}>
         <Curve1 className={styles.curve1} />
         <Curve2 className={styles.curve2} />
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+          {position && (
+            <ApplicationForm
+              positionData={positionData}
+              selectedPosition={position}
+            />
+          )}
+        </div>
       </div>
       <Footer />
     </div>
