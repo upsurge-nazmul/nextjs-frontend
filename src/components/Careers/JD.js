da444075ca0eeaa676931c86c324bd424d1c430f
Nazmul Alom <nazmul@upsurge.in>
2022-08-24 17:12:20 +0600

    first job description added
    Former-commit-id: 890909414e4f2c7d3c373c002538e2d347e7d582 [formerly ff9cb5157e64e80ce4db7b3ba760df0084e78dc3]
    Former-commit-id: abd37d9fba4532057ce4b62296c0d99f8c4bdddf

--- src/components/Careers/JD.js
+++ src/components/Careers/JD.js
@@ -1,7 +1,45 @@
+import styles from "../../styles/Careers/jd.module.scss";
+
+const positionData = [
+  {
+    id: "gameProgrammer",
+    position: "Game Programmer",
+    location: "Delhi, India",
+  },
+  {
+    id: "gameArtist2D",
+    position: "Game Artist - 2D",
+    location: "Delhi, India",
+  },
+  {
+    id: "reactJsIntern",
+    position: "Reactjs Intern",
+    location: "Delhi, India",
+  },
+  {
+    id: "reactNativeIntern",
+    position: "React Native Intern",
+    location: "Delhi, India",
+  },
+  {
+    id: "uiUxDesigner",
+    position: "UI/UX designer",
+    location: "Delhi, India",
+  },
+];
+
 export default function JD({ position }) {
   return (
-    <div>
-      <h1>This is Job Description {position}</h1>
+    <div className={styles.jd}>
+      <div className={styles.heading}>upsurge is hiring</div>
+      <div className={styles.title}>
+        {positionData.find((item) => item.id === position).position}
+      </div>
+      <iframe
+        id="iframe"
+        className={styles.iframe}
+        src={`/JDs/${position}.html`}
+      ></iframe>
     </div>
   );
 }
