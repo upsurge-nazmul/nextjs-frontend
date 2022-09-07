cbba747d1e0f4ef87f6613ab515331fb10866489
Alok <alok@upsurge.in>
2022-09-07 13:50:25 +0530

    Remove sponsors
    Former-commit-id: 596376e717c688b418e7433d6a0d44ef74899e6a [formerly 0ec51e0bbd53f49a1e71e706612badabd1f752eb]
    Former-commit-id: 9287fce4b632838566e0508397fc0b2a8e47242c

--- src/pages/business_league/index.js
+++ src/pages/business_league/index.js
@@ -369,18 +369,7 @@ to start and scale a business.`}</p>
           </a>
           <p>{`Established in 2019, Mentor Mpact provides expert admissions counseling for top global undergraduate & MBA programs, along with profile-building & career development services to students. They have helped over 1,000 students get 3,000 admits across 20 countries!.`}</p>
         </div>
-        <div className={styles.sponsor}>
-          <a
-            target="_blank"
-            href="https://happyjars.com/"
-            rel="noopener noreferrer"
-          >
-            <img src={cosponsors[2]} alt="" />
-          </a>
-
-          <p>{`Leading startup in the F&B space which makes delicious range of healthy,
-clean-label and delicious nut butters, dessers & spreads for all.`}</p>
-        </div>
+        
       </div>
       <FaqSection customfaq={faqs} />
       <Footer />
