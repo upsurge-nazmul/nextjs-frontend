afc49f4374f2a5853e28688b22b947c65986ad73
Nazmul Alom <nazmul@upsurge.in>
2022-09-28 11:52:12 +0600

    kq full screen added
    Former-commit-id: 96be21625bae0e386a45ad9af32de2f7d7da5d13 [formerly ba48fb648cac8091e8ca044b94421c7ab827e639]
    Former-commit-id: 9aa1bea4d57bb47054ecff7b94de19f1b14e96e7

--- src/components/ChildQuest/RecordingView.js
+++ src/components/ChildQuest/RecordingView.js
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from "../../styles/knowledgeQuest/Views.module.scss";
+import FullScreen from "../SVGcomponents/FullScreen";
+import FullScreenExit from "../SVGcomponents/FullScreenExit";
 
 export default function RecordingView({ chapterId }) {
+  const [fullScreen, setFullScreen] = useState(false);
+
+  const manageFullScreen = () => {
+    setFullScreen((prev) => !prev);
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.body.requestFullscreen();
+    }
+  };
+
   return (
     <div className={styles.view}>
       <iframe
         id="iframe"
-        className={styles.iframe}
+        className={fullScreen ? styles.fullScreenIframe : styles.iframe}
         src={`/KnowledgeQuest/${chapterId}/story.html`}
+        allowFullScreen={true}
       ></iframe>
+      <button
+        className={
+          fullScreen ? styles.shrinkScreenButton : styles.fullScreenButton
+        }
+        onClick={manageFullScreen}
+      >
+        {fullScreen ? <FullScreenExit /> : <FullScreen />}
+      </button>
     </div>
   );
 }
