5f514691595973a19a806f2b4240bd58c49b009b
Nazmul Alom <nazmul@upsurge.in>
2022-09-29 17:10:52 +0600

    partner card flip on hovar added
    Former-commit-id: 4279d53e86fb42efd0ae55150fdc8f192252e494 [formerly f2b700df37af69ead706850e49aa9d16fc6a3e23]
    Former-commit-id: 583f2a23852f4a37d2c32dc8c4c36e9535a67883

--- src/components/PartnersComponent/PartnerCard.js
+++ src/components/PartnersComponent/PartnerCard.js
@@ -6,73 +6,72 @@ export default function PartnerCard({
   seeMoreCard = false,
   seeMoreClick = () => {},
 }) {
-  const [flip, setFlip] = useState(false);
-
   return (
-    <>
-      {!flip ? (
-        <div
-          className={styles.partnerCardFront}
-          style={{ backgroundColor: data ? data.color : "transparent" }}
-          onClick={() => setFlip((prev) => !prev)}
-        >
-          {seeMoreCard ? (
-            <div className={styles.seeMore} onClick={seeMoreClick}>
-              See more...{" "}
-            </div>
-          ) : (
-            <div className={styles.frontFace}>
-              <img
-                className={styles.banner}
-                src={
-                  require(`../../assets/partners/${data.banner}.svg`).default
-                    .src
-                }
-                alt={data.name}
-                loading="lazy"
-              />
-              <div className={styles.cardBody}>
-                {data.badge && (
-                  <div className={styles.badgeArea}>
-                    <div className={styles.badge}>{data.badge}</div>
-                  </div>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      ) : (
-        <div
-          className={styles.partnerCardBack}
-          style={{ backgroundColor: data ? data.color : "transparent" }}
-          onClick={() => setFlip((prev) => !prev)}
-        >
-          <div className={styles.backFace}>
-            <div className={styles.bannerArea}>
-              <img
-                className={styles.smallBanner}
-                src={
-                  require(`../../assets/partners/${data.banner}.svg`).default
-                    .src
-                }
-                alt={data.name}
-                loading="lazy"
-              />
-            </div>
+    <div
+      className={styles.partnerCard}
+      style={{
+        backgroundColor: data
+          ? data.color
+            ? data.color
+            : "transparent"
+          : "transparent",
+      }}
+    >
+      <div className={`${styles.card__face} ${styles.card__face__front}`}>
+        {seeMoreCard ? (
+          <div className={styles.seeMore} onClick={seeMoreClick}>
+            See more...{" "}
+          </div>
+        ) : (
+          <div className={styles.frontFace}>
+            <img
+              className={styles.banner}
+              src={
+                require(`../../assets/partners/${data.banner}.svg`).default.src
+              }
+              alt={data.name}
+              loading="lazy"
+            />
             <div className={styles.cardBody}>
-              <div className={styles.details}>
-                {data.details ? data.details : ""}
-              </div>
-              <div className={styles.terms}>
-                {data.details ? "Terms and Conditions apply." : ""}
-              </div>
-              <div className={styles.claimArea}>
-                <button className={styles.claimButton}>CLAIM NOW</button>
-              </div>
+              {data.badge && (
+                <div className={styles.badgeArea}>
+                  <div className={styles.badge}>{data.badge}</div>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={`${styles.card__face} ${styles.card__face__back}`}>
+        <div className={styles.backFace}>
+          <div className={styles.bannerArea}>
+            <img
+              className={styles.smallBanner}
+              src={
+                require(`../../assets/partners/${data.banner}.svg`).default.src
+              }
+              alt={data.name}
+              loading="lazy"
+            />
+          </div>
+          <div className={styles.cardBody}>
+            <div className={styles.details}>
+              {data.details ? data.details : ""}
+            </div>
+            <div className={styles.terms}>
+              {data.details ? "Terms and Conditions apply." : ""}
+            </div>
+            <div className={styles.claimArea}>
+              <button
+                className={styles.claimButton}
+                onClick={() => console.log("claim clicked")}
+              >
+                CLAIM NOW
+              </button>
             </div>
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
