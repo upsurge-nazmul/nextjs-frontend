8b0d3c95d67d817cbe06f7d720d47f90c8f0d57b
Nazmul Alom <nazmul@upsurge.in>
2022-09-29 15:43:44 +0600

    partner card rotation added
    Former-commit-id: e04083f03e690763d66f16dfd0cd537170571ea3 [formerly a6db19e9a1b807edbce3cb77dcba50a261b19b0b]
    Former-commit-id: 04f9a9005416cd1fac021529dc524797608bf5d1

--- src/components/PartnersComponent/PartnerCard.js
+++ src/components/PartnersComponent/PartnerCard.js
@@ -9,42 +9,53 @@ export default function PartnerCard({
   const [flip, setFlip] = useState(false);
 
   return (
-    <div
-      className={styles.partnerCard}
-      style={{ backgroundColor: data ? data.color : "transparent" }}
-      onClick={() => setFlip((prev) => !prev)}
-    >
-      {seeMoreCard ? (
-        <div className={styles.seeMore} onClick={seeMoreClick}>
-          See more...{" "}
-        </div>
-      ) : !flip ? (
-        <div className={styles.frontFace}>
-          <img
-            className={styles.banner}
-            src={
-              require(`../../assets/partners/${data.banner}.svg`).default.src
-            }
-            alt={data.name}
-            loading="lazy"
-          />
-          <div className={styles.cardBody}>
-            {data.badge && (
-              <div className={styles.badgeArea}>
-                <div className={styles.badge}>{data.badge}</div>
+    <>
+      {!flip ? (
+        <div
+          className={styles.partnerCardFront}
+          style={{ backgroundColor: data ? data.color : "transparent" }}
+          onClick={() => setFlip((prev) => !prev)}
+        >
+          {seeMoreCard ? (
+            <div className={styles.seeMore} onClick={seeMoreClick}>
+              See more...{" "}
+            </div>
+          ) : (
+            <div className={styles.frontFace}>
+              <img
+                className={styles.banner}
+                src={
+                  require(`../../assets/partners/${data.banner}.svg`).default
+                    .src
+                }
+                alt={data.name}
+                loading="lazy"
+              />
+              <div className={styles.cardBody}>
+                {data.badge && (
+                  <div className={styles.badgeArea}>
+                    <div className={styles.badge}>{data.badge}</div>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       ) : (
-        <div className={styles.backFace}>
-          <div className={styles.cardBody}>
-            <div className={styles.claimArea}>
-              <button className={styles.claimButton}>CLAIM NOW</button>
+        <div
+          className={styles.partnerCardBack}
+          style={{ backgroundColor: data ? data.color : "transparent" }}
+          onClick={() => setFlip((prev) => !prev)}
+        >
+          <div className={styles.backFace}>
+            <div className={styles.cardBody}>
+              <div className={styles.claimArea}>
+                <button className={styles.claimButton}>CLAIM NOW</button>
+              </div>
             </div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
