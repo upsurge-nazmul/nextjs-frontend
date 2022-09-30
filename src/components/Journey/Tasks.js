60d9e2577bcf7cd09d88697bfe4fdd7c5396034e
Nazmul Alom <nazmul@upsurge.in>
2022-09-30 20:11:42 +0600

    task type color updated
    Former-commit-id: 7cf9c631fc495d142e1ea2ef21d230b6f6a1ca88 [formerly 52616943ed69acfc00aaf9e06d7e6d0f413ce6ea]
    Former-commit-id: 12f6ea942b8ad18ae9f007c04215c0c997440766

--- src/components/Journey/Tasks.js
+++ src/components/Journey/Tasks.js
@@ -1,13 +1,7 @@
 import styles from "../../styles/Journey/tasks.module.scss";
 import UniCoinSvg from "../SVGcomponents/UniCoinSvg";
 import RewardTask from "./RewardTask";
-
-const TASK_TYPE_COLORS = [
-  { id: "Knowledge Quest", color: "#17d1bc" },
-  { id: "Game", color: "#ffcd00" },
-  { id: "Chore", color: "#4066eb" },
-  { id: "Activity", color: "#ff6163" },
-];
+import { TASK_TYPE_COLORS } from "../../static_data/Pathways_Data";
 
 export default function Tasks({ highlight, handleTaskClick }) {
   const taskCompleted = 0;
