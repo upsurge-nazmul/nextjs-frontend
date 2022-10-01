fd61721ef4ba6aef6f8e416de3d0c7f4982913b2
Alok <alok@upsurge.in>
2022-10-01 11:04:36 +0530

    reward unicoins
    Former-commit-id: 99cec06259b626190a401c6b11c671483c2c2d94 [formerly 2032a79593c228862f358ca7845e76011e8c9c01]
    Former-commit-id: f622767feb868d33109ff80fe615b264eb6b87ec

--- src/components/Journey/TaskDisplay.js
+++ src/components/Journey/TaskDisplay.js
@@ -4,9 +4,26 @@ import Modal from "../Modal";
 import { PATHWAY_TASK_TYPE } from "../../static_data/Pathways_Data";
 import { useRouter } from "next/dist/client/router";
 import PathwayQuiz from "./PathwayQuiz";
-;
+import DashboardApis from "../../actions/apis/DashboardApis";
+import { getCookie } from "../../actions/cookieUtils";
+
+async function addcoins(rewardamount) {
+  let res = await DashboardApis.rewardunicoinstochild(
+    {
+      coincount: rewardamount,
+    },
+    getCookie("accesstoken")
+  );
+  if (res && res.data && res.data.success) {
+    console.log(res);
+  } else {
+    console.log(res);
+  }
+}
+
 export default function TaskDisplay({ task, handleCancelClick }) {
   const router = useRouter();
+  addcoins(task.taskReward);
   if(task.type === PATHWAY_TASK_TYPE[0]) {
   return (
     <Modal
