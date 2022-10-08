b5137ffdd642a10d817050d249eab325606adfb2
Aaryansh <aaryanshsharma8@gmail.com>
2022-10-08 13:14:19 +0530

    CSS and content fixes
    Former-commit-id: fce6b42f01a2626de2b14c052d84074f4701786c [formerly 6f312d86fdfbe88ce2af6fe2f9b5040148dad7cb]
    Former-commit-id: 60674a2da27ec53fd15627faed4e2337618ce127

--- src/components/Home/Who.js
+++ src/components/Home/Who.js
@@ -31,17 +31,6 @@ function Who() {
           </div>
         </div>
         <div className={styles.left}>
-          <p>
-            {`upsurge is India’s 1st learning focussed gaming platform that aims to inspire & empower students with modern skills & knowledge, in a fun & experiential way through games, immersive learning & real rewards. We believe in practical learning and have developed our own curriculum & content in collaboration with entrepreneurs, and experiential learning and financial experts.`}
-          </p>
-          <p>
-            {`Starting with entrepreneurship & money management, critical life skills that are ignored in school, we are here to help elevate the next generation & help them achieve their financial success. In fact, money is taught in schools to children as young as 5 in some countries!`}
-          </p>
-          <p>
-            {`Only 25% of Indians are financially literate, and it is our mission to give wings to a `}
-            <b>financial literacy education</b>
-            {` & entrepreneurship movement in India by equipping and empowering the youth. The world as we know it is evolving, and the earlier we equip our children with these skills, the better!`}
-          </p>
         </div>
       </div>
     </div>
