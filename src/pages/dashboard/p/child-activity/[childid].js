0f95054b7f03d523bafb92fd22447d27d9d60bea
Alok <alok@upsurge.in>
2022-10-15 17:50:25 +0530

    remove tribes and leaderboards
    Former-commit-id: 49c5642031efac88390a07808f7fa6f65e1ef8a2 [formerly 415ad2d643d736c18160843f146cb6828788107e]
    Former-commit-id: beaabdd8a2de8284898d9e43668e2e9267bcbead

--- src/pages/dashboard/p/child-activity/[childid].js
+++ src/pages/dashboard/p/child-activity/[childid].js
@@ -96,187 +96,148 @@ export default function ChildActivity({
                   </div>
                 </div>
               </div>
-              {/*    {childTribes.length > 0 && (
-                <div className={styles.tribeheading}>
-                  <h2 className={styles.mainheading}>Tribes</h2>
-                </div>
-              )}
-              <div className={styles.tribes} id="tribewrapper">
-                {childTribes.map((tribe) => (
-                  <div className={styles.tribe} key={tribe.id}>
-                    <img
-                      src={
-                        tribe.tribe_img_url ||
-                        "https://imgcdn.upsurge.in/images/default-avatar.png"
-                      }
-                      alt=""
-                    />
-                    <p className={styles.name}>{tribe.name}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className={styles.leaderboardsection}> 
-              <h2 className={styles.heading}>Leaderboards</h2>
-              <div className={styles.wrapper}>
-                <div className={styles.element}>
-                  <p className={styles.rank}>250</p>
-                  <p className={styles.section}>Money ace</p>
-                </div>
-                <div className={styles.element}>
-                  <p className={styles.rank}>{highestquizscore ?? 0}</p>
-                  <p className={styles.section}>Money Quotient</p>
-                </div>
-                <div className={styles.element}>
-                  <p className={styles.rank}>2</p>
-                  <p className={styles.section}>Quests</p>
-                </div>
-                <div className={styles.element}>
-                  <p className={styles.rank}>100</p>
-                  <p className={styles.section}>Stock simulator</p>
-                </div>
-              </div>
-            </div> */}
-          </div>
-          <div className={styles.flexRight}>
-            <div className={styles.choreSection}>
-              <h2
-                className={styles.mainheading}
-                onClick={() => router.push("/dashboard/p/chores")}
-              >
-                Chores
-                <HeadingArrow />
-              </h2>
 
-              <div className={styles.wrapper}>
-                {chorearray.map((data, index) => {
-                  return (
-                    <ChoreComponent
-                      data={data}
-                      settoastdata={settoastdata}
-                      key={"chorecomponent" + index}
+            </div>
+            <div className={styles.flexRight}>
+              <div className={styles.choreSection}>
+                <h2
+                  className={styles.mainheading}
+                  onClick={() => router.push("/dashboard/p/chores")}
+                >
+                  Chores
+                  <HeadingArrow />
+                </h2>
+
+                <div className={styles.wrapper}>
+                  {chorearray.map((data, index) => {
+                    return (
+                      <ChoreComponent
+                        data={data}
+                        settoastdata={settoastdata}
+                        key={"chorecomponent" + index}
+                      />
+                    );
+                  })}
+                  {chorearray.length === 0 && (
+                    <FillSpace
+                      text={"No chores in progress"}
+                      extrastyle={{ margin: 0 }}
                     />
-                  );
-                })}
-                {chorearray.length === 0 && (
-                  <FillSpace
-                    text={"No chores in progress"}
-                    extrastyle={{ margin: 0 }}
-                  />
-                )}
+                  )}
+                </div>
               </div>
-            </div>
-            <div className={styles.questsection}>
-              <h2
-                className={styles.heading}
-                onClick={() => router.push("/dashboard/p/quests")}
-              >
-                Quests
-                <HeadingArrow />
-              </h2>
-              <div className="wrapper">
-                {quests.length === 0 && (
-                  <FillSpace
-                    text={"No quest in progress"}
-                    extrastyle={{ margin: 0 }}
-                  />
-                )}
+              <div className={styles.questsection}>
+                <h2
+                  className={styles.heading}
+                  onClick={() => router.push("/dashboard/p/quests")}
+                >
+                  Quests
+                  <HeadingArrow />
+                </h2>
+                <div className="wrapper">
+                  {quests.length === 0 && (
+                    <FillSpace
+                      text={"No quest in progress"}
+                      extrastyle={{ margin: 0 }}
+                    />
+                  )}
+                </div>
               </div>
-            </div>
-            <div className={styles.gamessection}>
-              <h2 className={styles.heading}>Recently played games</h2>
-              <div className={styles.wrapper}>
-                {recentgames.map((game) => {
-                  return <GameCard data={Game_Data[game]} key={game.id} />;
-                })}
-                {recentgames.length === 0 && (
-                  <FillSpace
-                    text={"No recent games"}
-                    extrastyle={{ margin: "0" }}
-                  />
-                )}
+              <div className={styles.gamessection}>
+                <h2 className={styles.heading}>Recently played games</h2>
+                <div className={styles.wrapper}>
+                  {recentgames.map((game) => {
+                    return <GameCard data={Game_Data[game]} key={game.id} />;
+                  })}
+                  {recentgames.length === 0 && (
+                    <FillSpace
+                      text={"No recent games"}
+                      extrastyle={{ margin: "0" }}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+      );
 }
 
-export async function getServerSideProps({ params, req }) {
-  let token = req.cookies.accesstoken;
-  let msg = "";
-  if (token) {
-    let response = await LoginApis.checktoken({
-      token: token,
+      export async function getServerSideProps({params, req}) {
+        let token = req.cookies.accesstoken;
+      let msg = "";
+      if (token) {
+        let response = await LoginApis.checktoken({
+        token: token,
     });
-    if (response && !response.data.success) {
-      msg = response.data.msg;
+      if (response && !response.data.success) {
+        msg = response.data.msg;
       return {
-        props: { isLogged: false, msg },
-        redirect: {
-          permanent: false,
-          destination: "/?err=02",
+        props: {isLogged: false, msg },
+      redirect: {
+        permanent: false,
+      destination: "/?err=02",
         },
       };
     } else {
-      let pendinchores = await ChoreApis.getchildchores(
-        { id: params.childid, type: "pending" },
-        token
+        let pendinchores = await ChoreApis.getchildchores(
+      {id: params.childid, type: "pending" },
+      token
       );
       let childdetail = await DashboardApis.getChildDetails(
-        {
-          id: params.childid,
+      {
+        id: params.childid,
         },
-        token
+      token
       );
       let highestquizscore = await QuizApis.highestscore({
         email: response.data.data.email,
       });
       let userTribes = await TribeApis.userTribes(
-        {
-          userId: params.childid,
+      {
+        userId: params.childid,
         },
-        token
+      token
       );
       let recentgames = await FreeGameApis.getrecentGames(
-        { id: params.childid },
-        token
+      {id: params.childid },
+      token
       );
 
       return {
         props: {
-          isLogged: true,
-          pendingchores:
-            pendinchores && pendinchores.data && pendinchores.data.success
-              ? pendinchores.data.data
-              : [],
-          childdetail:
-            childdetail && childdetail.data && childdetail.data.data
-              ? childdetail.data.data
-              : [],
-          highestquizscore: highestquizscore?.data.success
-            ? highestquizscore.data.data.score
-            : 0,
-          childTribes:
-            userTribes && userTribes.data && userTribes.data.success
-              ? userTribes.data.data
-              : [],
-          recentgames:
-            recentgames && recentgames.data && recentgames.data.success
-              ? recentgames.data.data
-              : [],
-          userdatafromserver: response.data.data,
+        isLogged: true,
+      pendingchores:
+      pendinchores && pendinchores.data && pendinchores.data.success
+      ? pendinchores.data.data
+      : [],
+      childdetail:
+      childdetail && childdetail.data && childdetail.data.data
+      ? childdetail.data.data
+      : [],
+      highestquizscore: highestquizscore?.data.success
+      ? highestquizscore.data.data.score
+      : 0,
+      childTribes:
+      userTribes && userTribes.data && userTribes.data.success
+      ? userTribes.data.data
+      : [],
+      recentgames:
+      recentgames && recentgames.data && recentgames.data.success
+      ? recentgames.data.data
+      : [],
+      userdatafromserver: response.data.data,
         },
       };
     }
   } else {
     return {
-      props: { isLogged: false, msg: "cannot get token" },
+        props: {isLogged: false, msg: "cannot get token" },
       redirect: {
         permanent: false,
-        destination: "/?err=01",
+      destination: "/?err=01",
       },
     };
   }
