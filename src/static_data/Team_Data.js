ba3229eb30cb9e746c3221fe3a0b39c44604d151
Nazmul Alom <nazmul@upsurge.in>
2022-11-21 18:35:31 +0600

    team page images updated
    Former-commit-id: d9fe91a553fea794b991ea224a712c016d9eed9c [formerly 2a470facffd465bed892100ac0785e23bed0bd9a]
    Former-commit-id: a38b1c3dbef512208f6fe81d9b191f6b36270318

--- src/static_data/Team_Data.js
+++ src/static_data/Team_Data.js
@@ -2,34 +2,31 @@ export const TEAM_DATA = {
   productAndGrowth: [
     {
       name: "Karan Baweja",
-      img_url: "/images/mentors/Karan.jpg",
+      img_url: "/images/team/Karan.jpg",
       position: "Product & Growth",
       link: "https://www.linkedin.com/in/karanbaweja/",
     },
     {
       name: "Anuj Sankla",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C4D03AQEUq-UPU-wwxA/profile-displayphoto-shrink_200_200/0/1642061295129?e=2147483647&v=beta&t=1OkFS-bl-ub92VcaNHAH28w2q_TGFjhEzAoQ2tU979U",
+      img_url: "/images/team/Anuj.jpeg",
       position: "Product & Growth",
       link: "https://www.linkedin.com/in/anujsankla/",
     },
     {
       name: "Ankur Tandon",
-      img_url: "/images/mentors/Ankur.jpg",
+      img_url: "/images/team/Ankur.jpg",
       position: "Product & Growth",
       link: "https://www.linkedin.com/in/ankur-tandon-636332a7/",
     },
     {
       name: "Ritika Dhall",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C5603AQHKCqLlU6471w/profile-displayphoto-shrink_200_200/0/1637672645990?e=2147483647&v=beta&t=PDY4L88plRaFKnGU0_lPzS33Vdt1oNv6FemmuMSwbsY",
+      img_url: "/images/team/Ritika.jpeg",
       position: "Product & Growth",
       link: "https://www.linkedin.com/in/ritika-dhall-30a84217a/",
     },
     {
       name: "Parth Sharma",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C4D03AQFHX0EWGjrEkw/profile-displayphoto-shrink_200_200/0/1603011603169?e=2147483647&v=beta&t=Y2ERkeEytS7BZ58BwsupnlE0DGv-RWFW2bqeOLod2u8",
+      img_url: "/images/team/Parth.jpeg",
       position: "Product & Growth",
       link: "https://www.linkedin.com/in/parthsprofile/",
     },
@@ -43,13 +40,13 @@ export const TEAM_DATA = {
     },
     {
       name: "Pukkalla Kartheek Raj",
-      img_url: "/images/mentors/Kartheek.jpg",
+      img_url: "/images/team/Kartheek.jpg",
       position: "Gaming",
       link: "https://www.linkedin.com/in/p-kartheek-raj-917957146/",
     },
     {
       name: "Ujjwal Saini",
-      img_url: "/images/mentors/Ujjwal.png",
+      img_url: "/images/team/Ujjwal.png",
       position: "Gaming",
       link: "https://www.linkedin.com/in/ujjwal-saini-a7bb30206/",
     },
@@ -61,15 +58,13 @@ export const TEAM_DATA = {
     },
     {
       name: "Nitish Varun",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C4D03AQG-FYS3JLJIqA/profile-displayphoto-shrink_800_800/0/1600745071335?e=2147483647&v=beta&t=ZIiTIIfs87EDjB9RpS4VkWlQoEzieNfFWQNpxOmSrmA",
+      img_url: "/images/team/Nitish.jpeg",
       position: "Gaming",
       link: "https://www.linkedin.com/in/nitish-varun-5996b4148/",
     },
     {
       name: "Harshit kumar",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C5603AQFTHRsRnxgqng/profile-displayphoto-shrink_200_200/0/1663733392927?e=2147483647&v=beta&t=S7B1Jy8jpLxLWjtjBKV7n71YhLndtQH0h-kVb7viD-U",
+      img_url: "/images/team/Harshit.jpg",
       position: "Gaming",
       link: "https://www.linkedin.com/in/krharshit/",
     },
@@ -77,7 +72,7 @@ export const TEAM_DATA = {
   learning: [
     {
       name: "Medha Narayanan",
-      img_url: "/images/mentors/Medha.jpg",
+      img_url: "/images/team/Medha.jpg",
       position: "Learning",
       link: "https://www.linkedin.com/in/medhanarayanan/",
     },
@@ -105,20 +100,19 @@ export const TEAM_DATA = {
   engineering: [
     {
       name: "Alok Prateek",
-      img_url: "/images/mentors/Alok.jpg",
+      img_url: "/images/team/Alok.jpg",
       position: "Engineering",
       link: "https://www.linkedin.com/in/alokprateek/",
     },
     {
       name: "Nazmul Alom",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/D5603AQH1o2oLEiETQA/profile-displayphoto-shrink_800_800/0/1665987562780?e=1671667200&v=beta&t=c_0g7C8MsmRd6AJtid5ZXurTKYA6y5BlnQBBVq2CTBU",
+      img_url: "/images/team/Nazmul.jpeg",
       position: "Engineering",
       link: "https://www.linkedin.com/in/nazmul-alom/",
     },
     {
       name: "Aaryansh Bhardwaj",
-      img_url: "/images/mentors/Aaryansh.jpg",
+      img_url: "/images/team/Aaryansh.jpg",
       position: "Engineering",
       link: "https://www.linkedin.com/in/aaryansh-b-5b98b41b3",
     },
@@ -126,15 +120,13 @@ export const TEAM_DATA = {
   design: [
     {
       name: "Nakul Saxena",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C4E03AQH8FePMEJZP6g/profile-displayphoto-shrink_200_200/0/1637245908352?e=2147483647&v=beta&t=UCMbm4dTk_sWkP1OcNESZL5JPeo-QnWsk_TubIOgKCk",
+      img_url: "/images/team/Nakul.jpeg",
       position: "Art & Design",
       link: "https://www.linkedin.com/in/nakul-saxena",
     },
     {
       name: "Yash Sharma",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C4D03AQH8KYW-E7tGkA/profile-displayphoto-shrink_800_800/0/1652769332598?e=2147483647&v=beta&t=iw9xT6eQPEjg74sj5e4XZi5FNu7JfOvC20RqzXEfd4U",
+      img_url: "/images/team/Yash.jpeg",
       position: "Art & Design",
       link: "https://www.linkedin.com/in/yash-sharma-522a00217/",
     },
@@ -153,15 +145,13 @@ export const TEAM_DATA = {
     },
     {
       name: "Anushka Sundram",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C5103AQG_utajTaqRKg/profile-displayphoto-shrink_800_800/0/1584534843757?e=2147483647&v=beta&t=84HBWP22nT9aJX_KcqPVM0GagPaUp3Rqfymtq4Ikrf0",
+      img_url: "/images/team/Anushka.jpeg",
       position: "Art & Design",
       link: "https://www.linkedin.com/in/anushka-sundram-1a022117a",
     },
     {
       name: "Bijendra Kumar",
-      img_url:
-        "https://media-exp1.licdn.com/dms/image/C5103AQETC-IrxyZ1Tw/profile-displayphoto-shrink_200_200/0/1562490644333?e=2147483647&v=beta&t=VhQkjtQyVjG3Mxb5__aHpXxQV4Fzy3zvRt6m42fFopM",
+      img_url: "/images/team/Bijendra.jpeg",
       position: "Art & Design",
       link: "https://www.linkedin.com/in/bijendra-kumar-aa462b40",
     },
@@ -169,7 +159,7 @@ export const TEAM_DATA = {
   boardOfAdvisors: [
     {
       name: "Varun Jairath",
-      img_url: "/images/mentors/Varun.jpg",
+      img_url: "/images/team/Varun.jpg",
       position: "Board of Advisors",
       link: "https://www.linkedin.com/in/varun-jairath-3305047/",
     },
@@ -187,67 +177,67 @@ export const TEAM_DATA = {
     },
     {
       name: "Chandni Singh",
-      img_url: "/images/mentors/Chandni.jpg",
+      img_url: "/images/team/Chandni.jpg",
       position: "Board of Advisors",
     },
     {
       name: "Yatin Thakur",
-      img_url: "/images/mentors/Yatin.jpg",
+      img_url: "/images/team/Yatin.jpg",
       position: "Board of Advisors",
     },
   ],
   mentors: [
     {
       name: "Ayush S",
-      img_url: "/images/mentors/Ayush.jpg",
+      img_url: "/images/team/Ayush.jpg",
       link: "https://www.linkedin.com/in/sharmaay/",
       position: "Kearney | IIM | IIT",
     },
     {
       name: "Deeksha N",
-      img_url: "/images/mentors/Deeksha.jpg",
+      img_url: "/images/team/Deeksha.jpg",
       link: "https://www.linkedin.com/in/deeksha-narula-b5941932/",
       position: "BCG | IIM",
     },
     {
       name: "Abhinandan G",
-      img_url: "/images/mentors/Abhinandan.jpg",
+      img_url: "/images/team/Abhinandan.jpg",
       link: "https://www.linkedin.com/in/abhinandan-gopalsetty-a936a116/",
       position: "Marketing @ BMW | IMI",
     },
     {
       name: "Shruti J",
-      img_url: "/images/mentors/Shruti.jpg",
+      img_url: "/images/team/Shruti.jpg",
       link: "https://www.linkedin.com/in/shruti-jolly-2217139/",
       position: "Startup Marketing Expert | ISB | x-McKinsey",
     },
     {
       name: "Harsha N",
-      img_url: "/images/mentors/Harsha.jpg",
+      img_url: "/images/team/Harsha.jpg",
       link: "https://www.linkedin.com/in/harsha8/",
       position: "Industry Leader @ Google | ISB",
     },
     {
       name: "Mayur S",
-      img_url: "/images/mentors/Mayur.jpg",
+      img_url: "/images/team/Mayur.jpg",
       link: "https://www.linkedin.com/in/mayursingh/",
       position: "Sustainability Warrior | Entrepreneur | ISB | Angel Investor",
     },
     // {
     //   name: "Yatin T",
-    //   img_url: "/images/mentors/Yatin.jpg",
+    //   img_url: "/images/team/Yatin.jpg",
     //   link: "https://www.linkedin.com/in/yatinthakur/",
     //   position: "GEN India MD | Entrepreneur | Startup India Founder",
     // },
     {
       name: "Purnima G",
-      img_url: "/images/mentors/Purnima.jpg",
+      img_url: "/images/team/Purnima.jpg",
       link: "https://www.linkedin.com/in/purnimagandhi/",
       position: "Temasek | Oxford",
     },
     {
       name: "Amit G",
-      img_url: "/images/mentors/Amit.jpg",
+      img_url: "/images/team/Amit.jpg",
       link: "https://www.linkedin.com/in/amit-gupta-42a2ab13/",
       position: "EY | ISB",
     },
