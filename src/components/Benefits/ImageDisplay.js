f36874df056cb4395c3404dcc70820c28587be16
Nazmul Alom <nazmul@upsurge.in>
2022-11-28 17:10:28 +0600

    image frame shapes added
    Former-commit-id: 0fefa786fb0abf6c9916d9e6ffeb50a2b71c3677 [formerly 671816804e55aa9d107c14872cbb3b03c97eb2d9]
    Former-commit-id: c9515ccf957103bdd956fc3441cf517eeda7d988

--- src/components/Benefits/ImageDisplay.js
+++ src/components/Benefits/ImageDisplay.js
@@ -1,8 +1,30 @@
 import styles from "../../styles/Benefits/imageDisplay.module.scss";
 
-export default function ImageDisplay({ src, alt }) {
+export default function ImageDisplay({
+  src = "",
+  alt = "",
+  frameType = "default",
+}) {
   return (
-    <div className={styles.layer1}>
+    <div
+      className={styles.layer1}
+      style={
+        frameType === "vertical"
+          ? {
+              height: "70vh",
+              width: "50vh",
+            }
+          : frameType === "horizontal"
+          ? {
+              height: "50vh",
+              width: "70vh",
+            }
+          : {
+              height: "60vh",
+              width: "60vh",
+            }
+      }
+    >
       <div className={styles.layer2}>
         <div className={styles.layer3}>
           <img src={src} alt={alt} className={styles.image} />
--- src/components/Home/Benefits.js
+++ src/components/Home/Benefits.js
@@ -10,6 +10,7 @@ const DATA = [
     description:
       "Financial literacy is a life-skill and the earlier your children start, the easier it will be for them to achieve their financial goals. Through our games, courses and live classes, all designed by experts, children will understand money, saving, investing and entrepreneurship like never before!",
     img: "https://upsurgevideoassets.s3.ap-south-1.amazonaws.com/images/benefit1+(1).png",
+    frameType: "vertical",
   },
   {
     id: 2,
@@ -17,6 +18,7 @@ const DATA = [
     description:
       "We believe that learning is most effective when it’s through play! Our games, activities and quizzes will instill healthy money habits that will last a lifetime.",
     img: "https://upsurgevideoassets.s3.ap-south-1.amazonaws.com/images/benefit2+(1).png",
+    frameType: "horizontal",
   },
   {
     id: 3,
@@ -24,6 +26,7 @@ const DATA = [
     description:
       "Equip your children with the necessary skills and knowledge to understand and evaluate the fundamentals of business and starting-up, and (hopefully) be the next unicorn founder.",
     img: "https://imgcdn.upsurge.in/images/benefit3.png",
+    frameType: "default",
   },
   {
     id: 4,
@@ -31,12 +34,14 @@ const DATA = [
     description:
       "As adults, when we do any job well, we get rewarded. Then why should it be any different for children? Students get rewarded with Unicoins for jobs well done, which can be redeemed for books, games, vouchers and educational courses from your favorite brands.",
     img: "https://imgcdn.upsurge.in/images/Maskz-Group.png",
+    frameType: "vertical",
   },
   {
     id: 5,
     title: "Develop 21st century skills & knowledge",
     description: `While jobs & roles change, the skills needed to succeed remain the same. Our offerings help children gain confidence & excel by developing their critical & analytical thinking and problem-solving skills.`,
     img: "https://imgcdn.upsurge.in/images/benefit4.png",
+    frameType: "horizontal",
   },
 ];
 
@@ -103,7 +108,11 @@ function Benefits() {
         </div>
       </div>
       <div className={styles.right}>
-        <ImageDisplay src={DATA[current].img} alt={DATA[current].title} />
+        <ImageDisplay
+          src={DATA[current].img}
+          alt={DATA[current].title}
+          frameType={DATA[current].frameType}
+        />
       </div>
     </div>
   );
