865a5a151361bd649c65f21122141b3ccea68d0e
Nazmul Alom <nazmul@upsurge.in>
2022-11-28 17:57:08 +0600

    vertical image frame svgs position fixed
    Former-commit-id: 72269d81624616839897a35fb21c4c51f08de823 [formerly 2cda5983d9348985e1fbe5a6bfa5c849b2d03625]
    Former-commit-id: 129305225835f834c8b19e6b5510bb9704b49ca6

--- src/components/Benefits/ImageDisplay.js
+++ src/components/Benefits/ImageDisplay.js
@@ -28,18 +28,46 @@ export default function ImageDisplay({
       <img
         src={require("../../assets/whyUpsurge/yellow.svg").default.src}
         alt={"Yellow Star"}
+        style={{
+          position: "absolute",
+          top: "3%",
+          left: "87%",
+          height: "47px",
+          width: "47px",
+        }}
       />
       <img
         src={require("../../assets/whyUpsurge/blue.svg").default.src}
         alt={"Blue Circle"}
+        style={{
+          position: "absolute",
+          top: "25%",
+          left: "4%",
+          height: "60px",
+          width: "60px",
+        }}
       />
       <img
         src={require("../../assets/whyUpsurge/red.svg").default.src}
         alt={"Red Circle"}
+        style={{
+          position: "absolute",
+          top: "75%",
+          left: "83%",
+          height: "30px",
+          width: "30px",
+        }}
       />
       <img
         src={require("../../assets/whyUpsurge/green.svg").default.src}
         alt={"Green Parabolla"}
+        style={{
+          position: "absolute",
+          top: "80%",
+          left: "2%",
+          height: "50px",
+          width: "50px",
+        }}
       />
       <div className={styles.layer1}>
         <div className={styles.layer2}>
