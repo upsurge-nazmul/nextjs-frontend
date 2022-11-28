a3874f71ce1e4c525a1d4a7480d069bff97c8d37
Nazmul Alom <nazmul@upsurge.in>
2022-11-28 18:12:25 +0600

    svgs placed for all type of frames
    Former-commit-id: 28ad50d586056394f5e62b6f57c1d7be53ae44ef [formerly 2120779a76a1596e26131f40346d69d7fa09f4c0]
    Former-commit-id: 0e6a605b378830d34cc410e30cfbbe7725ce843e

--- src/components/Benefits/ImageDisplay.js
+++ src/components/Benefits/ImageDisplay.js
@@ -28,46 +28,118 @@ export default function ImageDisplay({
       <img
         src={require("../../assets/whyUpsurge/yellow.svg").default.src}
         alt={"Yellow Star"}
-        style={{
-          position: "absolute",
-          top: "3%",
-          left: "87%",
-          height: "47px",
-          width: "47px",
-        }}
+        style={
+          frameType === "vertical"
+            ? {
+                position: "absolute",
+                top: "3%",
+                left: "87%",
+                height: "47px",
+                width: "47px",
+              }
+            : frameType === "horizontal"
+            ? {
+                position: "absolute",
+                top: "-5%",
+                left: "55%",
+                height: "40px",
+                width: "40px",
+              }
+            : {
+                position: "absolute",
+                top: "15%",
+                left: "75%",
+                height: "40px",
+                width: "40px",
+              }
+        }
       />
       <img
         src={require("../../assets/whyUpsurge/blue.svg").default.src}
         alt={"Blue Circle"}
-        style={{
-          position: "absolute",
-          top: "25%",
-          left: "4%",
-          height: "60px",
-          width: "60px",
-        }}
+        style={
+          frameType === "vertical"
+            ? {
+                position: "absolute",
+                top: "25%",
+                left: "4%",
+                height: "60px",
+                width: "60px",
+              }
+            : frameType === "horizontal"
+            ? {
+                position: "absolute",
+                top: "20%",
+                left: "4%",
+                height: "25px",
+                width: "25px",
+              }
+            : {
+                position: "absolute",
+                top: "50%",
+                left: "5%",
+                height: "25px",
+                width: "25px",
+              }
+        }
       />
       <img
         src={require("../../assets/whyUpsurge/red.svg").default.src}
         alt={"Red Circle"}
-        style={{
-          position: "absolute",
-          top: "75%",
-          left: "83%",
-          height: "30px",
-          width: "30px",
-        }}
+        style={
+          frameType === "vertical"
+            ? {
+                position: "absolute",
+                top: "75%",
+                left: "83%",
+                height: "30px",
+                width: "30px",
+              }
+            : frameType === "horizontal"
+            ? {
+                position: "absolute",
+                top: "88%",
+                left: "35%",
+                height: "30px",
+                width: "30px",
+              }
+            : {
+                position: "absolute",
+                top: "80%",
+                left: "10%",
+                height: "30px",
+                width: "30px",
+              }
+        }
       />
       <img
         src={require("../../assets/whyUpsurge/green.svg").default.src}
         alt={"Green Parabolla"}
-        style={{
-          position: "absolute",
-          top: "80%",
-          left: "2%",
-          height: "50px",
-          width: "50px",
-        }}
+        style={
+          frameType === "vertical"
+            ? {
+                position: "absolute",
+                top: "80%",
+                left: "2%",
+                height: "50px",
+                width: "50px",
+              }
+            : frameType === "horizontal"
+            ? {
+                position: "absolute",
+                top: "94%",
+                left: "55%",
+                height: "50px",
+                width: "50px",
+              }
+            : {
+                position: "absolute",
+                top: "75%",
+                left: "85%",
+                height: "50px",
+                width: "50px",
+              }
+        }
       />
       <div className={styles.layer1}>
         <div className={styles.layer2}>
