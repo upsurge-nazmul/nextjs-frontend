4a385a74788e6e9be0a939697bd49b93a1e3fc86
Nazmul Alom <nazmul@upsurge.in>
2022-12-14 17:22:11 +0600

    values content added and structure created
    Former-commit-id: 407e70640236f674b5fd3ea9c411a7407bb93c9b [formerly 5ab757b025d5b08d48f9e442bb321b5933ea7f65]
    Former-commit-id: 6c501a637f8e87ca1c37f531c1032871f9fe9601

--- src/components/Home/Values.js
+++ src/components/Home/Values.js
@@ -1,5 +1,69 @@
 import styles from "../../styles/Home/values.module.scss";
 
+const DATA = [
+  {
+    id: 1,
+    title: "Understanding Money",
+    description: `Understand how ‘money’ works in the 21st century`,
+    color: "blue",
+  },
+  {
+    id: 2,
+    title: "Managing Money",
+    description: "Learn to wisely manage money & be fiscally responsible",
+    color: "green",
+  },
+  {
+    id: 3,
+    title: "Practice Investing",
+    description: "Practice profitable Investing & multiply their money",
+    color: "yellow",
+  },
+  {
+    id: 4,
+    title: "Develop a Growth Mindset",
+    description: "Develop an entrepreneurial & growth mindset",
+    color: "red",
+  },
+];
+
 export default function Values() {
-  return <div>Values</div>;
+  return (
+    <div className={styles.valuesArea}>
+      <div className={styles.valuesContent}>
+        <div className={styles.left}>
+          <img
+            src={require("../../assets/home/values/person.svg").default.src}
+            alt={"Person"}
+          />
+        </div>
+        <div className={styles.right}>
+          <div className={styles.rightTop}>
+            Through Upsurge, we aim to help kids with
+          </div>
+          <div className={styles.rightBottom}>
+            {DATA.map((item) => {
+              return (
+                <div className={styles.valueItem} key={item.id}>
+                  <div className={styles.valueBanner}>
+                    <img
+                      src={
+                        require(`../../assets/home/values/${item.color}.svg`)
+                          .default.src
+                      }
+                      alt={"Person"}
+                    />
+                  </div>
+                  <div className={styles.itemTitle}>{item.title}</div>
+                  <div className={styles.itemDesctiption}>
+                    {item.description}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
