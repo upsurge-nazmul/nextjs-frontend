eabf3fe8e608579fca46af6cc29edf9ec2e61010
Alok <alok@upsurge.in>
2022-12-15 16:00:56 +0530

    build fix
    Former-commit-id: 11e841863259450446670213519e7d235597d59c [formerly 942e83d3eb2499148d3994c65af1202ad8043e89]
    Former-commit-id: 1e925c98a9ae1454d501bf418365e60f4df1c271

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -28,15 +28,15 @@ function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png?v=1.5">
-        <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png?v=1.5">
-        <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png?v=1.5">
-        <link rel="mask-icon" href="/public/safari-pinned-tab.svg?v=1.5" color="#17d1bc">
-        <link rel="shortcut icon" href="/public/favicon.ico?v=1.5">
-        <meta name="msapplication-TileColor" content="#17d1bc">
-        <meta name="msapplication-TileImage" content="/public/mstile-144x144.png?v=1.5">
-        <meta name="msapplication-config" content="/public/browserconfig.xml?v=1.5">
-        <meta name="theme-color" content="#ffffff">
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png?v=1.5" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png?v=1.5" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png?v=1.5" />
+        <link rel="mask-icon" href="/public/safari-pinned-tab.svg?v=1.5" color="#17d1bc" />
+        <link rel="shortcut icon" href="/public/favicon.ico?v=1.5" />
+        <meta name="msapplication-TileColor" content="#17d1bc" />
+        <meta name="msapplication-TileImage" content="/public/mstile-144x144.png?v=1.5" />
+        <meta name="msapplication-config" content="/public/browserconfig.xml?v=1.5" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <MainContextProider>
         <HMSRoomProvider>
