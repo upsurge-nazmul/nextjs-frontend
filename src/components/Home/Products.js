6d760b57f13d6ee12fd362451918ff8c01dd766e
Nazmul Alom <nazmul@upsurge.in>
2022-12-19 17:28:33 +0600

    products icons updated
    Former-commit-id: 412b63f7d5e184a4a915ca014e58843b2b9a5cfd [formerly 0409cccac97e737727828fc1261d8d989ee0381b]
    Former-commit-id: 25fd45a1b2d2b38a5015ae15f740c112cb42e2b9

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "../../styles/Home/products.module.scss";
+import IconWithBg from "../SVGcomponents/IconWithBG";
 
 const DATA = [
   {
@@ -9,6 +10,8 @@ const DATA = [
     details:
       "Interactive gamified lessons that cover fundamentals of entrepreneurship & personal finance, and include activities, games, stories and quizzes.",
     side: "left",
+    iconClr: "#4066eb",
+    iconBorder: "#c6d2ff",
   },
   {
     sn: 2,
@@ -17,6 +20,8 @@ const DATA = [
     details:
       "A platform of our proprietary games where children can get hands-on experience in applying concepts to games and competing with other students.",
     side: "right",
+    iconClr: "#17d1bc",
+    iconBorder: "#91fed1",
   },
   {
     sn: 3,
@@ -25,6 +30,8 @@ const DATA = [
     details:
       "Fun & rewarding way for parents to reinforce good financial behaviors by assigning jobs and rewarding children with UniCoins!",
     side: "left",
+    iconClr: "#fdcc03",
+    iconBorder: "#feefad",
   },
   {
     sn: 4,
@@ -33,6 +40,8 @@ const DATA = [
     details:
       "Live online & offline workshops, designed and delivered by our experts, on entrepreneurship and personal finance.",
     side: "right",
+    iconClr: "#ff7f4c",
+    iconBorder: "#ffebcd",
   },
   {
     sn: 5,
@@ -41,6 +50,8 @@ const DATA = [
     details:
       "Offline games & competitions to give children the opportunities to gain recognition, build their profiles for higher studies & gain competitive capital.",
     side: "left",
+    iconClr: "#ff6263",
+    iconBorder: "#ffc8c8",
   },
   {
     sn: 6,
@@ -49,6 +60,8 @@ const DATA = [
     details:
       "Range of book-sets & board games, focused on building modern skills & knowledge, with integrated AR features for families to enjoy together.",
     side: "right",
+    iconClr: "#69dbf2",
+    iconBorder: "#c7ebff",
   },
 ];
 
@@ -60,13 +73,15 @@ function ProductCard({ product, actionHandler }) {
       onMouseLeave={() => actionHandler("default")}
     >
       <div className={styles.icon}>
-        <img
-          src={
-            require(`../../assets/home/products/${product.id}.svg`).default.src
-          }
-          alt={product.title}
-          className={styles.iconImage}
-        />
+        <IconWithBg bgClr={product.iconClr} borderClr={product.iconBorder}>
+          <img
+            src={
+              require(`../../assets/home/products/${product.id}.svg`).default
+                .src
+            }
+            alt={product.title}
+          />
+        </IconWithBg>
       </div>
       <div className={styles.content}>
         <div className={styles.productTitle}>{product.title}</div>
