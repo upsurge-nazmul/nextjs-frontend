11a94c47281e566fd7f6c2858c2486fc980ade8c
Alok <alok@upsurge.in>
2023-01-18 05:28:22 +0530

    hide old kq cards
    Former-commit-id: 7fea3d27846390e76273ff0162d36c5f0b18daa7 [formerly c01e2c8b2d387eae9d544d7bf7b212f5d86e4b6f] [formerly 76f01308dfac955354a6e23f6f4fd6cf1baedd32 [formerly 2ba96225156bc98e8b9cd00bd38f2066f2844db9]]
    Former-commit-id: 8029cc1b2a9f603a5f71cedb21c993767e2f12af [formerly 31f8bdc7bce0243e7b50a46acfba8848f7ecebcd]
    Former-commit-id: 5109b54608cb7f37577ab767bfcbc6774b09f2e3

--- src/pages/dashboard/k/quest/index.js
+++ src/pages/dashboard/k/quest/index.js
@@ -51,22 +51,22 @@ export default function KnowledgeQuest({ userData, questData }) {
         <div className={styles.mainContent} id="quest-main">
           <p style={{fontSize:"large",fontWeight:"bold"}}>Delightful journeys with games & interactive courses - and rewards!</p>
            {/* <Tabs list={QUEST_TYPES} current={tab} setCurrent={setTab} /> */}
-          <HeadArea
+          {/* <HeadArea
             data={questData}
             tab={tab}
             handleCardClick={handleCardClick}
-          /> 
+          />  */}
           <MapHeadArea 
           data={questData}
           tab={tab}
           handleCardClick={handleCardClick}
           />
-          <MainSection
+          {/* <MainSection
             data={questData}
             handleCardClick={handleCardClick}
             QUEST_TYPES={QUEST_TYPES}
             tab={tab}
-          />
+          /> */}
         </div>
       </div>
     </div>
