125dfeab576919aa51371028f0a4c855e493672a
Alok <alok@upsurge.in>
2023-01-18 09:31:38 +0530

    update images
    Former-commit-id: fcbff9da2dc543070c9fd6cd98c4158807578fb1 [formerly fdbb9591da7c9313d9805982e6fb5aec6851c4c0] [formerly aae607fdaef9fecba674b18ef868570c3059cca3 [formerly 17e449ef517e6fedfe202e19062f249b2e043f29]]
    Former-commit-id: 1fcdf1852034a8c27916bce18db64fc5a920504d [formerly a78323bed8e8242e71d87603649add32c2f8735a]
    Former-commit-id: c15e7a77250c61401d92bbdd585daabf3de0a31d

--- src/components/Home/Benefits.js
+++ src/components/Home/Benefits.js
@@ -9,7 +9,7 @@ const DATA = [
     title: "Kickstart your child’s financial education journey",
     description:
       "Financial literacy is a life-skill and the earlier your children start, the easier it will be for them to achieve their financial goals. Through our games, courses and live classes, all designed by experts, children will understand money, saving, investing and entrepreneurship like never before!",
-    img: "https://upsurgevideoassets.s3.ap-south-1.amazonaws.com/images/benefit1+(1).png",
+    img: "https://imgcdn.upsurge.in/images/landing/benefits-stock/1.jpg",
     frameType: "vertical",
   },
   {
@@ -17,7 +17,7 @@ const DATA = [
     title: "Learning by doing",
     description:
       "We believe that learning is most effective when it’s through play! Our games, activities and quizzes will instill healthy money habits that will last a lifetime.",
-    img: "https://upsurgevideoassets.s3.ap-south-1.amazonaws.com/images/benefit2+(1).png",
+    img: "https://imgcdn.upsurge.in/images/landing/benefits-stock/2.jpg",
     frameType: "horizontal",
   },
   {
@@ -25,7 +25,7 @@ const DATA = [
     title: "Developing entrepreneurial mindsets.",
     description:
       "Equip your children with the necessary skills and knowledge to understand and evaluate the fundamentals of business and starting-up, and (hopefully) be the next unicorn founder.",
-    img: "https://imgcdn.upsurge.in/images/benefit3.png",
+    img: "https://imgcdn.upsurge.in/images/landing/benefits-stock/3.jpg",
     frameType: "default",
   },
   {
@@ -33,14 +33,14 @@ const DATA = [
     title: "Earn real rewards",
     description:
       "As adults, when we do any job well, we get rewarded. Then why should it be any different for children? Students get rewarded with Unicoins for jobs well done, which can be redeemed for books, games, vouchers and educational courses from your favorite brands.",
-    img: "https://imgcdn.upsurge.in/images/Maskz-Group.png",
+    img: "https://imgcdn.upsurge.in/images/landing/benefits-stock/4.jpg",
     frameType: "vertical",
   },
   {
     id: 5,
     title: "Develop 21st century skills & knowledge",
     description: `While jobs & roles change, the skills needed to succeed remain the same. Our offerings help children gain confidence & excel by developing their critical & analytical thinking and problem-solving skills.`,
-    img: "https://imgcdn.upsurge.in/images/benefit4.png",
+    img: "https://imgcdn.upsurge.in/images/landing/benefits-stock/5.jpg",
     frameType: "horizontal",
   },
 ];
