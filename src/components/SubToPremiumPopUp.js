0bd106b51a446a41df28c63be92f2adae1905568
Aaryansh1 <aaryanshsharma8@gmail.com>
2023-02-18 18:01:59 +0530

    SubToPremiumPopUp added
    Former-commit-id: a21ddda2d48fafed6b1b8f7ec6e61df55bde8203 [formerly ba3ca3f0bd67f4c4f65171eb1aaeaa3fb848f2b8] [formerly d4a23734c989eafe31c56645b11fa837a3b8068d [formerly 390ca8270631fc428c2f769efd0c33230bc584ee]]
    Former-commit-id: fb86813839058f4fbb458b094d83e197cc636586 [formerly a9b10c9a31a7766d3323b13f4903b1357b2a4d7f]
    Former-commit-id: e2809deb96debddaded13c074c08433047d62361

---
+++ src/components/SubToPremiumPopUp.js
@@ -0,0 +1,147 @@
+import { useRouter } from "next/router";
+import React from "react";
+import styles from "../styles/GeneralComponents/subToPremium.module.scss";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+
+function SubToPremiumPopUp({ setShowSubToPremium }) {
+  const router = useRouter();
+  console.log("Showing");
+  return (
+    <div className={styles.subToPremium}>
+      <div
+        className={styles.background}
+        onClick={() => {
+          setShowSubToPremium(false);
+        }}
+      ></div>
+      <div className={styles.block}>
+        <div className={styles.cross} onClick={() => setShowSubToPremium(false)}>
+          <CancelOutlinedIcon className={styles.icon} />
+        </div>
+        <h2>Uh-Oh!</h2>
+        <h3>This feature is only available to the Premium Users.</h3>
+        <p>
+            Upgrade today to premium and enjoy {/* Change the premium family to something more catchy*/}
+        </p>
+        <div className={styles.section}>
+            <div className={styles.sectionLeft}>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/educational_games.svg")
+                      .default.src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                  />
+                12+ Educational Games
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/bonus_unicoins_red.svg").default
+                      .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                />
+                5 Flagship Games
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/habit_builder.svg").default
+                      .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                />
+               5 Workshop Invites 
+               <p className={styles.small_fontsize}>
+               (Worth ₹5000)
+               </p>
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/bonus_unicoins_red.svg").default
+                    .src
+                  }
+                  alt="Avatar Sets"
+                  className={styles.sectionItemImage}
+                  />
+                Avatar Sets
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/partner_rewards.svg").default
+                      .src
+                  }
+                  alt="Certificate Games"
+                  className={styles.sectionItemImage}
+                />
+                Certificates
+              </div>
+            </div>
+            <div className={styles.sectionRight}>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/kq_red.png").default
+                    .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                />
+                20 Knowledge Quests
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/partner_rewards.svg").default
+                    .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                  />
+                Unlimited Voucher Redemption and 10000 Bonus Unicoins.
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/bonus_unicoins_red.svg")
+                      .default.src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                />
+                Free Access to events and Challenges - Win rewards worth ₹25,000 every month
+              </div>
+              <div className={styles.sectionItem}>
+                <img
+                  src={
+                    require("../assets/pricing/upsurge_goodie.svg").default
+                      .src
+                    }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                />
+                upsurge Goodie Bag
+              </div>
+            </div>
+          </div>
+        <div
+        className={styles.button}
+          onClick={() => {
+            router.push("/pricing");
+          }}
+        >
+          Upgrade Now!
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SubToPremiumPopUp;
