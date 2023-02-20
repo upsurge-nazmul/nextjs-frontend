07fee5209a71b2fb843f03b398152694111eeada
Nazmul Alom <nazmul@upsurge.in>
2023-02-20 18:56:52 +0600

    invoice page added
    Former-commit-id: 4cf0e88a8e7761f29c3df53efd46bfc2ee070f08 [formerly 988e44ba8e693439f443bc1838b998ce6b6f5e23] [formerly a645e839bd34bb260400d3b822df9511a851e29f [formerly fc2b7fe54121cc776d1cfc981b37b69118e10b6c]]
    Former-commit-id: 7a30757702ef9936252f86460daf78694803cde8 [formerly 2c0f65bca960a8054496b19e11493ba10238e64a]
    Former-commit-id: b9b43e1876b0ac80230c366b290e41f1a0d4dad4

---
+++ src/pages/payments/invoice.js
@@ -0,0 +1,167 @@
+import { useRouter } from "next/dist/client/router";
+import React, { useEffect, useRef, useState } from "react";
+import LoginApis from "../../actions/apis/LoginApis";
+import PaymentsApi from "../../actions/apis/PaymentsApi";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Home/Footer";
+import Fb from "../../components/SVGcomponents/Fb";
+import Insta from "../../components/SVGcomponents/Insta";
+import LinkedIN from "../../components/SVGcomponents/LinkedInSvg";
+import styles from "../../styles/waitlist/waitlist.module.scss";
+import ConfirmInvoice from "../../components/ConfirmInvoice";
+import { useReactToPrint } from "react-to-print";
+import PageTitle from "../../components/PageTitle";
+
+export default function Subscribed({ invoice_data, userdatafromserver }) {
+  const [showauth, setshowauth] = useState(false);
+  const [showpopup, setshowpopup] = useState(false);
+  const [stickyheader, setstickyheader] = useState(false);
+  const [invoiceData, setInvoiceData] = useState();
+  const pdfRef = useRef(null);
+  const router = useRouter();
+  const { amount, bundle } = router.query;
+
+  useEffect(() => {
+    const invoiceModel = {
+      id: `INV-${Math.random * 1000000}`,
+      timestamp: new Date(),
+      recipient_name: userdatafromserver.user_name,
+      recipient_email: userdatafromserver.email,
+      recipient_address: userdatafromserver.address,
+      description: bundle,
+      quantity: 1,
+      amount: amount,
+    };
+    setInvoiceData(invoiceModel);
+  }, []);
+
+  useEffect(() => {
+    const handlescroll = () => {
+      if (window.scrollY > 0) {
+        setstickyheader(true);
+      } else {
+        setstickyheader(false);
+      }
+    };
+    window.addEventListener("scroll", handlescroll);
+    return () => window.removeEventListener("scroll", handlescroll);
+  }, []);
+
+  const handlerSave = useReactToPrint({
+    content: () => pdfRef.current,
+  });
+
+  console.log("%%%%%%%%%%%", userdatafromserver, router.query);
+
+  return (
+    <div className={styles.waitlist}>
+      <PageTitle />
+      <Header
+        stickyheader={stickyheader}
+        showauth={showauth}
+        setshowauth={setshowauth}
+        setshowpopup={setshowpopup}
+        showpopup={showpopup}
+      />
+      <div className={styles.container}>
+        <div className={styles.green}></div>
+        <div className={styles.white}></div>
+        <div className={styles.ball4}></div>
+        <div className={styles.yellow}></div>
+        <p className={styles.heading3}>Subscription Confirmed </p>
+        <div className={styles.line}></div>
+
+        <p className={styles.heading2}>Thank you for subscribing!</p>
+        <p className={styles.msg}>
+          Your payment is successful you can check the invoice and download or
+          print the invoice.
+        </p>
+
+        <div className={styles.invoiceContainer}>
+          <div className={styles.invoiceWrapper}>
+            {invoiceData && <ConfirmInvoice data={invoiceData} ref={pdfRef} />}
+          </div>
+          <div className={styles.btnContainer}>
+            <button onClick={() => handlerSave()} className={styles.btn}>
+              Print Invoice
+            </button>
+            {/* <button onClick={() => handlerSave()} className={styles.btn}>
+              Download Invoice
+            </button> */}
+          </div>
+        </div>
+        <p className={styles.subheading}>
+          To stay up to date at all times, follow us on.
+        </p>
+        <div className={styles.socials}>
+          <a
+            href="https://www.facebook.com/upsurgeindia/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Fb className={styles.social} />
+          </a>
+          <a
+            href="https://www.instagram.com/upsurge.in/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Insta className={styles.social} />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/upsurgeindia/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedIN className={styles.socialyt} />
+          </a>
+          {/* <Fb className={styles.social} />
+          <Twitter className={styles.social} alt="" />
+          <Insta className={styles.social} />
+          <YtSvg className={styles.socialyt} />
+          <LinkedIN className={styles.socialyt} /> */}
+        </div>
+        <div className={styles.goback} onClick={() => router.push("/")}>
+          Go Back
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export async function getServerSideProps({ params, req }) {
+  let token = req.cookies.accesstoken;
+  let msg = "";
+  if (token) {
+    let response = await LoginApis.checktoken({
+      token: token,
+    });
+    if (response && !response.data.success) {
+      msg = response.data.msg;
+      return {
+        props: { isLogged: false, msg },
+        redirect: {
+          permanent: false,
+          destination: "/?err=02",
+        },
+      };
+    } else {
+      return {
+        props: {
+          isLogged: true,
+          userdatafromserver: response.data.data,
+          token: token,
+        },
+      };
+    }
+  } else {
+    return {
+      props: { isLogged: false, msg: "cannot get token" },
+      redirect: {
+        permanent: false,
+        destination: "/?err=01",
+      },
+    };
+  }
+}
