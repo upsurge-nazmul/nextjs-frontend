b0b401c96e81ce0da0b38faedbdfd5f4fadffe8b
Aaryansh1 <aaryanshsharma8@gmail.com>
2023-02-28 20:47:32 +0530

    pricing page update
    Former-commit-id: 6feb2005c572b699f355322ddfc7ab6d365821cd [formerly 11ba169e53b64b584d49ff13f525e47b3b845eae] [formerly 829c7a932d41776dbf6367c7e4d3972c02a6c4ca [formerly 3ce7908a8893e8fa31ece754ba01985d97cf2f9f]]
    Former-commit-id: 7347cd1c9fdbe76c114fd57b5e1830cfa2dcf877 [formerly b211a7cf0c1dd27fa2cf65d61917e6cb6cdcaa01]
    Former-commit-id: cb9ef954902f004af75077a2d3f719568fcf6b6e

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -9,6 +9,7 @@ import Curve1 from "../../components/SVGcomponents/Curve1";
 import Curve2 from "../../components/SVGcomponents/Curve2";
 import { MainContext } from "../../context/Main";
 import styles from "../../styles/Pricing/pricing.module.scss";
+import PaymentsApi from "../../actions/apis/PaymentsApi";
 
 const PREMIUM_PRICE = 1799;
 
@@ -22,8 +23,18 @@ export default function Pricing() {
   const [premiumPrice, setPremiumPrice] = useState();
 
   const { theme } = useContext(MainContext);
+  const [plans, setPlans] = useState();
+
+  async function fetchPlans() {
+    const res = await PaymentsApi.getPlans();
+    if (res && res.data && res.data.success) {
+      console.log(res.data.data);
+      setPlans(res.data.data);
+    }
+  }
 
   useEffect(() => {
+    fetchPlans();
     const handlescroll = () => {
       if (window.scrollY > 1) {
         setstickyheader(true);
@@ -77,102 +88,57 @@ export default function Pricing() {
             Power up your child’s journey in the finance world with upsurge
             <u className={styles.heading_underline}>Premium.</u>
           </div>
-          <div className={styles.sectionContainer}>
-            <div className={styles.contentPlanFreemium}>
-              <div className={styles.contentTagFreemium}>Premium</div>
-              <div className={styles.section}>
-              <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/knowledge_quests.png")
-                        .default.src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  20 Knowledge Quests
-                </div>
-                <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/educational_games.svg")
-                        .default.src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  16 Educational Games
-                </div>
-                <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/bonus_unicoins.svg").default
-                        .src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  5 Flagship Game{" "}
-                  <p className={styles.small_fontsize}>(1000 hours)</p>
-                </div>
-                <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/habit_builder.svg").default
-                        .src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  Events and Challenges
-                </div>
-                <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/partner_rewards.svg")
-                          .default.src
-                      }
-                      alt="Avatar Sets"
-                      className={styles.sectionItemImage}
-                    />
-                    10,000 Bonus Unicoins
-                  </div>
-                <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/partner_rewards.svg")
-                        .default.src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  Redeem Vouchers
-                </div>
-                <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/leaderboard_prizes.svg")
-                        .default.src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  Leaderboard Access
-                </div>
-                <div className={styles.sectionItem}>
-                  <img
-                    src={
-                      require("../../assets/pricing/partner_rewards.svg")
-                        .default.src
-                    }
-                    alt="Education Games"
-                    className={styles.sectionItemImage}
-                  />
-                  Win monthly rewards worth <div className={styles.small_fontsize}><u><b>₹25,000/-</b></u></div>
-                </div>
-              </div>
-              <div className={styles.actionArea}>
-              <div className={styles.pricing}>
+          <div className={styles.subToPremium}>
+      <div className={styles.block}>
+        <div className={styles.section}>
+          <div className={styles.sectionLeft}>
+            <h3 className={styles.header}>Upsurge Premium</h3>
+            <p className={styles.smallfont}> for 6 months </p>
+            <p className={styles.subheading}>
+              Get access to premium quests and games
+            </p>
+            {/* Change the premium family to something more catchy*/}
+            <ul>
+              <li className={styles.sectionItem}>
+                {/* <img
+                  src={
+                    require("../assets/pricing/kq_red.png").default
+                    .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                /> */}
+                20 Knowledge Quests
+              </li>
+              <li className={styles.sectionItem}>
+                {/* <img
+                src={
+                  require("../assets/pricing/educational_games.svg").default.src
+                }
+                alt="Education Games"
+                className={styles.sectionItemImage}
+              /> */}
+                16 Educational Games
+              </li>
+              <li className={styles.sectionItem}>
+                {/* <img
+                  src={
+                    require("../assets/pricing/bonus_unicoins_red.svg").default
+                      .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                /> */}
+                5 Flagship Games (1000 hours)
+              </li>
+              <li className={styles.sectionItem}>Events and Challenges</li>
+              <li className={styles.sectionItem}>10,000 Bonus Unicoins</li>
+              <li className={styles.sectionItem}>Redeem Vouchers</li>
+              <li className={styles.sectionItem}>
+                Win monthly rewards worth ₹25,000/-
+              </li>
+            </ul>
+            <div className={styles.pricing}>
               <div className={styles.bottom}></div>
               <div className={styles.pricingSectionTop}>
                 <p className={styles.slashedPrice}>₹2499</p>{" "}
@@ -182,148 +148,110 @@ export default function Pricing() {
                 <p className={styles.smallfont}>(limited period offer)</p>
               </div>
             </div>
-                <button
-                  className={styles.actionButton}
-                  onClick={() => {
-                    setAuthMode("parent");
-                    setshowauth(true);
-                    setPremiumPrice(null);
-                  }}
-                >
-                  Subscribe to Premium
-                </button>
-              </div>
+            <div
+              className={styles.button}
+              onClick={() => {
+                console.log(plans[1].id)
+                setPremiumPrice(plans[1].id)
+                setAuthMode("parentChild")
+                setshowauth(true)
+                //router.push(`/payments/stripe?plan_id=${plans[1].id}`);
+              }}
+            >
+              {`Subscribe to Premium`}
             </div>
-            <div className={styles.contentPlanPremium}>
-              <div className={styles.contentTagPremium}>Best Value</div>
-              <div className={styles.section}>
-                <div className={styles.sectionLeft}>
-                <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/knowledge_quests.png")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                    20 Knowledge Quests
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/educational_games.svg")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                    16 Educational Games
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/bonus_unicoins.svg")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                    5 Flagship Games <p className={styles.small_fontsize}>(1000 hours)</p>
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/habit_builder.svg")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                    Events and Challenges
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/partner_rewards.svg")
-                          .default.src
-                      }
-                      alt="Unicoins"
-                      className={styles.sectionItemImage}
-                    />
-                    20,000 Bonus Unicoins
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/partner_rewards.svg")
-                          .default.src
-                      }
-                      alt=""
-                      className={styles.sectionItemImage}
-                    />
-                    Redeem Vouchers
-                  </div>
-                </div>
-                <div className={styles.sectionRight}>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/leaderboard_prizes.svg")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                   Win monthly rewards worth <div className={styles.small_fontsize}><u><b>₹1,00,000/-</b></u></div>
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/partner_rewards.svg")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                  Invitation to Online Workshops
-                  </div>
-                  <div className={styles.sectionItem}>
-                    <img
-                      src={
-                        require("../../assets/pricing/partner_rewards.svg")
-                          .default.src
-                      }
-                      alt="Education Games"
-                      className={styles.sectionItemImage}
-                    />
-                    <div className={styles.sectionItemGolden}>Upsurge branded goodies(worth ₹2,500)</div>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.actionArea}>
-              <div className={styles.pricing}>
+          </div>
+          <div className={styles.verticleLine}></div>
+          <div className={styles.sectionRight}>
+            <div className={styles.bestValue}>Best Value</div>
+            <h3 className={styles.header}>Upsurge Premium</h3>
+            <p className={styles.smallfont}> for 12 months </p>
+            <p className={styles.subheading}>
+              Get access to premium quests and games
+            </p>
+            <ul>
+              <li className={styles.sectionItem}>
+                {/* <img
+                  src={
+                    require("../assets/pricing/kq_red.png").default
+                    .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                /> */}
+                20 Knowledge Quests
+              </li>
+              <li className={styles.sectionItem}>
+                {/* <img
+                src={
+                  require("../assets/pricing/educational_games.svg").default.src
+                }
+                alt="Education Games"
+                className={styles.sectionItemImage}
+              /> */}
+                16 Educational Games
+              </li>
+              <li className={styles.sectionItem}>
+                {/* <img
+                  src={
+                    require("../assets/pricing/bonus_unicoins_red.svg").default
+                      .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                /> */}
+                5 Flagship Games (1000 hours)
+              </li>
+              <li className={styles.sectionItem}>Events and Challenges</li>
+              <li className={styles.sectionItem}>20,000 Bonus Unicoins</li>
+              <li className={styles.sectionItem}>Redeem Vouchers</li>
+              <li className={styles.sectionItem}>
+                Win monthly rewards worth ₹1,00,000/-
+              </li>
+              <li className={styles.sectionItem}>
+                Invites to Online Workshops.
+              </li>
+              <li className={styles.sectionItemGolden}>
+                {/* <img
+                  src={
+                    require("../assets/pricing/upsurge_goodie.svg").default
+                    .src
+                  }
+                  alt="Education Games"
+                  className={styles.sectionItemImage}
+                /> */}
+                <p>upsurge branded goodies (worth ₹2,500)</p>
+              </li>
+            </ul>
+            <div className={styles.pricing}>
               <div className={styles.pricingSectionTop}>
                 <p className={styles.slashedPrice}>₹4799</p>{" "}
-                <p className={styles.actualPrice}>₹2499</p>{" "}
+                <p className={styles.actualPriceGolden}>₹2499</p>{" "}
               </div>
               <div className={styles.pricingSectionBottom}>
                 <p className={styles.smallfont}>(limited period offer)</p>
               </div>
             </div>
-                <button
-                  className={styles.actionButtonPremium}
-                  onClick={() => {
-                    // setAuthMode("parent");
-                    // setshowauth(true);
-                    // setPremiumPrice(PREMIUM_PRICE);
-                    // handleBuyPremium();
-                  }}
-                >
-                  Subscribe to Premium
-                </button>
-              </div>
+            <div
+              className={styles.button}
+              onClick={async() => {
+                setPremiumPrice(plans[0].id)
+                setAuthMode("parentChild")
+                setshowauth(true)
+              }}
+              >
+              {`Subscribe to Premium`}
             </div>
+          </div>
+        </div>
+        <p className={styles.clickable} onClick={() => {
+          setPremiumPrice(null)
+          setAuthMode("parentChild")
+          setshowauth(true)
+          }}>
+          <u>Try free version</u>
+        </p>
+        </div>
           </div>
         </div>
       </div>
