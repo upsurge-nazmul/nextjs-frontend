08caa3a6c3189eae296733a99b485fe19b2b76c9
Alok <alok@upsurge.in>
2023-03-01 14:32:30 +0530

    Revert "remove hms room provider"
    This reverts commit 91df64ec431b5804a711ed6cd77f0e3355addc58 [formerly 2284e6998c902725744bda60d161f57de3f7fc0b] [formerly f562f850746a75490c29ac52d3fa2d3ad8717198 [formerly c2841dc56708d2bd371e888574791b8752f99f44]] [formerly b134c0857a22d284813133adb70fc7658ad40343 [formerly c77ded49cf72105fa4d8e461d1c0ea62003f741c] [formerly 5d7a8365a08d7520f796745046b4c1bcf8c28f29 [formerly 68f2cfc4be4ab75c0eb9aa7e427ba7b792ada7f2]]].
    
    
    Former-commit-id: 3b2204c0d66c60fa5300ff51233d1671025fb132 [formerly 7b4b1044c90c4f00130f970dce1caf169872021d] [formerly 20cff73b36e0142614b97675d27b2270a5172340 [formerly b2ff562a362b3b1e2d63acd2bb879301e4e2a39e]]
    Former-commit-id: 89a2ccbba8e9f841fd6ff23a3598b19926249b4f [formerly da48cf70e275df3ba43c6eeebfb3fc84e37ff1ca]
    Former-commit-id: 004efd1d4c15ad36feca3fb5c9f8ec36a53b3a74

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,9 @@ import "../styles/globals.scss";
 import { useEffect } from "react";
 import { useRouter } from "next/dist/client/router";
 import NextNprogress from "nextjs-progressbar";
+import Head from 'next/head';
 import NotificationComponent from "../components/NotificationComponent";
-// import { HMSRoomProvider } from "@100mslive/react-sdk";
+import { HMSRoomProvider } from "@100mslive/react-sdk";
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
@@ -22,11 +23,11 @@ function MyApp({ Component, pageProps }) {
 
   return (
       <MainContextProider>
-        {/* <HMSRoomProvider> */}
+        <HMSRoomProvider>
           <NextNprogress color="#4166EB" />
           <Component {...pageProps} />
           <NotificationComponent />
-        {/* </HMSRoomProvider> */}
+        </HMSRoomProvider>
       </MainContextProider>
   );
 }
