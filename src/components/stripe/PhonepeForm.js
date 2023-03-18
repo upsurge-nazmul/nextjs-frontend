3574f1eb73e2ad6798dc98a8fc479b66a2ff560b
Pratham <pratham.sharma2105@gmail.com>
2023-03-18 18:46:10 +0530

    phonepe payment init
    Former-commit-id: 86d02bb0148a0a90b96246843d203146b3d71bd1 [formerly 4bf3a641defa218d84b5d13c6fc7bfd714604b93] [formerly f170963c8711c113fab3bca8f16582a6e25be16c [formerly 67af156857fd1675ec83904ec40ca3f230d4cc3e]]
    Former-commit-id: cdcf15b93ce2ac0f3a486a963dbff5863a50bb36 [formerly a6d5b2811c4b42a208e54203bb833d16b5b668cd]
    Former-commit-id: 9e720198481891e5eb0463b65e3921d61cbc1457

--- src/components/stripe/PhonepeForm.js
+++ src/components/stripe/PhonepeForm.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import PaymentsApi from "../../actions/apis/PaymentsApi";
-import axios from "axios";
-import crypto from "crypto";
+import { useRouter } from "next/router";
 
-export default function PhonepeForm({ planId }) {
+export default function PhonepeForm({ planId, userdata }) {
   const [plan, setPlan] = useState();
-
+  const router = useRouter();
   async function fetchPlan() {
     const res = await PaymentsApi.getPlans({ plan_id: planId });
     if (res && res.data && res.data.success) {
@@ -13,95 +12,23 @@ export default function PhonepeForm({ planId }) {
     }
   }
 
-  function computeSHA256(lines) {
-    const hash = crypto.createHash("sha256");
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i].trim(); // remove leading/trailing whitespace
-      if (line === "") continue; // skip empty lines
-      hash.write(line); // write a single line to the buffer
-    }
-
-    return hash.digest("base64"); // returns hash as string
-  }
-
   async function paymentInit() {
-    const data = Buffer.from(
-      `{
-      "merchantId": "UPSURGEUAT",
-      "merchantTransactionId": "ef310ggkgkjjkjhjkc0a-b984-11ed-afa1-0242ac120002PRATHAM",
-      "merchantUserId": "d780c37f-55e2-4aff-ghjgfjsgsjjsjaaae-1d1ba93e0440PRATHAM",
-      "amount": 10000,
-      "redirectUrl": "http://localhost:3000/test",
-      "redirectMode": "POST",
-      "callbackUrl": "http://localhost:3000/callback-url",
-      "mobileNumber": "9999999999",
-      "paymentInstrument": {
-        "type": "TOKEN"
-      }
-    }`
-    ).toString("base64");
-
-    const hash = crypto
-      .createHash("sha256")
-      .update(data + "/pg/v1/pay89efe538-f358-4a7f-959f-1d6edf953bde")
-      .digest("hex");
-
-    const options = {
-      method: "POST",
-      url: "https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/pay",
-      data: {
-        request: data,
-      },
-      mode: "no-cors",
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-        "X-VERIFY": `${hash}###1`,
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    const result = await PaymentsApi.getPhonePe({
+      userId: userdata.user_id,
+      plan_id: planId,
+      hostURL: window.location.origin,
+    });
+    console.log({ result });
+    if (result && result.data) {
+      router.push(
+        result.data.redirectURL.data.instrumentResponse.redirectInfo.url
+      );
+    }
   }
 
   useEffect(() => {
     fetchPlan();
     paymentInit();
-  }, []);
-
-  useEffect(() => {
-    const xVerify =
-      crypto
-        .createHash("sha256")
-        .update(
-          "/pg/v1/status/UPSURGEUAT/OD620471739210623pay89efe538-f358-4a7f-959f-1d6edf953bde"
-        )
-        .digest("hex") + "###1";
-    const options = {
-      method: "GET",
-      url: "https://cors-anywhere.herokuapp.com/https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/status/UPSURGEUAT/OD620471739210623",
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-        "X-VERIFY": xVerify,
-        "X-MERCHANT-ID": "UPSURGEUAT",
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
   }, []);
 
   return <div>Payment method</div>;
--- src/pages/payments/phonepe.js
+++ src/pages/payments/phonepe.js
@@ -1,9 +1,16 @@
 import { useRouter } from "next/dist/client/router";
+import { useContext, useEffect } from "react";
 import PhonepeForm from "../../components/stripe/PhonepeForm";
+import { MainContext } from "../../context/Main";
 
-export default function Phonepe() {
+export default function PhonepePage() {
   const router = useRouter();
+  const { userdata } = useContext(MainContext);
   const { plan_id } = router.query;
 
-  return <PhonepeForm planId={plan_id} />;
+  useEffect(() => {
+    console.log({ plan_id, userdata });
+  }, [userdata, plan_id]);
+
+  return <>{plan_id && <PhonepeForm planId={plan_id} userdata={userdata} />}</>;
 }
