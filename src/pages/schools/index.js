3c630dc050faea7257571872abc4b739a5614a26
Pratham <pratham.sharma2105@gmail.com>
2023-04-13 23:55:32 +0530

    created school page
    Former-commit-id: 2abf6241b1c211f92bafecd375f1714a79bd7902 [formerly 25f51273edb4eb060710b1a8a9368ea6be73d025] [formerly b2009e550d0e42869dff51e858e436b2a7527d4f [formerly 65abfa858968e23c78d15d4e91e9f17a93905e7a]]
    Former-commit-id: ced43d93b4aba2075a0491fa468a0223759fc114 [formerly 9cfbaa714b5b37161363e1dd1757b6e8e948560a]
    Former-commit-id: 7134b9f9023ccb655d482ab8533254a0bd197773

---
+++ src/pages/schools/index.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import SchoolHeroSection from "../../components/School/HeroSection";
+import PageTitle from "../../components/PageTitle";
+import Header from "../../components/Header/Header";
+import Toast from "../../components/Toast";
+import LeftPanel from "../../components/LeftPanel";
+import JoinUs from "../../components/Home/JoinUs";
+import Footer from "../../components/Home/Footer";
+import ProgramSection from "../../components/School/ProgramSection";
+import ReadMoreSection from "../../components/School/ReadMoreSection";
+import FinancialSection from "../../components/School/FinancialSection";
+import EntrepreneurshipSection from "../../components/School/EntrepreneurshipSection";
+import SkillsSection from "../../components/School/SkillsSection";
+import WhyChoose from "../../components/School/WhyChoose";
+import PartnerSchoolSection from "../../components/School/PartnerSchoolSection";
+import TestimonialsSection from "../../components/School/TestimonialsSection";
+import ContactUsSection from "../../components/School/ContactUsSection";
+
+function SchoolsPage() {
+  const [stickyheader, setstickyheader] = useState(false);
+  const [openLeftPanel, setOpenLeftPanel] = useState(false);
+  const [showwaitlistblock, setshowwaitlistblock] = useState(false);
+  const [showauth, setshowauth] = useState(false);
+  const [authmode, setauthmode] = useState("");
+  const [error, setError] = useState("");
+  const [showpopup, setshowpopup] = useState(false);
+  const [toastdata, settoastdata] = useState({
+    show: false,
+    type: "success",
+    msg: "",
+  });
+
+  useEffect(() => {
+    history.scrollRestoration = "manual";
+    const handlescroll = () => {
+      if (window.scrollY > 0) {
+        setstickyheader(true);
+      } else {
+        setstickyheader(false);
+      }
+    };
+    window.addEventListener("scroll", handlescroll);
+    return () => window.removeEventListener("scroll", handlescroll);
+  }, []);
+  return (
+    <>
+      <PageTitle />
+      <Header
+        setOpenLeftPanel={setOpenLeftPanel}
+        showauth={showauth}
+        stickyheader={stickyheader}
+        setshowauth={setshowauth}
+        authmode={authmode}
+        setauthmode={setauthmode}
+        settoastdata={settoastdata}
+        showpopup={showpopup}
+        setshowpopup={setshowpopup}
+      />
+      <Toast data={toastdata} />
+      <LeftPanel
+        openLeftPanel={openLeftPanel}
+        setOpenLeftPanel={setOpenLeftPanel}
+      />
+      <SchoolHeroSection />
+      <ProgramSection />
+      <ReadMoreSection />
+      <FinancialSection />
+      <EntrepreneurshipSection />
+      <SkillsSection />
+      <WhyChoose />
+      <PartnerSchoolSection />
+      <TestimonialsSection />
+      <ContactUsSection />
+      <JoinUs />
+      <Footer />
+    </>
+  );
+}
+
+export default SchoolsPage;
