74806053b91ca043fd6bc22de55eb5a395007a0c
Pratham <pratham.sharma2105@gmail.com>
2023-04-15 21:03:41 +0530

    redesigned events card
    Former-commit-id: 0e044b3f64876b37f7fbf818b6141202448e8414 [formerly 227aae4139ba7e503cb806eb83f47bb767725b0a] [formerly 20194f7c8395caeac61399ad4b6a7163eacc51d0 [formerly 83e3bdf25a684e075d9d40a70e6a2c76f92f486d]]
    Former-commit-id: e7e842c7c0ab2365c644bba1d07f5bf12f7fbb23 [formerly 4047b6bb8025f0390a0983e9354df8b91cd62208]
    Former-commit-id: c4c2a229a5c9950ce1fcd469cbc8d7e403178696

--- src/components/Card/ChallengesCard.js
+++ src/components/Card/ChallengesCard.js
@@ -8,22 +8,24 @@ export default function ChallengesCard({
 }) {
   return (
     <div className={styles.card}>
-      <img src={data.image} alt={data.name} className={styles.banner} />
-      <div className={styles.cardContent}>
-        <div className={styles.cardName}>{data.name}</div>
-        <div className={styles.eventType}>{data.eventType}</div>
-        <div className={styles.eventDate}>{data.eventDate}</div>
-      </div>
-      <div className={styles.actionArea}>
-        {data.eventOpen ? (
-          <button className={styles.registerButton}>Register Now</button>
-        ) : (
-          <button className={styles.comingButton}>Coming Soon</button>
-        )}
-        <button className={styles.detailsButton} onClick={handleSelect}>
-          See Details
-        </button>
-      </div>
+      <figure className={styles.cardThumbnail}>
+        <img src={data.image} alt={data.name} className={styles.banner} />
+        <div className={styles.cardContent}>
+          <div className={styles.cardName}>{data.name}</div>
+          {/* <div className={styles.eventType}>{data.eventType}</div>
+        <div className={styles.eventDate}>{data.eventDate}</div> */}
+          <div className={styles.actionArea}>
+            {data.eventOpen ? (
+              <button className={styles.registerButton}>Register Now</button>
+            ) : (
+              <button className={styles.comingButton}>Coming Soon</button>
+            )}
+            <button className={styles.detailsButton} onClick={handleSelect}>
+              See Details
+            </button>
+          </div>
+        </div>
+      </figure>
     </div>
   );
 }
--- src/components/Card/EventsCard.js
+++ src/components/Card/EventsCard.js
@@ -1,5 +1,20 @@
 import styles from "../../styles/cards/eventsCard.module.scss";
 
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export default function EventsCard({
   data,
   height = "15vh",
@@ -8,22 +23,30 @@ export default function EventsCard({
 }) {
   return (
     <div className={styles.card}>
-      <img src={data.image} alt={data.name} className={styles.banner} />
-      <div className={styles.cardContent}>
-        <div className={styles.cardName}>{data.name}</div>
-        <div className={styles.eventType}>{data.eventType}</div>
-        <div className={styles.eventDate}>{data.eventDate}</div>
-      </div>
-      <div className={styles.actionArea}>
-        {data.eventOpen ? (
-          <button className={styles.registerButton}>Register Now</button>
-        ) : (
-          <button className={styles.comingButton}>Coming Soon</button>
-        )}
-        <button className={styles.detailsButton} onClick={handleSelect}>
-          See Details
-        </button>
-      </div>
+      <figure className={styles.cardThumbnail}>
+        <img src={data.image} alt={data.name} className={styles.banner} />
+        <div className={styles.dataContainer}>
+          <p className={styles.date}>
+            {monthNames[new Date(data.expiry).getMonth()]}
+          </p>
+          <p>{new Date(data.expiry).getDate()}</p>
+        </div>
+        <div className={styles.cardContent}>
+          <div className={styles.cardName}>{data.name}</div>
+          <div className={styles.actionArea}>
+            {data.eventOpen ? (
+              <button className={styles.registerButton}>Register Now</button>
+            ) : (
+              <button className={styles.comingButton}>Coming Soon</button>
+            )}
+            <button className={styles.detailsButton} onClick={handleSelect}>
+              See Details
+            </button>
+          </div>
+          {/* <div className={styles.eventType}>{data.eventType}</div>
+        <div className={styles.eventDate}>{data.eventDate}</div> */}
+        </div>
+      </figure>
     </div>
   );
 }
