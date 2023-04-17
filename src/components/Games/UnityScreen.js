a7dca2b9ccd5e99592b847c59456a83c2eab6c47
Nazmul Alom <nazmul@upsurge.in>
2023-04-17 22:04:20 +0600

    unity webgl send received message functions added
    Former-commit-id: e687aaaa7c4842536cd9a9bfa174ae694721fc01 [formerly 09136935ca52be49ccac754fc883429ac4370efd] [formerly f7fc3dcc35db04761845084490193a2939d2eb84 [formerly 6d6d1ea0b2bcb1ffddca2bfa4d04c61d07a09df9]]
    Former-commit-id: 5abae897c1785e34366d4cffc0dc4c89e6a71137 [formerly 016e4bc67acf5dfa5f88620cdc09edefb761a501]
    Former-commit-id: 9878014ec1cb8f432f0d7fa48835114df2ea1161

--- src/components/Games/UnityScreen.js
+++ src/components/Games/UnityScreen.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import styles from "../../styles/Games/gameView.module.scss";
 import GameLoading from "./GameLoading";
 
 export default function UnityScreen({ data }) {
-  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+  const {
+    unityProvider,
+    isLoaded,
+    loadingProgression,
+    sendMessage, // (gameObjectName: string, methodName: string, parameter)
+    addEventListener, // (eventNamme: string, callback: (..params))
+    removeEventListener, // (eventNamme: string, callback: (..params))
+  } = useUnityContext({
     loaderUrl: data.loaderUrl,
     dataUrl: data.dataUrl,
     frameworkUrl: data.frameworkUrl,
     codeUrl: data.codeUrl,
   });
+
+  function handleSendMessage() {
+    sendMessage("GameController", "sendMessage", 100);
+  }
+
+  const handleReceiveMessage = useCallback((params) => {
+    console.log("params received", params);
+  });
+
+  useEffect(() => {
+    addEventListener("SetReceivedParams", handleReceiveMessage);
+    return () => {
+      removeEventListener("SetReceivedParams", handleReceiveMessage);
+    };
+  }, [addEventListener, removeEventListener, handleReceiveMessage]);
 
   return (
     <>
@@ -17,6 +39,7 @@ export default function UnityScreen({ data }) {
         unityProvider={unityProvider}
         className={isLoaded ? styles.gameScreen : styles.hidden}
       />
+      <button onClick={handleSendMessage}>Send Message</button>
       <div className={!isLoaded ? styles.loadingArea : styles.hidden}>
         <GameLoading percentage={loadingProgression} />
       </div>
