053d3c8c79487887c8af5e47dbbbe87c389bf767
Pratham <pratham.sharma2105@gmail.com>
2023-04-24 17:05:55 +0530

    created events page with components
    Former-commit-id: 1d15c40d3191c67cc5d385feae77d2abb9ea8223 [formerly 840b4991ab3d30baac08c333a0b107bf76ee182a] [formerly bfd9304be295a5db8bd0e9c85d1d82fa8619aef6 [formerly 54339be4de0a8cb62c7d5526df9a52351575dbd1]]
    Former-commit-id: c718238bf0259fec06226b5d5d452c2e2cd506bd [formerly 354f15c080f265d030f2290f9783ea4c37526a6f]
    Former-commit-id: 8d645e8fc1e901b1fe869d0fb323dc3fb8053732

---
+++ src/components/PreSignupEvents/EventsSection.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import styles from "../../styles/pre-signup-events/events.module.scss";
+import { useEffect } from "react";
+import EventsApis from "../../actions/apis/EventsApis";
+
+const EventsSection = () => {
+  const [data, setData] = useState([]);
+  const fetchAllEvents = async () => {
+    const res = await EventsApis.getAllInternShip();
+    console.log(res);
+    if (res && res.data && res.data.data) {
+      setData(res.data.data);
+    }
+  };
+  useEffect(() => {
+    fetchAllEvents();
+  }, []);
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Upcoming Events</h2>
+      <div className={styles.cardsContainer}>
+        <div className={styles.cards}>
+          {data.map((card, index) => (
+            <div key={"event-card" + index} className={styles.card}>
+              {card.coming && (
+                <div className={styles.blur}>
+                  <span>Coming Soon...</span>
+                </div>
+              )}
+              <div className={styles.imgContainer}>
+                <img src={card.img} />
+              </div>
+              <div className={styles.content}>
+                <h3 className={`${styles.title} ${styles.cutoffText}`}>
+                  {card.title}
+                </h3>
+                <div className={styles.buttonContainer}>
+                  <button className={styles.button}>Register Now</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventsSection;
