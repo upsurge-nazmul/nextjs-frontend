e73d146a55e888b28eb03ee7c556da32b8133f17
Nazmul Alom <nazmul@upsurge.in>
2023-05-02 20:23:20 +0600

    common game list component added
    Former-commit-id: dcd437a86b651101d8d1a8b07278c9e29dd803a0 [formerly e20eeb02743dc11081b5f09c27f166a1ec64340f] [formerly 6bc8c7f5026077add5099068278447627075353f [formerly 3c024dc6c937e6cb1e5d0d6f83e3714bbbc88a7a]]
    Former-commit-id: 743184009718d7bf54993e22d37b9e86f7324fff [formerly 2ca39f1a347bd9ec3f782f12262776cf56ba9d1f]
    Former-commit-id: a2bfbadba52d70598c7f2cc50b16104d0508f165

---
+++ src/components/Games/GameList.js
@@ -0,0 +1,43 @@
+import GameCard from "../Dashboard/GameCard";
+import styles from "../../styles/WaitlistDashboard/gameList.module.scss";
+import { Game_Data } from "../../static_data/Game_Data";
+
+export default function GameList({
+  data,
+  handlegameclick = () => {},
+  gameunicoinrewards = null,
+  userdata = null,
+}) {
+  return (
+    <div className={styles.wrapper}>
+      {Object.keys(data).map((item, index) => {
+        return (
+          <GameCard
+            onClick={() =>
+              handlegameclick(
+                item,
+                Game_Data[item].pushto
+                  ? Game_Data[item].pushto.split("/")[
+                      Game_Data[item].pushto.split("/").length - 1
+                    ]
+                  : "",
+                Game_Data[item].webgl_key,
+                Game_Data[item].premium_plan,
+                userdata.premium_plan
+              )
+            }
+            reward={
+              gameunicoinrewards
+                ? gameunicoinrewards.includes(item)
+                  ? "Completed"
+                  : 200
+                : null
+            }
+            data={Game_Data[item]}
+            key={"chorecomponent" + index}
+          />
+        );
+      })}
+    </div>
+  );
+}
--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -17,6 +17,7 @@ import AvailableGames from "../../components/DownloadGames/AvailableGames";
 import GameCard from "../../components/Dashboard/GameCard";
 import GameView from "../../components/Games/GameView";
 import UnicoinsAwards from "../../components/UnicoinsAwards";
+import GameList from "../../components/Games/GameList";
 
 export default function GamePage() {
   const router = useRouter();
@@ -27,11 +28,17 @@ export default function GamePage() {
   const [showpopup, setshowpopup] = useState(false);
   const [openGame, setOpenGame] = useState("");
   const [showUnicoinsAwards, setShowUnicoinsAwards] = useState(false);
-  const [updateTimesPlayed,setUpdateTimesPlayed] = useState(false);
+  const [updateTimesPlayed, setUpdateTimesPlayed] = useState(false);
   const comingsoongames = ["Ludo", "HighAndLow", "MoneyMath"];
-  const [unicoins,setUnicoins] = useState(null);
-  const { userdata, theme, skipActive, setskipActive,timesPlayed,setTimesPlayed } =
-    useContext(MainContext);
+  const [unicoins, setUnicoins] = useState(null);
+  const {
+    userdata,
+    theme,
+    skipActive,
+    setskipActive,
+    timesPlayed,
+    setTimesPlayed,
+  } = useContext(MainContext);
   const [gameOpened, setGameOpened] = useState(null);
   useEffect(() => {
     const handlescroll = () => {
@@ -44,17 +51,17 @@ export default function GamePage() {
     window.addEventListener("scroll", handlescroll);
     return () => window.removeEventListener("scroll", handlescroll);
   }, []);
-  useEffect(()=>{
-    if(!userdata){
-      if(timesPlayed > 0){
+  useEffect(() => {
+    if (!userdata) {
+      if (timesPlayed > 0) {
         setShowUnicoinsAwards(true);
         setUnicoins(4000);
         let game = localStorage.getItem("gameOpened");
-        console.log(`game`,game);
+        console.log(`game`, game);
         setGameOpened(JSON.parse(game));
       }
     }
-  },[updateTimesPlayed]);
+  }, [updateTimesPlayed]);
   async function handlegameclick(
     title,
     pushto,
@@ -62,8 +69,8 @@ export default function GamePage() {
     premium_plan,
     userPlan,
     isSimulator = false
-    ) {
-    localStorage.setItem("gameOpened",JSON.stringify(title));
+  ) {
+    localStorage.setItem("gameOpened", JSON.stringify(title));
     setOpenGame(pushto ? pushto : title);
   }
   return (
@@ -99,104 +106,55 @@ export default function GamePage() {
         />
         
           */}
-        <CarouselGames userdata={userdata} setshowauth={setshowauth} setauthmode={setauthmode} />
+        <CarouselGames
+          userdata={userdata}
+          setshowauth={setshowauth}
+          setauthmode={setauthmode}
+        />
         <div className={styles.headingSection}>
-          <h2 className={styles.heading}>Kids see fun. Parents see progress!</h2>
+          <h2 className={styles.heading}>
+            Kids see fun. Parents see progress!
+          </h2>
           <h3 className={styles.subheading}>
-            Get access to the most fun educational financial literacy & entrepreneurial games for kids.
+            Get access to the most fun educational financial literacy &
+            entrepreneurial games for kids.
             <br />
             <br />
-          </h3> 
+          </h3>
         </div>
-        
-        <div className={styles.gamelistwrapper}>
-          {Object.keys(Game_Data).map((item, index) => {
-            return (
-              <GameCard
-                onClick={() =>{
-                  if(timesPlayed === 0){
-                    handlegameclick(
-                      item,
-                      Game_Data[item].pushto
-                      ? Game_Data[item].pushto.split("/")[
-                        Game_Data[item].pushto.split("/").length - 1
-                      ]
-                    : "",
-                    Game_Data[item].webgl_key,
-                    Game_Data[item].premium_plan,
-                    null
-                    )
-                  }
-                  else{
-                    setUpdateTimesPlayed(true);
-                  }
-                }
-                }
-                data={Game_Data[item]}
-                key={"game" + index}
-                className={styles.gameCard}
-                // onClick={() => router.push(`/games/${item}`)}
-              >
-                <img
-                  src={ Game_Data[item].img_ludo || `/images/games/${item}.png` }
-                  alt=""
-                />
-                <p className={styles.title}>{Game_Data[item].name}</p>
-                <p className={styles.detail}>{Game_Data[item].description}</p>
-                {/* <p
-                  className={styles.activebutton}
-                  onClick={() => handleclick(item)}
-                >
-                  Play
-                </p> */}
 
-                {/* {comingsoongames.includes(item) ? (
-                  <p className={styles.button}>Coming Soon....</p>
-                ) : (
-                )} 
-                */}
-                {skipActive ? (
-                  <p
-                    className={styles.activebutton}
-                    onClick={() => {
-                      setskipActive(false);
-                      router.push("/games/" + item);
-                    }}
-                  >
-                    Play Now
-                  </p>
-                ) : (
-                  <p
-                    className={styles.activebutton}
-                    onClick={() => {
-                      if (!userdata) {
-                        setshowauth(true);
-                        setauthmode("parentChild");
-                      } else if (userdata.user_type === "child") {
-                        router.push("/dashboard/k/games");
-                      } else if (userdata.user_type === "parent") {
-                        router.push("/dashboard/p/games");
-                      }
-                    }}
-                  >
-                    Play Now
-                  </p>
-                )}
-            </GameCard>
-            );
-          })}
+        <div className={styles.gamelistwrapper}>
+          <GameList
+            data={Game_Data}
+            handlegameclick={handlegameclick}
+            userdata={userdata}
+          />
         </div>
-        {/* <AvailableGames /> */}
       </div>
       <JoinUs />
       <Footer />
-      {openGame ? <GameView game={openGame} setGame={setOpenGame} timesPlayed={timesPlayed} setTimesPlayed={setTimesPlayed} setShowUnicoinsAwards={setShowUnicoinsAwards} setUnicoins={setUnicoins} /> : ""}
-      {!userdata && showUnicoinsAwards ? 
-        <UnicoinsAwards 
-         setShowUnicoinsAwards={setShowUnicoinsAwards}
-         unicoins={unicoins}
-         setshowauth={setshowauth}
-         setauthmode={setauthmode} /> :"" }
+      {openGame ? (
+        <GameView
+          game={openGame}
+          setGame={setOpenGame}
+          timesPlayed={timesPlayed}
+          setTimesPlayed={setTimesPlayed}
+          setShowUnicoinsAwards={setShowUnicoinsAwards}
+          setUnicoins={setUnicoins}
+        />
+      ) : (
+        ""
+      )}
+      {!userdata && showUnicoinsAwards ? (
+        <UnicoinsAwards
+          setShowUnicoinsAwards={setShowUnicoinsAwards}
+          unicoins={unicoins}
+          setshowauth={setshowauth}
+          setauthmode={setauthmode}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
