14a927cd3e196efdfabef820eab5d6ff4060c12c
Alok <alok@upsurge.in>
2023-05-09 14:11:23 +0530

    exclude unused files from sitemap

--- sitemapconfig.js
+++ sitemapconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteUrl: "https://upsurge.in",
   generateRobotsTxt: true, // (optional)
-  exclude: ["/dashboard/*"],
+  exclude: ["/dashboard/*", "/cheatcodes/*", "/class/*", "/classroom/*", "/community/*", "/games/test/*", "/generatepass/*", "/payments/*", "/resetpassword/*", "/test/*", "/tribes", "/unsub/*"],
   // ...other options
 };
