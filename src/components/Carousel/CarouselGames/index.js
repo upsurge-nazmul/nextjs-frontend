9201fc4d735c2351e3e4481f2255b4516d940dcb
Pratham <pratham.sharma2105@gmail.com>
2023-06-08 00:16:24 +0530

    game page responsive

--- src/components/Carousel/CarouselGames/index.js
+++ src/components/Carousel/CarouselGames/index.js
@@ -3,6 +3,8 @@ import { useRouter } from "next/dist/client/router";
 import styles from "../../../styles/GeneralComponents/carouselgames.module.scss";
 import Buttons from "../../Buttons";
 import { motion } from "framer-motion";
+import GameSvg from "../../SVGcomponents/GameSvg";
+// import { isDesktop } from "react-device-detect";
 
 const slidesData = [
   {
@@ -26,39 +28,13 @@ const slidesData = [
     description:
       "A version of cult classic. Learn concepts like saving and investing while enjoying the chance based thrill.",
   },
-  {
-    id: 3,
-    image: "./images/downloadGames/Money_dash_2.png",
-    title: "Mini Miner",
-    description:
-      "A game that lets you buy and sell virtual stocks. Learn concepts like investing mutual funds and more.",
-  },
-  {
-    id: 4,
-    image: "./images/downloadGames/Money_dash_3.png",
-    title: "Snakes and Ladders",
-    description:
-      "A version of cult classic. Learn concepts like saving and investing while enjoying the chance based thrill.",
-  },
-  {
-    id: 5,
-    image: "./images/downloadGames/Money_dash_2.png",
-    title: "Mini Miner",
-    description:
-      "A game that lets you buy and sell virtual stocks. Learn concepts like investing mutual funds and more.",
-  },
-  {
-    id: 6,
-    image: "./images/downloadGames/Money_dash_3.png",
-    title: "Snakes and Ladders",
-    description:
-      "A version of cult classic. Learn concepts like saving and investing while enjoying the chance based thrill.",
-  },
 ];
 
 export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
   const router = useRouter();
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
     const id = setInterval(() => {
       if (currentSlide === slidesData.length - 1) {
@@ -70,6 +46,25 @@ export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
     return () => clearInterval(id);
   }, [currentSlide]);
 
+  useEffect(() => {
+    const mobileAndTabletCheck = () => {
+      let check = false;
+      (function (a) {
+        if (
+          /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+            a
+          ) ||
+          /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+            a.substr(0, 4)
+          )
+        )
+          check = true;
+      })(navigator.userAgent || navigator.vendor || window.opera);
+      setIsMobile(check);
+    };
+    mobileAndTabletCheck();
+  }, []);
+
   return (
     <div className={styles.slider}>
       <div className={styles.sliderWrapper}>
@@ -79,7 +74,11 @@ export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
             initial={{ scale: 0, rotate: -180 }}
             animate={{
               rotate: 0,
-              left: `${(index - currentSlide) * 34 + 25}vw`,
+              left: `${
+                isMobile
+                  ? (index - currentSlide) * 90 + 5
+                  : (index - currentSlide) * 34 + 25
+              }vw`,
               scale: index === currentSlide ? 1 : 0.8,
             }}
             transition={{
@@ -89,6 +88,7 @@ export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
             }}
             style={{
               opacity: index === currentSlide ? 1 : 0.8,
+              // backgroundColor: isMobile ? "red" : "blue",
             }}
             className={styles.slide}
           >
@@ -99,8 +99,8 @@ export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
                 <p className={styles.description}>{data.description}</p>
               </div>
               <div className={styles.activeButtonContainer}>
-                <Buttons
-                  handleClick={() => {
+                <button
+                  onClick={() => {
                     if (!userdata) {
                       setshowauth(true);
                       setauthmode("parentChild");
@@ -110,9 +110,11 @@ export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
                       router.push("/dashboard/p/games");
                     }
                   }}
+                  className={styles.actionButton}
                 >
-                  Play Now!
-                </Buttons>
+                  <GameSvg className={styles.icon} />
+                  Play Now
+                </button>
               </div>
             </div>
           </motion.div>
@@ -126,7 +128,6 @@ export default function CarouselGames({ userdata, setshowauth, setauthmode }) {
               slide.id === currentSlide ? styles.active : ""
             }`}
             onClick={() => {
-              // Slide(slide.id);
               setCurrentSlide(slide.id);
             }}
           ></div>
