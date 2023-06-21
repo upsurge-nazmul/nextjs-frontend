947ab5234c7c48b9b8cde6214fcad2052587a681
Pratham <pratham.sharma2105@gmail.com>
2023-06-22 03:06:49 +0530

    created new design of hero section of school page

--- src/components/School/HeroSection.js
+++ src/components/School/HeroSection.js
@@ -3,25 +3,47 @@ import styles from "../../styles/schools/schoolHero.module.scss";
 const SchoolHeroSection = () => {
   return (
     <div className={styles.container}>
-      <div className={`${styles.doodle} ${styles.dl1}`} />
+      {/* <div className={`${styles.doodle} ${styles.dl1}`} />
       <div className={`${styles.doodle} ${styles.dl2}`} />
       <div className={`${styles.hc1}`} />
-      <div className={`${styles.hc2}`} />
+      <div className={`${styles.hc2}`} /> */}
       <img
-        src={"https://imgcdn.upsurge.in/images/schools/school-hero-bg.png"}
+        // src={"https://imgcdn.upsurge.in/images/schools/school-hero-bg.png"}
+        src="/images/school-bg.svg"
         className={styles.bgImage}
         alt=" "
       />
       <div className={styles.left}>
+        <h1 className={styles.heading}>
+          Financial literacy and entrepreneurship programs for schools!
+        </h1>
         <p>
-          upsurge offers financial literacy and entrepreneurship workshops for
-          schools! Our workshops aim to equip students with the knowledge and
-          skills they need to succeed in the real world.
+          Our programs equip students with the knowledge and skills they need to
+          succeed in the real world.
         </p>
+        <button className={styles.exploreButton}>
+          <span>Explore</span>
+          <div className={styles.arrow}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+              />
+            </svg>
+          </div>
+        </button>
       </div>
       <div className={styles.right}>
         <img
-          src={"https://imgcdn.upsurge.in/images/schools/school-hero-img.png"}
+          src="https://imgcdn.upsurge.in/images/schools/new/proud-little-boy-with-glasses-graduation-cap.png"
           alt="Child Image"
         />
       </div>
