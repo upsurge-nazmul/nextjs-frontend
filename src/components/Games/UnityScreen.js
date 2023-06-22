7c280876c2563c8370ee1d577faee5ed83d5441e
Alok <alok@upsurge.in>
2023-06-22 18:22:52 +0530

    update the unity page to remove the action area

--- src/components/Games/UnityScreen.js
+++ src/components/Games/UnityScreen.js
@@ -5,6 +5,7 @@ import GameLoading from "./GameLoading";
 import { MainContext } from "../../context/Main";
 import { useRouter } from "next/router";
 import ActionArea from "./ActionArea";
+import firebase from "firebase";
 
 export default function UnityScreen({
   data = {},
@@ -36,18 +37,36 @@ export default function UnityScreen({
     if (isLoaded) {
       console.log("Game Loaded");
       sendMessage("GameData", "SetUserID", userdata?.user_id);
+      const firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyB0H7oe_1PNd_csJnYjswwnh198NQ_SG3s",
+        authDomain: "fir-test-90675.firebaseapp.com",
+        databaseURL: "https://fir-test-90675-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "fir-test-90675",
+        storageBucket: "fir-test-90675.appspot.com",
+        messagingSenderId: "822460102042",
+        appId: "1:822460102042:web:4553a0d5be919829e37623"
+    });
+    const db = firebaseApp.firestore();
     }
   }, [isLoaded]);
 
   useEffect(() => {
+    
     addEventListener("OnSeceneLoaded", () => {
       console.log("Scene Loaded Event called");
     });
+   
     addEventListener("Exit", async () => {
       console.log("Exiting Game");
-      await unload();
-      handleGameExit();
+      try {
+        await unload();
+        console.log("Unload success");
+        handleGameExit();
+      } catch (error) {
+        console.error(`Unable to unload: ${error}`);
+      }
     });
+
     addEventListener("Score", async function (score) {
       let res = await GameApis.unicoinreward({ gameId: game });
       if (res?.data?.success) {
@@ -56,10 +75,34 @@ export default function UnityScreen({
         console.log(res?.data?.message || "");
       }
     });
+
+    addEventListener("KQ_done", async () => {
+      console.log("KQ_done");
+      if(status === "success")
+    {
+      try {
+        await unload();
+        handleGameExit();
+        } catch (error) {
+          console.error(`Unable to unload: ${error}`);
+        }
+    }
+    else
+    {
+      try {
+        await unload();
+        handleGameExit();
+        } catch (error) {
+          console.error(`Unable to unload: ${error}`);
+          }
+    }
+      
+          });
     return () => {
       removeEventListener("OnSeceneLoaded", () => {});
       removeEventListener("Exit", () => {});
       removeEventListener("Score", () => {});
+      removeEventListener("KQ_done", () => {});
     };
   }, [addEventListener, removeEventListener]);
 
@@ -73,7 +116,7 @@ export default function UnityScreen({
       <div className={!isLoaded ? styles.loadingArea : styles.hidden}>
         <GameLoading percentage={loadingProgression} />
       </div>
-      {!loading && (
+      {/* {!loading && (
         <ActionArea
           onClose={async () => {
             await unload();
@@ -86,7 +129,7 @@ export default function UnityScreen({
           fullScreen={fullScreen}
           showDone={showDone}
         />
-      )}
+      )} */}
     </>
   );
 }
