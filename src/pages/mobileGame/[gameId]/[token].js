8b734ed016a72c2b00858d44566fe1047a1601d4
Nazmul Alom <nazmul@upsurge.in>
2023-07-12 21:31:34 +0600

    game view page added for mobile phone

--- src/pages/mobileGame/[gameId]/[token].js
+++ src/pages/mobileGame/[gameId]/[token].js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/dist/client/router";
-import KnowledgeQuestApi from "../../../actions/apis/KnowledgeQuestApi";
 import UnityScreen from "../../../components/Games/UnityScreen";
 import styles from "../../../styles/Games/gameView.module.scss";
+import GameApis from "../../../actions/apis/GameApis";
 
 export default function GameView({
   setUnicoins = () => {},
@@ -10,8 +10,8 @@ export default function GameView({
 }) {
   const gameRef = useRef();
   const router = useRouter();
-  // console.log("router", router);
-  const { questId, token } = router.query;
+  //   console.log("router", router);
+  const { gameId, token } = router.query;
   const [gameData, setGameData] = useState();
   const [loading, setLoading] = useState(false);
 
@@ -24,18 +24,18 @@ export default function GameView({
   useEffect(() => {
     async function fetchGameData() {
       setLoading(true);
-      const res = await KnowledgeQuestApi.getQuestFiles({ id: questId }, token);
+      const res = await GameApis.gamedata({ id: gameId });
       if (res && res.data && res.data.data) {
         setGameData(res.data.data);
       }
       setLoading(false);
     }
-    if (questId && token) fetchGameData();
-  }, [questId, token]);
+    if (gameId && token) fetchGameData();
+  }, [gameId, token]);
 
   const handleGameClose = () => {
     sendDataToReactNativeApp();
-    mixpanel.track("Quest Closed", { event: `Quest closed` });
+    mixpanel.track("Game Closed", { event: `Game closed` });
     setShowUnicoinsAwards(true);
     setUnicoins(4000);
   };
