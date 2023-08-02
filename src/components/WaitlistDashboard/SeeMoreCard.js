77a9eceec7ea536d43befcc6d13359a66214cc2a
Alok <alok@upsurge.in>
2023-08-02 16:34:06 +0530

    update see more

--- src/components/WaitlistDashboard/SeeMoreCard.js
+++ src/components/WaitlistDashboard/SeeMoreCard.js
@@ -3,7 +3,7 @@ import styles from "../../styles/WaitlistDashboard/seeMoreCard.module.scss";
 export default function SeeMoreCard({ handleClick }) {
   return (
     <div className={styles.seeMore} onClick={handleClick}>
-      <div className={styles.text}>See More...</div>
+      <div className={styles.text}>↓ See More ↓</div>
     </div>
   );
 }
