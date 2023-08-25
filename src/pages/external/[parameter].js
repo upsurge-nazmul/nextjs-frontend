fa8dd2baf4dbcede2aa294c4f699d4d67e84f688
Nazmul Alom <nazmul@upsurge.in>
2023-08-25 20:16:01 +0600

    external redirect to app is working

--- src/pages/external/[parameter].js
+++ src/pages/external/[parameter].js
@@ -7,13 +7,84 @@ function ExternalLanding() {
   const router = useRouter();
   const { parameter } = router.query;
 
+  // const checkAndRedirect = async () => {
+  //   const userAgent = navigator.userAgent.toLowerCase();
+
+  //   // Check if the user is on an Android device
+  //   const isAndroid = userAgent.includes("android");
+
+  //   if (isAndroid) {
+  //     // Check if the app is installed by trying to open a custom URL scheme
+  //     const appScheme = "upsurge://";
+  //     const deeplink = `upsurge://app/external/${parameter}`;
+  //     const playStoreURL =
+  //       "https://play.google.com/store/apps/details?id=com.upsurgefi.app";
+
+  //     const checkAppInstalled = new Image();
+  //     checkAppInstalled.src = appScheme;
+  //     checkAppInstalled.onload = () => {
+  //       console.log("App is installed");
+  //       // Open the app using the custom URL scheme
+  //       window.location.href = deeplink;
+  //     };
+  //     checkAppInstalled.onerror = () => {
+  //       console.log("App is not installed");
+  //       // Open the Play Store
+  //       window.location.href = playStoreURL;
+  //     };
+  //   }
+  // };
+
+  // const checkAndRedirect = async () => {
+  //   const userAgent = navigator.userAgent.toLowerCase();
+
+  //   // Check if the user is on an Android device
+  //   const isAndroid = userAgent.includes("android");
+
+  //   if (isAndroid) {
+  //     // Check if the app is installed by trying to open a custom URL scheme
+  //     const appScheme = "upsurge://";
+  //     const deeplink = `upsurge://app/external/${parameter}`;
+  //     const playStoreURL =
+  //       "https://play.google.com/store/apps/details?id=com.upsurgefi.app";
+
+  //     setTimeout(function () {
+  //       window.location.href = deeplink;
+
+  //       // If the page hasn't changed, the app is not installed
+  //       if (document.hidden) {
+  //         // Redirect to the Play Store
+  //         window.location.href = playStoreURL;
+  //       }
+  //     }, 1000);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // Define the deep link URL
+  //   const deepLinkURL =
+  //     "intent://app/external/games#Intent;scheme=upsurge;package=com.upsurgefi.app;end";
+
+  //   // Simulate a click on a hidden link to trigger the deep link
+  //   const hiddenLink = document.createElement("a");
+  //   hiddenLink.href = deepLinkURL;
+  //   hiddenLink.style.display = "none";
+  //   document.body.appendChild(hiddenLink);
+  //   hiddenLink.click();
+  //   document.body.removeChild(hiddenLink);
+  // }, []);
+
   useEffect(() => {
-    // Handle your logic for deep link parameter here
-    console.log("Received parameter:", parameter);
-    // You can perform actions based on the received parameter
+    // router.push(
+    //   "intent://app/external/games#Intent;scheme=upsurge;package=com.upsurgefi.app;end"
+    // );
+    // window.location.href =
+    //   "intent://app/external/games#Intent;scheme=upsurge;package=com.upsurgefi.app;end";
+    if (parameter)
+      window.location.href = `intent://app/external/${parameter}#Intent;scheme=upsurge;package=com.upsurgefi.app;end`;
   }, [parameter]);
 
-  return <div>External Landing Page</div>;
+  return <div>{parameter}</div>;
 }
 
 export default ExternalLanding;
