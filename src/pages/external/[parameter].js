b4ef44d8d4a234ea16c9830806708756db8fde76
Nazmul Alom <nazmul@upsurge.in>
2023-08-25 20:39:46 +0600

    external redirection page device detection added

--- src/pages/external/[parameter].js
+++ src/pages/external/[parameter].js
@@ -1,90 +1,41 @@
-// pages/ExternalLanding.js
-
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 function ExternalLanding() {
   const router = useRouter();
   const { parameter } = router.query;
 
-  // const checkAndRedirect = async () => {
-  //   const userAgent = navigator.userAgent.toLowerCase();
-
-  //   // Check if the user is on an Android device
-  //   const isAndroid = userAgent.includes("android");
-
-  //   if (isAndroid) {
-  //     // Check if the app is installed by trying to open a custom URL scheme
-  //     const appScheme = "upsurge://";
-  //     const deeplink = `upsurge://app/external/${parameter}`;
-  //     const playStoreURL =
-  //       "https://play.google.com/store/apps/details?id=com.upsurgefi.app";
-
-  //     const checkAppInstalled = new Image();
-  //     checkAppInstalled.src = appScheme;
-  //     checkAppInstalled.onload = () => {
-  //       console.log("App is installed");
-  //       // Open the app using the custom URL scheme
-  //       window.location.href = deeplink;
-  //     };
-  //     checkAppInstalled.onerror = () => {
-  //       console.log("App is not installed");
-  //       // Open the Play Store
-  //       window.location.href = playStoreURL;
-  //     };
-  //   }
-  // };
-
-  // const checkAndRedirect = async () => {
-  //   const userAgent = navigator.userAgent.toLowerCase();
-
-  //   // Check if the user is on an Android device
-  //   const isAndroid = userAgent.includes("android");
-
-  //   if (isAndroid) {
-  //     // Check if the app is installed by trying to open a custom URL scheme
-  //     const appScheme = "upsurge://";
-  //     const deeplink = `upsurge://app/external/${parameter}`;
-  //     const playStoreURL =
-  //       "https://play.google.com/store/apps/details?id=com.upsurgefi.app";
-
-  //     setTimeout(function () {
-  //       window.location.href = deeplink;
-
-  //       // If the page hasn't changed, the app is not installed
-  //       if (document.hidden) {
-  //         // Redirect to the Play Store
-  //         window.location.href = playStoreURL;
-  //       }
-  //     }, 1000);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   // Define the deep link URL
-  //   const deepLinkURL =
-  //     "intent://app/external/games#Intent;scheme=upsurge;package=com.upsurgefi.app;end";
-
-  //   // Simulate a click on a hidden link to trigger the deep link
-  //   const hiddenLink = document.createElement("a");
-  //   hiddenLink.href = deepLinkURL;
-  //   hiddenLink.style.display = "none";
-  //   document.body.appendChild(hiddenLink);
-  //   hiddenLink.click();
-  //   document.body.removeChild(hiddenLink);
-  // }, []);
+  const [isAndroid, setIsAndroid] = useState(false);
 
   useEffect(() => {
-    // router.push(
-    //   "intent://app/external/games#Intent;scheme=upsurge;package=com.upsurgefi.app;end"
-    // );
-    // window.location.href =
-    //   "intent://app/external/games#Intent;scheme=upsurge;package=com.upsurgefi.app;end";
-    if (parameter)
-      window.location.href = `intent://app/external/${parameter}#Intent;scheme=upsurge;package=com.upsurgefi.app;end`;
-  }, [parameter]);
+    const userAgent = window.navigator.userAgent.toLowerCase();
 
-  return <div>{parameter}</div>;
+    if (userAgent.includes("android")) {
+      // Operating system is Android
+      console.log("Operating system is Android");
+      setIsAndroid(true);
+    } else {
+      // Operating system is not Android
+      console.log("Operating system is not Android");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isAndroid) {
+      if (parameter)
+        window.location.href = `intent://app/external/${parameter}#Intent;scheme=upsurge;package=com.upsurgefi.app;end`;
+    }
+  }, [isAndroid, parameter]);
+
+  return (
+    <div>
+      {!isAndroid ? (
+        <div>This page is for Android users only</div>
+      ) : (
+        <div>Redirecting to app in {parameter} page ...</div>
+      )}
+    </div>
+  );
 }
 
 export default ExternalLanding;
