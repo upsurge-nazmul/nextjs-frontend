006b87c2be5ead1e4a1f2005373067f8809b05e2
Alok <alok@upsurge.in>
2023-08-28 23:19:11 +0530

    clean up journey card

--- src/components/Journey/JourneyCard.js
+++ src/components/Journey/JourneyCard.js
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import styles from "../../styles/Journey/journeyCard.module.scss";
-import Image from "../Image";
 
 export default function JourneyCard({
   data,
@@ -20,38 +19,13 @@ export default function JourneyCard({
   return (
     <div className={styles.journeyCard} style={{ backgroundImage: `url(/images/kq/${data.questId}.webp)`}}  onClick={() => handleActionClick(data?.questId)}>
       
-      
-        {/* <div className={`${styles.questType} mt-2`}>{data?.title}</div> */}
         <div className={styles.cardBottom}>
         <div className={`${styles.title} mb-3`}>{data?.title}</div>
-        <div className={`${styles.actionButton} rounded-pill text-center py-1`}     >
+        <div className={`${styles.actionButton} rounded-pill text-center py-1`} >
           &#9658; 
         </div>
         </div>
       
     </div>
   );
-  // return (
-  //   <div className={styles.journeyCard}>
-  //     <div className={styles.cardLeft}>
-  //       <div className={styles.cardImage}>
-  //         <Image
-  //           imageUrl={`/images/kq/${data.questId}.webp`}
-  //           alt={data.title}
-  //           placeholderText={data.title}
-  //         />
-  //       </div>
-  //     </div>
-  //     <div className={styles.cardRight}>
-  //       <div className={`${styles.questType} mt-2`}>{data?.quest_type}</div>
-  //       <div className={`${styles.title} mb-3`}>{data?.title}</div>
-  //       <div
-  //         onClick={() => handleActionClick(data?.questId)}
-  //         className={`${styles.actionButton} rounded-pill text-center py-1`}
-  //       >
-  //         Start Learning
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
