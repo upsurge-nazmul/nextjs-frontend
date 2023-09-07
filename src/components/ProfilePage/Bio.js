69c9c8d5ecd61294aaf9060758f07a720a0e42ca
Nazmul Alom <nazmul@upsurge.in>
2023-09-07 16:36:56 +0600

    profile left section design complete

--- src/components/ProfilePage/Bio.js
+++ src/components/ProfilePage/Bio.js
@@ -1,28 +1,93 @@
 import { useState } from "react";
 import styles from "../../styles/EditProfile/profilePage.module.scss";
+import EditIcon from "@mui/icons-material/Edit";
+import GppMaybeIcon from "@mui/icons-material/GppMaybe";
+import VerifiedIcon from "@mui/icons-material/Verified";
+
+const BioItem = ({
+  label = "",
+  value = "",
+  editActionHandler = () => {},
+  additionalAction = {
+    icon: "",
+    label: "",
+    actionHandler: () => {},
+  },
+}) => {
+  return (
+    <div className={styles.bioItem}>
+      <div className={styles.itemBody}>
+        <div className={styles.itemLabel}>{label}</div>
+        <div className={styles.itemValue}>{value}</div>
+        <div
+          className={styles.itemAdditionalAction}
+          style={{
+            cursor: additionalAction.actionHandler ? "pointer" : "default",
+          }}
+          onClick={additionalAction.actionHandler}
+        >
+          {additionalAction.icon} {additionalAction.label}
+        </div>
+      </div>
+      <div className={styles.itemAction}>
+        <button onClick={editActionHandler} className={styles.actionButton}>
+          <EditIcon /> Edit
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default function Bio({ data }) {
-  const [username, setusername] = useState(data?.user_name || "");
-  const [phone, setphone] = useState(data?.phone || "");
-  const [changephone, setchangephone] = useState("");
-  const [confirmphone, setconfirmphone] = useState("");
-  const [password, setpassword] = useState("");
-  const [confirmpassword, setconfirmpassword] = useState("");
-  const [showpassotp, setshowpassotp] = useState(false);
-  const [showphoneotp, setshowphoneotp] = useState(false);
-  const [showpopup, setshowpopup] = useState(false);
-  const [showphonepopup, setshowphonepopup] = useState(false);
-  const [showimgsetter, setshowimgsetter] = useState(false);
-  const [showavatarmodal, setshowavatarmodal] = useState(false);
+  const [bioData, setbioData] = useState({
+    userName: "",
+    email: "",
+    phone: "",
+  });
+
+  console.log("data", data);
 
   return (
-    <div className={styles.rightContent}>
+    <div className={styles.bio}>
       <div className={styles.avatarArea}>
-        <div className="avatar">Avatar</div>
+        <img
+          onClick={() => {}}
+          id="avatar"
+          src={
+            data?.user_img_url ||
+            "https://imgcdn.upsurge.in/images/default-avatar.png"
+          }
+          alt="User Avatar"
+          className={styles.avatarImg}
+        />
       </div>
-      <div className={styles.userNameArea}>User Name</div>
-      <div className={styles.emailArea}>Email</div>
-      <div className={styles.phoneArea}>Phone</div>
+      <BioItem label={"Username"} value={data.user_name} />
+      {data.email ? (
+        <BioItem label={"Email"} value={data.email} />
+      ) : (
+        <BioItem
+          label={"Parent Email"}
+          value={data.parent_email}
+          additionalAction={{
+            icon: <GppMaybeIcon style={{ color: "#de6869" }} />, // just pass color with the icon
+            label: "Click here to Verify your email address",
+            action: () => {},
+          }}
+        />
+      )}
+      {data.phone ? (
+        <BioItem label={"Phone"} value={data.phone} />
+      ) : (
+        <BioItem
+          label={"Parent Phone"}
+          value={data.parent_phone}
+          additionalAction={{
+            icon: <VerifiedIcon style={{ color: "#5955e5" }} />, // just pass color with the icon
+            label: "Verified",
+            action: () => {},
+          }}
+        />
+      )}
       <div className={styles.passwordArea}>
         <button className={styles.changePasswordButton}>Change Password</button>
       </div>
--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,19 +1,11 @@
-import { useState, useEffect, useContext } from "react";
 import styles from "../../styles/EditProfile/profilePage.module.scss";
-import { MainContext } from "../../context/Main";
 import Info from "./Info";
 import Bio from "./Bio";
 
 export default function ProfilePage({ data, childavatars }) {
-  const { userdata, setuserdata } = useContext(MainContext);
-
-  useEffect(() => {
-    setuserdata(data);
-  }, []);
-
   return (
     <div className={styles.mainContent}>
-      <Info {...{ data, childavatars }} />
+      <Info {...{ data }} />
       <Bio {...{ data, childavatars }} />
     </div>
   );
