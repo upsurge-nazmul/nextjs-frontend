955aabf066f4ccb515a790104ac2c692d448c09b
Alok <alok@upsurge.in>
2023-09-27 17:11:56 +0530

    update faq

--- src/components/Home/FaqSection.js
+++ src/components/Home/FaqSection.js
@@ -5,56 +5,32 @@ import Faq from "../Help/Faq";
 export default function FaqSection({ customfaq }) {
   const faqs = [
     {
-      question: "What do we do?",
-      answer: `We teach money management and Financial Literacy for Students of age 8 to 18 yrs`,
+      question: "What ages are upsurge games good for?",
+      answer: `It’s never too early to build skills and healthy financial habits! At the moment, our knowledge quests and games are relevant for kids over the age of 10. `,
     },
     {
-      question: "How old does my child have to be to join upsurge?",
-      answer: `It’s never too early to build healthy financial habits! As a parent, you’ll have to open an account first and then add your children. At the moment, our knowledge quests and games are relevant for kids over the age of 10.`,
+      question: "What topics are covered by the games?",
+      answer: `Our games are designed to deliver outcomes in financial literacy, entrepreneurship, problem solving, critical thinking, career awareness and real-life situational awareness.`,
     },
     {
-      question: "What are some of the Courses (Knowledge Quests) available?",
-      answer: `We have immersive gamified courses on 
-      Entrepreneurship
-      Smart Money Management
-      Investing
-      Marketing 
-      Industry Fundamentals (Hospitality, Media & Entertainment, VC, FMCG etc.)
-      Career Quests (Author, Sportsperson, Game Developer)
-      and much more
-      `,
+      question: `How can I login?`,
+      answer: `Once you’ve created your account, you can login using your username and password.`,
     },
     {
-      question: `What will children learn?`,
-      answer: `We aim to empower Financial Literacy for students with the life skills such as Functioning of money in 21st Century, Money Management, Investing and growing money, Building and executing a Business`,
+      question: `Can multiple students use the same account?`,
+      answer: `No - each student should have a unique account with a unique phone number/email`,
     },
     {
-      question: "What skills will the children gain?",
-      answer: `Through our courses and games, the children will aslo gain exposure to situations where they will get to develop their
-      Problem Solving
-      Critical Thinking
-      Innovative Thinking
-      Communication
-      `,
+      question: `Will I need to give my mobile number to create an account in upsurge?`,
+      answer: `We will need your mobile number to verify your account and make sure that you, and only you, can access your account.`,
     },
     {
-      question: "Why understand personal finance?",
-      answer: `The most important skills to build a future are not taught in school. One of the most important aspects of a successful individual is managing money. And what if we told you it's as simple as enrolling your child with us? Our comprehensive program will help your child achieve his/her financial goals
-      `,
+      question: `Will  I be able to  create an upsurge account, if I am not in India?`,
+      answer: `Yes! You can create an account from any part of the world. We also accept international payment options through our payment partners.`,
     },
     {
-      question: "Why Entrepreneurship skills?",
-      answer: `Entrepreneurship skills help students to ideate their thoughts into viable results. Students learn to identify economic opportunities and act upon them. It teaches innovation, perseverance, dedication, decision-making, and self-confidence.`,
-    },
-    {
-      question: "What does my child need to know to take the courses?",
-      answer:
-        "Our course are made keeping children in mind - so all they have to know is english comprehension and basic mathematical concepts.",
-    },
-    {
-      question: "What if the student does not like the course?",
-      answer:
-        "We have a 14 day no question asked refund policy if you enroll with us for 6 months or longer.",
+      question: `Will my information be secured?`,
+      answer: `Yes, all the data pertaining to your child will be completely secured and used only to analyze their performance so that we can share the same with you. We do not collect any other data, other than what they do on our platform`,
     },
   ];
   const [current, setcurrent] = useState("");
