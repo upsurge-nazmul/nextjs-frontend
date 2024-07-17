729acaa0dc562d5ba48543fe7df48f6e8e538991
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-07-17 20:45:50 +0600

    jasper updated

--- src/components/Home/JasperSection.js
+++ src/components/Home/JasperSection.js
@@ -5,31 +5,38 @@ import Jasper from "../SVGcomponents/Jasper";
 export default function JasperSection() {
   return (
     <>
-      <h2 className={styles.header}>A message from Jasper, your friend-cum-mentor at upsurge</h2>
-    <div className={styles.jasper}>
-      <div className={styles.leftpart}>
-        <div className={styles.text}>
-          <BottomArrowBubble className={styles.bottomarrow} />
-          <h3 className={styles.heading}>{`Hi there, I'm Jasper.`}</h3>
-          <p className={styles.subheading}>
-            {`Since I am a Unicorn (no, not the billion $$ kind) and so it only
+      <h2 className={styles.header}>
+        A message from Jasper, your friend-cum-mentor at upsurge
+      </h2>
+      <div className={styles.jasper}>
+        <div className={styles.leftpart}>
+          <div className={styles.text}>
+            <BottomArrowBubble className={styles.bottomarrow} />
+            <h3 className={styles.heading}>{`Hi there, I'm Jasper.`}</h3>
+            <p className={styles.subheading}>
+              {`Since I am a Unicorn (no, not the billion $$ kind) and so it only
             makes sense that I am the resident money expert here at upsurge. I
             will be your mentor throughout the platform, where children will get
             to understand money, investing, and entrepreneurship like never
             before.`}
-          </p>
-          <p className={styles.subheading}>
-            {`They can go on knowledge quests, play one of the many games
+            </p>
+            <p className={styles.subheading}>
+              {`They can go on knowledge quests, play one of the many games
             in the Arena, or earn UniCoins by completing chores. They can then
             redeem the UniCoins they've earned for real prizes from brands they
             (and you) love!`}
-          </p>
+            </p>
+          </div>
+        </div>
+        <div className={styles.rightpart}>
+          {/* <Jasper className={styles.jasperimg} /> */}
+          <img
+            src={"/images/home/superJasper.png"}
+            alt={"Super Jasper"}
+            className={styles.jasperimg}
+          />
         </div>
       </div>
-      <div className={styles.rightpart}>
-        <Jasper className={styles.jasperimg} />
-      </div>
-    </div>
-            </>
+    </>
   );
 }
