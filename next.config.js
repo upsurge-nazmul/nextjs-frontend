287f3edf8796d1a7b8707e7e7be6b09848c07241
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-08-02 11:34:07 +0600

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   async rewrites() {
     return [
@@ -21,9 +23,25 @@ module.exports = {
     ],
   },
   swcMinify: true,
+  // env: {
+  //   NEXT_PUBLIC_LIVE_SERVER: process.env.NEXT_PUBLIC_LIVE_SERVER,
+  //   NEXT_PUBLIC_TEST_SERVER: process.env.NEXT_PUBLIC_TEST_SERVER,
+  //   NEXT_PUBLIC_MEDIA_BUCKET: process.env.NEXT_PUBLIC_MEDIA_BUCKET,
+  //   NEXT_PUBLIC_GAME_BUCKET: process.env.NEXT_PUBLIC_GAME_BUCKET,
+  // },
+  // env: {
+  //   NEXT_PUBLIC_LIVE_SERVER: "https://server-g5bprkvzla-uc.a.run.app/",
+  //   NEXT_PUBLIC_TEST_SERVER: "https://server-g5bprkvzla-uc.a.run.app/",
+  //   NEXT_PUBLIC_MEDIA_BUCKET: "https://storage.googleapis.com",
+  //   NEXT_PUBLIC_GAME_BUCKET: "https://storage.googleapis.com",
+  // },
   env: {
-    NEXT_PUBLIC_LIVE_SERVER: "https://server-g5bprkvzla-uc.a.run.app/",
-    NEXT_PUBLIC_TEST_SERVER: "https://server-g5bprkvzla-uc.a.run.app/",
+    NEXT_PUBLIC_LIVE_SERVER: isProd
+      ? "https://server-g5bprkvzla-uc.a.run.app/"
+      : process.env.NEXT_PUBLIC_LIVE_SERVER || "http://localhost:3000",
+    NEXT_PUBLIC_TEST_SERVER: isProd
+      ? "https://server-g5bprkvzla-uc.a.run.app/"
+      : process.env.NEXT_PUBLIC_TEST_SERVER || "http://localhost:3000",
     NEXT_PUBLIC_MEDIA_BUCKET: "https://storage.googleapis.com",
     NEXT_PUBLIC_GAME_BUCKET: "https://storage.googleapis.com",
   },
