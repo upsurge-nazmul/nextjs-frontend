b97ef836c1758fc38f18979d44417ee0de3103d8
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-08-12 21:19:20 +0600

    leaderboard updated

--- src/components/ChildLeaderboard/LeaderboardTable.js
+++ src/components/ChildLeaderboard/LeaderboardTable.js
@@ -1,8 +1,10 @@
 import styles from "../../styles/Leaderboard/leaderboardTable.module.scss";
 import CrownCircle from "../SVGcomponents/leaderboard/CrownCircleSVG";
+
 export default function LeaderboardTable({ data, userdata }) {
-  const colors = ["#FFC700","#C6C6C6","#FFA96A"];
-  const whiteColor = ["","#fff"];
+  const colors = ["#FFC700", "#C6C6C6", "#FFA96A"];
+  const whiteColor = ["", "#fff"];
+
   return (
     <div className={styles.table}>
       <div className={styles.tableHead}>
@@ -15,8 +17,12 @@ export default function LeaderboardTable({ data, userdata }) {
         data.map((item, index) => {
           return (
             <div className={styles.row} key={item?.id || item.name + index}>
-              <p className={styles.rank}>{index + 1}
-              <CrownCircle color={colors[index]} whiteColor={whiteColor[index]} />
+              <p className={styles.rank}>
+                {index <= 9 ? index + 1 : "*"}
+                <CrownCircle
+                  color={colors[index]}
+                  whiteColor={whiteColor[index]}
+                />
               </p>
               <p className={styles.name}>
                 {item.user_name ||
@@ -26,10 +32,9 @@ export default function LeaderboardTable({ data, userdata }) {
                 {item.id === userdata?.user_id && "(you)"}
               </p>
               <p className={styles.score}>
-                
                 {(item.total_unicoins > 1000
-                    ? item.total_unicoins / 1000 + "K "
-                    : item.total_unicoins) ??
+                  ? item.total_unicoins / 1000 + "K "
+                  : item.total_unicoins) ??
                   (item.num_unicoins > 1000
                     ? item.num_unicoins / 1000 + "K "
                     : item.num_unicoins)}
