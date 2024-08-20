d0956d4d63fc89959bd4a5e6d060ef46887f91d8
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-08-20 12:51:30 +0600

    unicoin value updated

--- next.config.js
+++ next.config.js
@@ -23,26 +23,15 @@ module.exports = {
     ],
   },
   swcMinify: true,
-  // env: {
-  //   NEXT_PUBLIC_LIVE_SERVER: process.env.NEXT_PUBLIC_LIVE_SERVER,
-  //   NEXT_PUBLIC_TEST_SERVER: process.env.NEXT_PUBLIC_TEST_SERVER,
-  //   NEXT_PUBLIC_MEDIA_BUCKET: process.env.NEXT_PUBLIC_MEDIA_BUCKET,
-  //   NEXT_PUBLIC_GAME_BUCKET: process.env.NEXT_PUBLIC_GAME_BUCKET,
-  // },
-  // env: {
-  //   NEXT_PUBLIC_LIVE_SERVER: "https://server-g5bprkvzla-uc.a.run.app/",
-  //   NEXT_PUBLIC_TEST_SERVER: "https://server-g5bprkvzla-uc.a.run.app/",
-  //   NEXT_PUBLIC_MEDIA_BUCKET: "https://storage.googleapis.com",
-  //   NEXT_PUBLIC_GAME_BUCKET: "https://storage.googleapis.com",
-  // },
   env: {
     NEXT_PUBLIC_LIVE_SERVER: isProd
       ? "https://server-g5bprkvzla-uc.a.run.app/"
-      : process.env.NEXT_PUBLIC_LIVE_SERVER || "http://localhost:3000",
+      : process.env.NEXT_PUBLIC_LIVE_SERVER,
     NEXT_PUBLIC_TEST_SERVER: isProd
       ? "https://server-g5bprkvzla-uc.a.run.app/"
-      : process.env.NEXT_PUBLIC_TEST_SERVER || "http://localhost:3000",
+      : process.env.NEXT_PUBLIC_TEST_SERVER,
     NEXT_PUBLIC_MEDIA_BUCKET: "https://storage.googleapis.com",
     NEXT_PUBLIC_GAME_BUCKET: "https://storage.googleapis.com",
+    NEXT_PUBLIC_UNICOIN_VALUE: 1000,
   },
 };
