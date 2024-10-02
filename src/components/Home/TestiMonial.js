e8b004ad654cc35d4659058aa282b11575d7d7b2
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-10-02 12:14:48 +0600

    testimonial images updated

--- src/components/Home/TestiMonial.js
+++ src/components/Home/TestiMonial.js
@@ -11,71 +11,73 @@ const testimonials = [
     quote:
       "I am so glad someone is trying to teach our kids money management. Being a mother of 2 daughters, I want them to be much more financially independent and savvy than I am. Kudos upsurge for taking up this cause!",
     name: "Divya",
-    img: "https://imgcdn.upsurge.in/images/Untitled-design-73.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/Untitled-design-73.png",
   },
   {
     video: assetsCdn("video/testimonial-princi.mp4"),
-    picture: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+    picture:
+      "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
     name: "Veema Gaur",
-    img: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
     des: "Principal, VIBGYOR High School Gurugram",
   },
   {
     video: assetsCdn("video/testimonial-child.mov"),
-    picture: "https://imgcdn.upsurge.in/images/student-testimonial.png",
+    picture:
+      "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/student-testimonial.png",
     name: "Upsurge Intern",
-    img: "https://imgcdn.upsurge.in/images/student-testimonial.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/student-testimonial.png",
     // des: "Principal at Vibgyor High School Gurugram",
   },
   {
     quote:
       "We were fortunate enough to get access to upsurge for testing, and both my children absolutely loved it. The chore management feature has them competing against each other to finish their chores first and earn more unicoins, and the games have them competing against each other! And of course, they are learning very important money skills that were never taught to us.",
     name: "Sophia",
-    img: "https://imgcdn.upsurge.in/images/Untitled-design-70.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/Untitled-design-70.png",
   },
   {
     quote:
       "When I heard that there’s someone teaching personal finance to children, I got in touch with upsurge to get access. While they were under testing, they gave us access and my son has been loving the courses and the games. In fact, some of the games and activities that are meant for the family to play together are currently a favorite in our home.",
     name: "Sonam",
-    img: "https://imgcdn.upsurge.in/images/Untitled-design-76.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/Untitled-design-76.png",
   },
   {
     quote:
       "My 17-year-old daughter, after going through the quests and playing the investment games has started investing Rs. 2,500 a month in mutual funds! I couldn’t have imagined this a few months ago!",
     name: "Radha",
-    img: "https://imgcdn.upsurge.in/images/Untitled-design-69.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/Untitled-design-69.png",
   },
   {
     quote:
       "Within a month of starting upsurge, my son has become so involved with household budgets and money in general! He discusses business news with us, and wants to become a electric vehicle entrepreneur in the long term (just like his hero Mr. Musk)",
     name: "Pradeep",
-    img: "https://imgcdn.upsurge.in/images/Untitled-design-74.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/Untitled-design-74.png",
   },
   {
     quote:
       "The Quests are designed really well - easy to understand concepts, real examples and fun activities make sure that my daughter is learning and having fun.",
     name: "Mayur",
-    img: "https://imgcdn.upsurge.in/images/Untitled-design-75.png",
+    img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/Untitled-design-75.png",
   },
   // {
   //   video: assetsCdn("video/aarti-testimonial.mp4"),
-  //   picture: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+  //   picture: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
   //   name: "Aarti",
-  //   img: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+  //   img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
   //   des: "Principal, VIBGYOR High School Gurugram",
   // },
   // {
   //   video: assetsCdn("video/bluebells-testimonial.mov"),
   //   picture: "  images/teacher-testimonial.png",
   //   name: "Bluebells",
-  //   img: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+  //   img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
   //   des: "Principal, VIBGYOR High School Gurugram",
   // },
   // {
   //   video: assetsCdn("video/teen-testimonial.mp4"),
-  //   picture: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+  //   picture: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
   //   name: "Akhilesh",
-  //   img: "https://imgcdn.upsurge.in/images/teacher-testimonial.png",
+  //   img: "https://storage.googleapis.com/upsurge-assets-cdn/images/testimonial/teacher-testimonial.png",
   //   des: "Principal, VIBGYOR High School Gurugram",
   // },
 ];
