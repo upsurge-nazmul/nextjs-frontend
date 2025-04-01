55685bb907b217dbf13ed0992e7f9939cf964340
Nazmul Alom <nazmulshuvo03@gmail.com>
2025-04-02 02:40:09 +0600

    config updated for server

--- next.config.js
+++ next.config.js
@@ -24,8 +24,12 @@ module.exports = {
   },
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_LIVE_SERVER: process.env.NEXT_PUBLIC_LIVE_SERVER,
-    NEXT_PUBLIC_TEST_SERVER: process.env.NEXT_PUBLIC_TEST_SERVER,
+    NEXT_PUBLIC_LIVE_SERVER: isProd
+      ? "https://upsurge-server.onrender.com/"
+      : process.env.NEXT_PUBLIC_LIVE_SERVER,
+    NEXT_PUBLIC_TEST_SERVER: isProd
+      ? "https://upsurge-server.onrender.com/"
+      : process.env.NEXT_PUBLIC_TEST_SERVER,
     NEXT_PUBLIC_MEDIA_BUCKET: "https://storage.googleapis.com",
     NEXT_PUBLIC_GAME_BUCKET: "https://storage.googleapis.com",
     NEXT_PUBLIC_UNICOIN_VALUE: 1000,
